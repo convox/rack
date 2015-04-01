4d83b53f74ced1b0d27f2200afce9b195094a710
David Dollar <david@dollar.io>
2015-04-01 10:38:16 -0400

    move out into its own file

--- models/event.go
+++ models/event.go
@@ -21,29 +21,6 @@ type Event struct {
 
 type Events []Event
 
-// A Transaction groups more than one event on a Name/Type resource
-type Transaction struct {
-	Name   string
-	Type   string
-	Status string
-	Start  time.Time
-	End    time.Time
-}
-
-type Transactions []Transaction
-
-func (slice Transactions) Len() int {
-	return len(slice)
-}
-
-func (slice Transactions) Less(i, j int) bool {
-	return slice[i].Start.Before(slice[j].Start)
-}
-
-func (slice Transactions) Swap(i, j int) {
-	slice[i], slice[j] = slice[j], slice[i]
-}
-
 func GroupEvents(events Events) (Transactions, error) {
 	name_events := make(map[string][]Event)
 
