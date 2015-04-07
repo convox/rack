9f33e238d4daadb8f02ade9e000d9f2fdc35375a
David Dollar <david@dollar.io>
2015-04-07 10:17:29 -0400

    usage docs

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 	"os"
 )
 
-func main() {
+func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s <repo> <app> [ref]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "builder: turn a convox application into an ami\n\n")
+		fmt.Fprintf(os.Stderr, "Usage: %s <name> <repository> [ref]\n", os.Args[0])
 		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nExample:\n  builder example-sinatra https://github.com/convox-examples/sinatra.git master\n")
 	}
+}
 
+func main() {
 	region := flag.String("region", "us-east-1", "aws region")
 	access := flag.String("access", os.Getenv("AWS_ACCESS"), "aws access id")
 	secret := flag.String("secret", os.Getenv("AWS_SECRET"), "aws secret key")
@@ -30,8 +34,8 @@ func main() {
 	builder.AwsAccess = *access
 	builder.AwsSecret = *secret
 
-	repo := positional(args, 0)
-	app := positional(args, 1)
+	app := positional(args, 0)
+	repo := positional(args, 1)
 	ref := positional(args, 2)
 
 	err := builder.Build(repo, app, ref)
