a0e9d6bf1b9f90e5b685971bada4bf83baf19cb2
David Dollar <david@dollar.io>
2015-05-16 22:29:43 -0400

    camelcase process names with a dash

--- models/helpers.go
+++ models/helpers.go
@@ -257,5 +257,27 @@ func templateHelpers() template.FuncMap {
 }
 
 func upperName(name string) string {
-	return strings.ToUpper(name[0:1]) + name[1:]
+	us := strings.ToUpper(name[0:1]) + name[1:]
+
+	for {
+		i := strings.Index(us, "-")
+
+		if i == -1 {
+			break
+		}
+
+		s := us[0:i]
+
+		if len(us) > i+1 {
+			s += strings.ToUpper(us[i+1 : i+2])
+		}
+
+		if len(us) > i+2 {
+			s += us[i+2:]
+		}
+
+		us = s
+	}
+
+	return us
 }
