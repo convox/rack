06e3f35005f2406d2d8ecb49dcdff61096563246
Noah Zoschke <nzoschke@gmail.com>
2015-06-23 11:12:28 -0700

    test procfile parser

---
+++ convox/start/procfile_test.go
@@ -0,0 +1,45 @@
+package start
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+)
+
+type Cases []struct {
+	got, want interface{}
+}
+
+func TestParseProcfile(t *testing.T) {
+	p, err := parseProcfile([]byte(`web: ruby web.rb`))
+
+	if err != nil {
+		t.Errorf("TestParseProcfile err %q", err)
+	}
+
+	cases := Cases{
+		{p, map[string]string{"web": "ruby web.rb"}},
+	}
+
+	_assert(t, cases)
+}
+
+func _assert(t *testing.T, cases Cases) {
+	for _, c := range cases {
+		j1, err := json.Marshal(c.got)
+
+		if err != nil {
+			t.Errorf("Marshal %q, error %q", c.got, err)
+		}
+
+		j2, err := json.Marshal(c.want)
+
+		if err != nil {
+			t.Errorf("Marshal %q, error %q", c.want, err)
+		}
+
+		if !bytes.Equal(j1, j2) {
+			t.Errorf("Got %q, want %q", c.got, c.want)
+		}
+	}
+}
