65e86c6ea454b8127ca64b12b7a731cab4ea5274
Noah Zoschke <nzoschke@gmail.com>
2015-06-25 16:47:38 -0700

    start calls build then docker-compose up

---
+++ convox/start.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/convox/cli/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/convox/cli/stdcli"
+)
+
+func init() {
+	stdcli.RegisterCommand(cli.Command{
+		Name:        "start",
+		Description: "start an app for local development",
+		Usage:       "<directory>",
+		Action:      cmdStart,
+	})
+}
+
+func cmdStart(c *cli.Context) {
+	base := "."
+
+	if len(c.Args()) > 0 {
+		base = c.Args()[0]
+	}
+
+	base, err := filepath.Abs(base)
+
+	if err != nil {
+		panic(err)
+	}
+
+	cmdBuild(c)
+
+	err = run("docker-compose", "up")
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run(bin string, args ...string) error {
+	cmd := exec.Command(bin, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
