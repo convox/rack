e02b99526d428f5631697f81dc9cf633719ebafc
Noah Zoschke <nzoschke@gmail.com>
2015-06-25 15:02:05 -0700

    apps list and test

---
+++ convox/apps.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/convox/cli/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/convox/cli/stdcli"
+)
+
+type App struct {
+	Name string
+
+	Status     string
+	Repository string
+
+	Outputs    map[string]string
+	Parameters map[string]string
+	Tags       map[string]string
+}
+
+type Apps []App
+
+func init() {
+	stdcli.RegisterCommand(cli.Command{
+		Name:        "apps",
+		Description: "list apps",
+		Action:      cmdApps,
+	})
+}
+
+func cmdApps(c *cli.Context) {
+	data, err := convoxRequest("GET", "/apps")
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	var apps *Apps
+	err = json.Unmarshal(data, &apps)
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	fmt.Println("Apps:")
+
+	for _, app := range *apps {
+		fmt.Printf("  %s\n", app.Name)
+	}
+}
---
+++ convox/apps_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestApps(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apps := make(Apps, 0)
+		apps = append(apps, App{
+			Name: "sinatra",
+		})
+
+		data, _ := json.Marshal(apps)
+		_, _ = w.Write(data)
+	}))
+	defer ts.Close()
+
+	_, _ = appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
+	stdout, stderr := appRun([]string{"convox", "apps"})
+
+	expect(t, stdout, `Apps:
+  sinatra
+`)
+	expect(t, stderr, "")
+}
