4767c1175da4c28655ab2e0a024f214852c23587
Noah Zoschke <nzoschke@gmail.com>
2015-06-25 13:06:51 -0700

    remove sample cli test

--- convox/login_test.go
+++ convox/login_test.go
@@ -1,78 +1,55 @@
 package main
 
 import (
-	"flag"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"testing"
-
-	"github.com/convox/cli/Godeps/_workspace/src/github.com/codegangsta/cli"
+  "net/http"
+  "net/http/httptest"
+  "net/url"
+  "testing"
 )
 
 func TestInvalidLogin(t *testing.T) {
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "unauthorized", 401)
-	}))
-	defer ts.Close()
+  ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "unauthorized", 401)
+  }))
+  defer ts.Close()
 
-	stdout, stderr := appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
+  stdout, stderr := appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
 
-	expect(t, stdout, "")
-	expect(t, stderr, "ERROR: invalid login\n")
+  expect(t, stdout, "")
+  expect(t, stderr, "ERROR: invalid login\n")
 }
 
 func TestLogin(t *testing.T) {
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ok", 200)
-	}))
-	defer ts.Close()
+  ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "ok", 200)
+  }))
+  defer ts.Close()
 
-	stdout, stderr := appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
+  stdout, stderr := appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
 
-	expect(t, stdout, "Login Succeeded\n")
-	expect(t, stderr, "")
+  expect(t, stdout, "Login Succeeded\n")
+  expect(t, stderr, "")
 }
 
 func TestLoginHost(t *testing.T) {
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "ok", 200)
-	}))
-	defer ts.Close()
+  ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "ok", 200)
+  }))
+  defer ts.Close()
 
-	stdout, stderr := appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
+  stdout, stderr := appRun([]string{"convox", "login", "--password", "foobar", ts.URL})
 
-	expect(t, stdout, "Login Succeeded\n")
-	expect(t, stderr, "")
+  expect(t, stdout, "Login Succeeded\n")
+  expect(t, stderr, "")
 
-	u, _ := url.Parse(ts.URL)
-	stdout, stderr = appRun([]string{"convox", "login", "--password", "foobar", u.Host})
+  u, _ := url.Parse(ts.URL)
+  stdout, stderr = appRun([]string{"convox", "login", "--password", "foobar", u.Host})
 
-	expect(t, stdout, "Login Succeeded\n")
-	expect(t, stderr, "")
+  expect(t, stdout, "Login Succeeded\n")
+  expect(t, stderr, "")
 
-	stdout, stderr = appRun([]string{"convox", "login", "--password", "foobar", "BAD"})
+  stdout, stderr = appRun([]string{"convox", "login", "--password", "foobar", "BAD"})
 
-	expect(t, stdout, "")
-	expect(t, stderr, "ERROR: Get https://BAD/apps: dial tcp: lookup BAD: no such host\n")
-}
-
-func TestCommandDoNotIgnoreFlags(t *testing.T) {
-	app := cli.NewApp()
-	set := flag.NewFlagSet("test", 0)
-	test := []string{"blah", "blah", "-break"}
-	set.Parse(test)
-
-	c := cli.NewContext(app, set, nil)
-
-	command := cli.Command{
-		Name:        "test-cmd",
-		Aliases:     []string{"tc"},
-		Usage:       "this is for testing",
-		Description: "testing",
-		Action:      func(_ *cli.Context) {},
-	}
-	err := command.Run(c)
-
-	expect(t, err.Error(), "flag provided but not defined: -break")
+  expect(t, stdout, "")
+  expect(t, stderr, "ERROR: Get https://BAD/apps: dial tcp: lookup BAD: no such host\n")
 }
