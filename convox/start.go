e6db4b063da39893f38d63c48e8fc4752549dc09
David Dollar <david@dollar.io>
2015-07-06 11:53:55 -0700

    use convox/build

--- convox/start.go
+++ convox/start.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
 	"github.com/convox/cli/Godeps/_workspace/src/github.com/codegangsta/cli"
 	"github.com/convox/cli/stdcli"
 )
@@ -15,11 +21,82 @@ func init() {
 }
 
 func cmdStart(c *cli.Context) {
-	cmdBuild(c)
+	wd := "."
 
-	err := stdcli.Run("docker-compose", "up")
+	if len(c.Args()) > 0 {
+		wd = c.Args()[0]
+	}
+
+	dir, app, err := stdcli.DirApp(c, wd)
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	err = buildLocal(dir, app)
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	err = stdcli.Run("docker-compose", "up")
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+func buildLocal(dir, app string) error {
+	abs, err := filepath.Abs(dir)
+
+	if err != nil {
+		return err
+	}
+
+	err = run("docker", "run", "-i", "-v", "/var/run/docker.sock:/var/run/docker.sock", "-v", fmt.Sprintf("%s:/source", abs), "convox/build", app, "/source")
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func run(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+
+	if err != nil {
+		return err
+	}
+
+	cmd.Start()
+
+	scanner := bufio.NewScanner(stdout)
+
+	for scanner.Scan() {
+		parts := strings.SplitN(scanner.Text(), "|", 2)
+
+		if len(parts) == 2 {
+			switch parts[0] {
+			case "build", "compose":
+				fmt.Println(parts[1])
+			case "manifest":
+			default:
+				fmt.Println(scanner.Text())
+			}
+		}
+	}
+
+	err = cmd.Wait()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
