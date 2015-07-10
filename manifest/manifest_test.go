336f4b126ee170434bc9293c0fe0a87d113f9ad4
Noah Zoschke <nzoschke@gmail.com>
2015-07-10 12:20:45 -0700

    debug test working directory

--- manifest/manifest_test.go
+++ manifest/manifest_test.go
@@ -1,30 +1,32 @@
 package manifest
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"testing"
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "os/exec"
+  "path/filepath"
+  "testing"
 )
 
 type Cases []struct {
-	got, want interface{}
+  got, want interface{}
 }
 
 func TestDockerCompose(t *testing.T) {
-	wd, _ := os.Getwd()
-	defer os.Chdir(wd)
+  wd, _ := os.Getwd()
+  fmt.Printf("WD: %v\n", wd)
+  defer os.Chdir(wd)
 
-	destDir := mkBuildDir(t, "../examples/docker-compose/")
-	defer os.RemoveAll(destDir)
+  destDir := mkBuildDir(t, "../examples/docker-compose/")
+  defer os.RemoveAll(destDir)
 
-	m, _ := Generate(destDir)
+  m, _ := Generate(destDir)
 
-	cases := Cases{
-		{readFile(t, destDir, "docker-compose.yml"), `web:
+  cases := Cases{
+    {readFile(t, destDir, "docker-compose.yml"), `web:
   build: .
   links:
     - postgres
@@ -35,44 +37,44 @@ func TestDockerCompose(t *testing.T) {
 postgres:
   image: convox/postgres
 `},
-		{[]string{"postgres", "web"}, m.runOrder()},
-	}
+    {[]string{"postgres", "web"}, m.runOrder()},
+  }
 
-	_assert(t, cases)
+  _assert(t, cases)
 }
 
 func TestDockerfile(t *testing.T) {
-	wd, _ := os.Getwd()
-	defer os.Chdir(wd)
+  wd, _ := os.Getwd()
+  defer os.Chdir(wd)
 
-	destDir := mkBuildDir(t, "../examples/dockerfile/")
-	defer os.RemoveAll(destDir)
+  destDir := mkBuildDir(t, "../examples/dockerfile/")
+  defer os.RemoveAll(destDir)
 
-	m, _ := Generate(destDir)
+  m, _ := Generate(destDir)
 
-	cases := Cases{
-		{readFile(t, destDir, "docker-compose.yml"), `main:
+  cases := Cases{
+    {readFile(t, destDir, "docker-compose.yml"), `main:
   build: .
   ports:
   - 5000:3000
 `},
-		{[]string{"main"}, m.runOrder()},
-	}
+    {[]string{"main"}, m.runOrder()},
+  }
 
-	_assert(t, cases)
+  _assert(t, cases)
 }
 
 func TestProcfile(t *testing.T) {
-	wd, _ := os.Getwd()
-	defer os.Chdir(wd)
+  wd, _ := os.Getwd()
+  defer os.Chdir(wd)
 
-	destDir := mkBuildDir(t, "../examples/procfile/")
-	defer os.RemoveAll(destDir)
+  destDir := mkBuildDir(t, "../examples/procfile/")
+  defer os.RemoveAll(destDir)
 
-	m, _ := Generate(destDir)
+  m, _ := Generate(destDir)
 
-	cases := Cases{
-		{readFile(t, destDir, "docker-compose.yml"), `web:
+  cases := Cases{
+    {readFile(t, destDir, "docker-compose.yml"), `web:
   build: .
   command: ruby web.rb
   ports:
@@ -83,59 +85,59 @@ worker:
   ports:
   - 5100:3000
 `},
-		{[]string{"web", "worker"}, m.runOrder()},
-	}
+    {[]string{"web", "worker"}, m.runOrder()},
+  }
 
-	_assert(t, cases)
+  _assert(t, cases)
 }
 
 func mkBuildDir(t *testing.T, srcDir string) string {
-	destDir, err := ioutil.TempDir("", "")
+  destDir, err := ioutil.TempDir("", "")
 
-	if err != nil {
-		t.Errorf("ERROR mkBuildDir %v %v", srcDir, err)
-		return destDir
-	}
+  if err != nil {
+    t.Errorf("ERROR mkBuildDir %v %v", srcDir, err)
+    return destDir
+  }
 
-	cpCmd := exec.Command("cp", "-rf", srcDir, destDir)
-	err = cpCmd.Run()
+  cpCmd := exec.Command("cp", "-rf", srcDir, destDir)
+  err = cpCmd.Run()
 
-	if err != nil {
-		t.Errorf("ERROR mkBuildDir %v %v", srcDir, err)
-		return destDir
-	}
+  if err != nil {
+    t.Errorf("ERROR mkBuildDir %v %v", srcDir, err)
+    return destDir
+  }
 
-	return destDir
+  return destDir
 }
 
 func readFile(t *testing.T, dir string, name string) string {
-	filename := filepath.Join(dir, name)
+  filename := filepath.Join(dir, name)
 
-	dat, err := ioutil.ReadFile(filename)
+  dat, err := ioutil.ReadFile(filename)
 
-	if err != nil {
-		t.Errorf("ERROR readFile %v %v", filename, err)
-	}
+  if err != nil {
+    t.Errorf("ERROR readFile %v %v", filename, err)
+  }
 
-	return string(dat)
+  return string(dat)
 }
 
 func _assert(t *testing.T, cases Cases) {
-	for _, c := range cases {
-		j1, err := json.Marshal(c.got)
+  for _, c := range cases {
+    j1, err := json.Marshal(c.got)
 
-		if err != nil {
-			t.Errorf("Marshal %q, error %q", c.got, err)
-		}
+    if err != nil {
+      t.Errorf("Marshal %q, error %q", c.got, err)
+    }
 
-		j2, err := json.Marshal(c.want)
+    j2, err := json.Marshal(c.want)
 
-		if err != nil {
-			t.Errorf("Marshal %q, error %q", c.want, err)
-		}
+    if err != nil {
+      t.Errorf("Marshal %q, error %q", c.want, err)
+    }
 
-		if !bytes.Equal(j1, j2) {
-			t.Errorf("Got %q, want %q", c.got, c.want)
-		}
-	}
+    if !bytes.Equal(j1, j2) {
+      t.Errorf("Got %q, want %q", c.got, c.want)
+    }
+  }
 }
