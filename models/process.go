03a44bc51d6a596f4b86c819c61b57541c074a66
Noah Zoschke <nzoschke@gmail.com>
2015-07-28 17:05:05 +0200

    list tasks for family, dont filter any out

--- models/process.go
+++ models/process.go
@@ -37,6 +37,7 @@ type ProcessRunOptions struct {
 func ListProcesses(app string) (Processes, error) {
 	req := &ecs.ListTasksInput{
 		Cluster: aws.String(os.Getenv("CLUSTER")),
+		Family: aws.String(app),
 	}
 
 	res, err := ECS().ListTasks(req)
@@ -55,14 +56,7 @@ func ListProcesses(app string) (Processes, error) {
 	pss := Processes{}
 
 	for _, task := range tres.Tasks {
-		parts := strings.Split(*task.TaskDefinitionARN, "/")
-		fam := parts[1]
-
-		if !strings.HasPrefix(fam, fmt.Sprintf("%s-", app)) {
-			continue
-		}
-
-		parts = strings.Split(*task.TaskARN, "-")
+		parts := strings.Split(*task.TaskARN, "-")
 		id := parts[len(parts)-1]
 
 		tres, err := ECS().DescribeTaskDefinition(&ecs.DescribeTaskDefinitionInput{
