5ec405a9d397fe03f152c2affdeb7969cc887aba
Nguyen Dang Minh <nguyendangminh@gmail.com>
2015-08-14 13:57:48 +0700

    Make 'convox info' be a subcommand of 'apps'

--- convox/apps.go
+++ convox/apps.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"regexp"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/convox/cli/Godeps/_workspace/src/github.com/codegangsta/cli"
@@ -46,6 +49,12 @@ func init() {
 				Description: "delete an application",
 				Usage:       "<name>",
 				Action:      cmdAppDelete,
+			},
+			{
+				Name:        "info",
+				Description: "see info about an app",
+				Usage:       "<name>",
+				Action:      cmdAppInfo,
 			},
 		},
 	})
@@ -152,6 +161,78 @@ func cmdAppDelete(c *cli.Context) {
 	}
 
 	fmt.Println("OK")
+}
+
+func cmdAppInfo(c *cli.Context) {
+	_, app, err := stdcli.DirApp(c, ".")
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	if len(c.Args()) > 0 {
+		app = c.Args()[0]
+	}
+
+	data, err := ConvoxGet("/apps/" + app)
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	var a *App
+	err = json.Unmarshal(data, &a)
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	matcher := regexp.MustCompile(`^(\w+)Port\d+Balancer`)
+
+	ports := []string{}
+
+	for key, value := range a.Outputs {
+		if m := matcher.FindStringSubmatch(key); m != nil {
+			ports = append(ports, fmt.Sprintf("%s:%s", strings.ToLower(m[1]), value))
+		}
+	}
+
+	processes := []string{}
+
+	for key, _ := range a.Parameters {
+		if strings.HasSuffix(key, "Image") {
+			processes = append(processes, strings.ToLower(key[0:len(key)-5]))
+		}
+	}
+
+	sort.Strings(processes)
+
+	if len(processes) == 0 {
+		processes = append(processes, "(none)")
+	}
+
+	if len(ports) == 0 {
+		ports = append(ports, "(none)")
+	}
+
+	release := a.Parameters["Release"]
+
+	if release == "" {
+		release = "(none)"
+	}
+
+	fmt.Printf("Name       %s\n", a.Name)
+	fmt.Printf("Status     %s\n", a.Status)
+	fmt.Printf("Release    %s\n", release)
+	fmt.Printf("Processes  %s\n", strings.Join(processes, " "))
+
+	if a.Outputs["BalancerHost"] != "" {
+		fmt.Printf("Hostname   %s\n", a.Outputs["BalancerHost"])
+		fmt.Printf("Ports      %s\n", strings.Join(ports, " "))
+	}
 }
 
 func getApps() *Apps {
