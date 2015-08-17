b73046abdfd0436c8c75abcb0cd967f52d62ffb9
Noah Zoschke <nzoschke@gmail.com>
2015-08-17 11:22:20 -0700

    dont exit if config file doesnt exist; create new one

--- convox/login.go
+++ convox/login.go
@@ -182,11 +182,7 @@ func upgradeConfig() error {
 func addLogin(host, password string) error {
 	config := filepath.Join(ConfigRoot, "auth")
 
-	data, err := ioutil.ReadFile(filepath.Join(config))
-
-	if err != nil {
-		return err
-	}
+	data, _ := ioutil.ReadFile(filepath.Join(config))
 
 	if data == nil {
 		data = []byte("{}")
@@ -194,7 +190,7 @@ func addLogin(host, password string) error {
 
 	var auth ConfigAuth
 
-	err = json.Unmarshal(data, &auth)
+	err := json.Unmarshal(data, &auth)
 
 	if err != nil {
 		return err
