46039fc7e2b1f992ac9d54c3601ab7070dcc62a6
Noah Zoschke <nzoschke@gmail.com>
2015-08-17 11:08:20 -0700

    Revert "remove uninstall force flag"
    This reverts commit b6c23b2372406aba6a619383439e46ec61edc109.

--- convox/install.go
+++ convox/install.go
@@ -82,6 +82,10 @@ func init() {
 		Usage:       "",
 		Action:      cmdUninstall,
 		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:  "force",
+				Usage: "uninstall even if apps exist",
+			},
 			cli.StringFlag{
 				Name:   "region",
 				Value:  "us-east-1",
@@ -247,6 +251,14 @@ func cmdInstall(c *cli.Context) {
 }
 
 func cmdUninstall(c *cli.Context) {
+	if !c.Bool("force") {
+		apps := getApps()
+
+		if len(*apps) != 0 {
+			stdcli.Error(fmt.Errorf("Please delete all apps before uninstalling."))
+		}
+	}
+
 	fmt.Println(`
 
      ___    ___     ___   __  __    ___   __  _
