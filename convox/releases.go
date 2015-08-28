8f6917bacc59ad545bc58406c001d4b3578bf493
David Dollar <david@dollar.io>
2015-08-27 19:58:30 -0700

    releases info

--- convox/releases.go
+++ convox/releases.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/convox/cli/Godeps/_workspace/src/github.com/codegangsta/cli"
@@ -38,6 +39,18 @@ func init() {
 			},
 		},
 		Subcommands: []cli.Command{
+			{
+				Name:        "info",
+				Description: "see info about a release",
+				Usage:       "<release id>",
+				Action:      cmdReleaseInfo,
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "app",
+						Usage: "app name. Inferred from current directory if not specified.",
+					},
+				},
+			},
 			{
 				Name:        "promote",
 				Description: "promote a release",
@@ -97,6 +110,44 @@ func cmdReleases(c *cli.Context) {
 
 		fmt.Printf("%-12s  %-12s  %-12s\n", r.Id, humanize.Time(r.Created), active)
 	}
+}
+
+func cmdReleaseInfo(c *cli.Context) {
+	if len(c.Args()) < 1 {
+		stdcli.Usage(c, "release info")
+		return
+	}
+
+	release := c.Args()[0]
+
+	_, app, err := stdcli.DirApp(c, ".")
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	data, err := ConvoxGet(fmt.Sprintf("/apps/%s/releases/%s", app, release))
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	var r Release
+
+	err = json.Unmarshal(data, &r)
+
+	if err != nil {
+		stdcli.Error(err)
+		return
+	}
+
+	fmt.Printf("Id       %s\n", r.Id)
+	fmt.Printf("Build    %s\n", r.Build)
+	fmt.Printf("Created  %s\n", r.Created)
+	fmt.Printf("Env      ")
+	fmt.Println(strings.Replace(r.Env, "\n", "\n         ", -1))
 }
 
 func cmdReleasePromote(c *cli.Context) {
