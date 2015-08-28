e8a7ad9fb9cd04efa952a7f35af433c65a28373d
Noah Zoschke <nzoschke@gmail.com>
2015-08-28 12:33:48 -0700

    Godeps for docker/pkg/term and unused aws packages

--- Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws/request_pagination_test.go
+++ Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws/request_pagination_test.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/internal/test/unit"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/convox/cli/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
-	"github.com/convox/cli/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/s3"
 	"github.com/stretchr/testify/assert"
 )
 
