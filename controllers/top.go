a86b12d6ccec2c0856b8b2df690ae10dddd05dd9
Matthew Manning <matt.manning@gmail.com>
2015-08-28 22:18:56 -0700

    get cluster name from cloudformation outputs

--- controllers/top.go
+++ controllers/top.go
@@ -1,17 +1,37 @@
 package controllers
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/convox/kernel/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws"
+	"github.com/convox/kernel/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/service/cloudformation"
 	"github.com/convox/kernel/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/service/cloudwatch"
 	"github.com/convox/kernel/models"
 )
 
 func ClusterTop(rw http.ResponseWriter, r *http.Request) {
+	name := aws.String(os.Getenv("RACK"))
+
+	res, err := models.CloudFormation().DescribeStacks(&cloudformation.DescribeStacksInput{StackName: name})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	stack := res.Stacks[0]
+
+	outputs := make(map[string]string)
+
+	for _, output := range stack.Outputs {
+		outputs[*output.OutputKey] = *output.OutputValue
+	}
+
+	cluster := outputs["Cluster"]
+
 	params := &cloudwatch.GetMetricStatisticsInput{
 		MetricName: aws.String("CPUUtilization"),
 		StartTime:  aws.Time(time.Now().Add(-2 * time.Minute)),
@@ -25,7 +45,7 @@ func ClusterTop(rw http.ResponseWriter, r *http.Request) {
 		Dimensions: []*cloudwatch.Dimension{
 			{
 				Name:  aws.String("ClusterName"),
-				Value: aws.String("convo-Clust-1VY3S3P89LZUN"),
+				Value: aws.String(cluster),
 			},
 		},
 	}
