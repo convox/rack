b141539e09e76d2ca80609fd6c49d0a21cbca27b
csquared <christopher.continanza@gmail.com>
2015-09-10 01:58:20 -0700

    only report when set

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -19,28 +19,32 @@ func Error(log *logger.Logger, err error) {
 	if log != nil {
 		log.Error(err)
 	}
-	extraData := map[string]string{
-		"AWS_REGION": os.Getenv("AWS_REGION"),
-		"RACK":       os.Getenv("RACK"),
-		"RELEASE":    os.Getenv("RELEASE"),
-		"VPC":        os.Getenv("VPC"),
-	}
-	extraField := &rollbar.Field{"env", extraData}
 
-	rollbar.Error(rollbar.ERR, err, extraField)
+	if rollbar.Token != "" {
+		extraData := map[string]string{
+			"AWS_REGION": os.Getenv("AWS_REGION"),
+			"RACK":       os.Getenv("RACK"),
+			"RELEASE":    os.Getenv("RELEASE"),
+			"VPC":        os.Getenv("VPC"),
+		}
+		extraField := &rollbar.Field{"env", extraData}
+		rollbar.Error(rollbar.ERR, err, extraField)
+	}
 }
 
 func SendMixpanelEvent(event, message string) {
 	id := os.Getenv("CLIENT_ID")
 	token := os.Getenv("MIXPANEL_TOKEN")
-	release := os.Getenv("RELEASE")
+	if token != "" {
+		release := os.Getenv("RELEASE")
 
-	m := fmt.Sprintf(`{"event": %q, "properties": {"client_id": %q, "distinct_id": %q, "message": %q, "release": %q, "token": %q}}`, event, id, id, message, release, token)
-	encMessage := base64.StdEncoding.EncodeToString([]byte(m))
+		m := fmt.Sprintf(`{"event": %q, "properties": {"client_id": %q, "distinct_id": %q, "message": %q, "release": %q, "token": %q}}`, event, id, id, message, release, token)
+		encMessage := base64.StdEncoding.EncodeToString([]byte(m))
 
-	_, err := http.Get(fmt.Sprintf("http://api.mixpanel.com/track/?data=%s", encMessage))
+		_, err := http.Get(fmt.Sprintf("http://api.mixpanel.com/track/?data=%s", encMessage))
 
-	if err != nil {
-		Error(nil, err)
+		if err != nil {
+			Error(nil, err)
+		}
 	}
 }
