fe540c791b7d6d4bb3e1cda493add31fb05cec9f
Noah Zoschke <nzoschke@gmail.com>
2015-12-15 17:26:31 -0800

    remove dead code

--- api/models/resource.go
+++ api/models/resource.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"strings"
 	"time"
 
 	"github.com/convox/rack/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
@@ -22,13 +21,15 @@ type Resource struct {
 type Resources map[string]Resource
 
 func ListResources(app string) (Resources, error) {
-	res, err := CloudFormation().DescribeStackResources(&cloudformation.DescribeStackResourcesInput{StackName: aws.String(app)})
+	res, err := CloudFormation().DescribeStackResources(&cloudformation.DescribeStackResourcesInput{
+		StackName: aws.String(app),
+	})
 
 	if err != nil {
 		return nil, err
 	}
 
-	resources := make(Resources)
+	resources := make(Resources, len(res.StackResources))
 
 	for _, r := range res.StackResources {
 		resources[*r.LogicalResourceId] = Resource{
@@ -38,26 +39,6 @@ func ListResources(app string) (Resources, error) {
 			Status: cs(r.ResourceStatus, ""),
 			Type:   cs(r.ResourceType, ""),
 			Time:   ct(r.Timestamp),
-		}
-	}
-
-	return resources, nil
-}
-
-func ListProcessResources(app, process string) (Resources, error) {
-	res, err := ListResources(app)
-
-	if err != nil {
-		return nil, err
-	}
-
-	resources := make(Resources)
-
-	prefix := UpperName(process)
-
-	for name, resource := range res {
-		if strings.HasPrefix(name, prefix) {
-			resources[name] = resource
 		}
 	}
 
