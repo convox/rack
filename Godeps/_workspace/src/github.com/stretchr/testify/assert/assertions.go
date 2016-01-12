df13c9a59a3d3a4b0ed08fa240f7941996f02cf6
Noah Zoschke <nzoschke@gmail.com>
2016-01-12 15:19:47 -0800

    more godeps

--- Godeps/_workspace/src/github.com/stretchr/testify/assert/assertions.go
+++ Godeps/_workspace/src/github.com/stretchr/testify/assert/assertions.go
@@ -14,8 +14,8 @@ import (
 	"unicode"
 	"unicode/utf8"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/pmezard/go-difflib/difflib"
+	"github.com/convox/rack/Godeps/_workspace/src/github.com/davecgh/go-spew/spew"
+	"github.com/convox/rack/Godeps/_workspace/src/github.com/pmezard/go-difflib/difflib"
 )
 
 // TestingT is an interface wrapper around *testing.T
