977f0cf806387666e0d075ca9dbf5f1d9b1eccd9
csquared <christopher.continanza@gmail.com>
2016-01-22 16:49:42 -0800

    use convox binary

--- ci/delete-all-apps.sh
+++ ci/delete-all-apps.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x
 
-for app_name in $(cx api get /apps | jq '.[].name'); do
+for app_name in $(convox api get /apps | jq -r '.[].name'); do
   convox apps delete $app_name
 
   while convox apps info --app $app_name | grep -i deleting; do
     echo "app deleting"
     sleep 10
   done
+
+  sleep 5
 done
