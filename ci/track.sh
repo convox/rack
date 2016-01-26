b270e120db23b6948e70051f4c2be8fdf749595d
Noah Zoschke <nzoschke@gmail.com>
2016-01-26 15:52:59 -0500

    wrap all test command with segment track script

---
+++ ci/track.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Run the args, and track the command, exit code and time elapsed
+#
+# usage:
+#  track delete-all-apps.sh
+#  track echo hello
+#  track false
+
+SECONDS=0
+
+track(){
+  curl -vi https://api.segment.io/v1/track      \
+    -H "Content-Type: application/json" -X POST \
+    --user $SEGMENT_WRITE_KEY:                  \
+    -d "{
+      \"userId\": \"circleci\",
+      \"event\": \"command\",
+      \"properties\": {
+        \"cmd\": \"$1\",
+        \"code\": $2,
+        \"seconds\": $3
+      }
+    }"
+}
+
+set -x
+
+"$@"
+
+track $1 $? $SECONDS
