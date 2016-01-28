3610282d2666a03096fa7bff13135282c423890c
Noah Zoschke <nzoschke@gmail.com>
2016-01-28 15:05:31 -0500

    refactor getting log groups and cf events; move resource deletion to very end

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -18,6 +18,38 @@ case $CIRCLE_NODE_INDEX in
   ;;
 esac
 
+
+# Save ECS Artifacts
+aws ecs list-clusters | tee $CIRCLE_ARTIFACTS/list-clusters.json
+aws ecs describe-clusters --clusters $(jq -r '.clusterArns[]' $CIRCLE_ARTIFACTS/list-clusters.json) | tee $CIRCLE_ARTIFACTS/describe-clusters.json
+
+for cluster in $(jq -r '.clusters[].clusterName' $CIRCLE_ARTIFACTS/describe-clusters.json); do
+  aws ecs list-services     --cluster $cluster | tee $CIRCLE_ARTIFACTS/list-services-$cluster.json
+  aws ecs describe-services --cluster $cluster --services $(jq -r '.serviceArns[]' $CIRCLE_ARTIFACTS/list-services-$cluster.json) | tee $CIRCLE_ARTIFACTS/describe-services-$cluster.json
+done
+
+# Save CloudWatch Logs Artifacts
+aws logs describe-log-groups | tee $CIRCLE_ARTIFACTS/describe-log-groups.json
+
+for groupName in $(jq -r ".logGroups[] | select(.logGroupName | contains(\"-$CIRCLE_BUILD_NUM-\")) | .logGroupName" $CIRCLE_ARTIFACTS/describe-log-groups.json); do
+  aws logs describe-log-streams --log-group-name $groupName | tee $CIRCLE_ARTIFACTS/describe-log-streams-${groupName//\//-}.json
+
+  for streamName in $(jq -r '.logStreams[].logStreamName' $CIRCLE_ARTIFACTS/describe-log-streams-${groupName//\//-}.json); do
+    aws logs get-log-events --log-group-name $groupName --log-stream-name $streamName | jq '.events[]' | tee -a $CIRCLE_ARTIFACTS/get-log-events-${groupName//\//-}-${streamName//\//-}-unsorted.json
+  done
+
+  jq -s 'sort_by(.timestamp)' $CIRCLE_ARTIFACTS/get-log-events-${groupName//\//-}-${streamName//\//-}-unsorted.json > $CIRCLE_ARTIFACTS/get-log-events-${groupName//\//-}-${streamName//\//-}.json
+done
+
+
+# Save CF Artifacts
+aws cloudformation list-stacks | tee $CIRCLE_ARTIFACTS/list-stacks.json
+
+for stackId in $(jq -r ".StackSummaries[] | select(.StackId | contains(\"-$CIRCLE_BUILD_NUM\")) | .StackId" $CIRCLE_ARTIFACTS/list-stacks.json); do
+  aws cloudformation describe-stacks       --stack-name $stackId | tee $CIRCLE_ARTIFACTS/describe-stacks-${stackId//\//-}.json
+  aws cloudformation describe-stack-events --stack-name $stackId | tee $CIRCLE_ARTIFACTS/describe-stack-events-${stackId//\//-}.json
+done
+
 # Clean leaked S3 Buckets, Repositories and Log Groups
 aws s3api list-buckets |\
   jq ".Buckets[] | select(.Name | contains(\"-$CIRCLE_BUILD_NUM-\")) | .Name" |\
@@ -30,40 +62,3 @@ aws ecr describe-repositories |\
 aws logs describe-log-groups |\
   jq ".logGroups[] | select(.logGroupName | contains(\"-$CIRCLE_BUILD_NUM-\")) | .logGroupName" |\
   xargs -L1 aws logs delete-log-group --log-group-name
-
-# Save ECS Artifacts
-aws ecs list-clusters | tee $CIRCLE_ARTIFACTS/list-clusters.json
-aws ecs describe-clusters --clusters $(jq -r '.clusterArns[]' $CIRCLE_ARTIFACTS/list-clusters.json) | tee $CIRCLE_ARTIFACTS/describe-clusters.json
-
-for cluster in $(jq -r '.clusters[].clusterName' $CIRCLE_ARTIFACTS/describe-clusters.json); do
-  aws ecs list-services     --cluster $cluster | tee $CIRCLE_ARTIFACTS/list-services-$cluster.json
-  aws ecs describe-services --cluster $cluster --services $(jq -r '.serviceArns[]' $CIRCLE_ARTIFACTS/list-services-$cluster.json) | tee $CIRCLE_ARTIFACTS/describe-services-$cluster.json
-done
-
-# Save Lambda Artifacts
-aws logs describe-log-groups --log-group-name-prefix /aws/lambda/convox-$CIRCLE_BUILD_NUM | tee $CIRCLE_ARTIFACTS/describe-log-groups.json
-groupName=$(jq -r '.logGroups[].logGroupName' $CIRCLE_ARTIFACTS/describe-log-groups.json)
-aws logs describe-log-streams --log-group-name $groupName | tee $CIRCLE_ARTIFACTS/describe-log-streams.json
-
-for streamName in $(jq -r '.logStreams[].logStreamName' $CIRCLE_ARTIFACTS/describe-log-streams.json); do
-  aws logs get-log-events --log-group-name $groupName --log-stream-name $streamName | jq '.events[]' | tee -a $CIRCLE_ARTIFACTS/get-log-events-unsorted.json
-done
-
-jq -s 'sort_by(.timestamp)' $CIRCLE_ARTIFACTS/get-log-events-unsorted.json > $CIRCLE_ARTIFACTS/get-log-events.json
-
-# Save CF Artifacts
-
-# describe possible orphan kernel and app stacks from latest build
-for stack in $(aws cloudformation list-stacks | jq -r ".StackSummaries[] | select(.StackName | endswith(\"-$CIRCLE_BUILD_NUM\")) | .StackName"); do
-  aws cloudformation describe-stacks       --stack-name $stack | tee $CIRCLE_ARTIFACTS/describe-stacks-$stack.json
-  aws cloudformation describe-stack-events --stack-name $stack | tee $CIRCLE_ARTIFACTS/describe-stack-events-$stack.json
-done
-
-# describe possible DELETE_COMPLETE kernel and app stacks from latest build
-aws cloudformation list-stacks --stack-status-filter DELETE_COMPLETE | jq -r ".StackSummaries[] | select(.StackName | endswith(\"-$CIRCLE_BUILD_NUM\"))" | tee $CIRCLE_ARTIFACTS/list-stacks-delete-complete.json
-
-for stack in $(jq -r '.StackName' $CIRCLE_ARTIFACTS/list-stacks-delete-complete.json); do
-  stackId=$(jq -r "select (.StackName==\"$stack\") | .StackId" $CIRCLE_ARTIFACTS/list-stacks-delete-complete.json)
-  aws cloudformation describe-stacks       --stack-name $stackId | tee $CIRCLE_ARTIFACTS/describe-stacks-$stack.json
-  aws cloudformation describe-stack-events --stack-name $stackId | tee $CIRCLE_ARTIFACTS/describe-stack-events-$stack.json
-done
