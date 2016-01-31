b12d8d74426d6aa0cb693e354520e87118d74151
Noah Zoschke <nzoschke@gmail.com>
2016-01-31 11:36:34 -0800

    refactor: rename monitor.Listen() to monitor.Containers()

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ var MONITOR_INTERVAL = 5 * time.Minute
 func main() {
 	monitor := NewMonitor()
 
+	go monitor.Containers()
 	go monitor.Disk()
 	go monitor.Dmesg()
-	monitor.Listen()
+
+	for {
+		time.Sleep(60 * time.Second)
+	}
 }
