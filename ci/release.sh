d95f36d17586d0edbbb8706ebc32ec0b0af62584
Noah Zoschke <nzoschke@gmail.com>
2016-02-01 10:49:45 -0800

    debug publish call

--- ci/release.sh
+++ ci/release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 
-curl -k -X POST $RELEASE_URL/publish -d token=$RELEASE_TOKEN -d version=$VERSION
+curl -vik -X POST $RELEASE_URL/publish -d token=$RELEASE_TOKEN -d version=$VERSION
