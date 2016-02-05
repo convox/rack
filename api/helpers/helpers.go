f90b39e5d7fe252809c9be334dc62ffa9c415c1a
Noah Zoschke <nzoschke@gmail.com>
2016-02-05 08:22:30 -0800

    dont report ci signups

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -26,7 +26,7 @@ func init() {
 
 	clientId := os.Getenv("CLIENT_ID")
 
-	if regexpEmail.MatchString(clientId) {
+	if regexpEmail.MatchString(clientId) && clientId != "ci@convox.com" {
 		segment.Identify(&analytics.Identify{
 			UserId: RackId(),
 			Traits: map[string]interface{}{
