70803df37af1409dfdddcefb82d69699cee2123c
David Dollar <david@dollar.io>
2016-02-07 14:57:35 -0500

    split out monitor as singleton process

---
+++ api/cmd/monitor/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+
+	"github.com/convox/rack/api/workers"
+)
+
+func main() {
+	go workers.StartCluster()
+	go workers.StartHeartbeat()
+	go workers.StartImages()
+	go workers.StartServicesCapacity()
+
+	for {
+		time.Sleep(1 * time.Hour)
+	}
+}
