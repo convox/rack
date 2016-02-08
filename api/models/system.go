0a5b71db1cd6244bea3846bddb71ac0e775ecba8
C Anthony Risinger <anthony@xtfx.me>
2016-02-07 18:20:22 -0600

    Update system.go
    Cache `DescribeStacks` call only if err is nil.

--- api/models/system.go
+++ api/models/system.go
@@ -48,7 +48,7 @@ func doDescribeStack(input cloudformation.DescribeStacksInput) (*cloudformation.
 
 		res, err := CloudFormation().DescribeStacks(&input)
 
-		if err != nil {
+		if err == nil {
 			DescribeStacksCache[name] = DescribeStacksResult{
 				Name:        name,
 				Output:      res,
