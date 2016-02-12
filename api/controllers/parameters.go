129fff8da21e129a5233c4bf0e2090d2879ef4a8
David Dollar <david@dollar.io>
2016-02-12 17:06:44 -0500

    remove copypasta

--- api/controllers/parameters.go
+++ api/controllers/parameters.go
@@ -52,35 +52,4 @@ func ParametersSet(rw http.ResponseWriter, r *http.Request) *httperr.Error {
 	}
 
 	return RenderSuccess(rw)
-
-	// vars := mux.Vars(r)
-	// app := vars["app"]
-	// process := vars["process"]
-	// count := GetForm(r, "count")
-	// memory := GetForm(r, "memory")
-
-	// _, err := models.GetApp(app)
-
-	// if awsError(err) == "ValidationError" {
-	//   return httperr.Errorf(404, "no such app: %s", app)
-	// }
-
-	// err = models.SetParameters(app, process, count, memory)
-
-	// if ae, ok := err.(awserr.Error); ok {
-	//   if ae.Code() == "ValidationError" {
-	//     switch {
-	//     case strings.Index(ae.Error(), "No updates are to be performed") > -1:
-	//       return httperr.Errorf(403, "no updates are to be performed: %s", app)
-	//     case strings.Index(ae.Error(), "can not be updated") > -1:
-	//       return httperr.Errorf(403, "app is already updating: %s", app)
-	//     }
-	//   }
-	// }
-
-	// if err != nil {
-	//   return httperr.Server(err)
-	// }
-
-	// return RenderSuccess(rw)
 }
