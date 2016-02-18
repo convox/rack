8236f46ee9358f84102f3464ff475442e085c62f
Noah Zoschke <nzoschke@gmail.com>
2016-02-18 09:32:52 -0800

    delete apps in parallel; wait with timeout for deleted apps to 404 because the stack is really gone

--- ci/delete-all-apps.sh
+++ ci/delete-all-apps.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 set -x
 
-# convox apps doesn't have cli-formatted output
-for app_name in $(convox api get /apps | jq -r '.[].name'); do
-  convox apps delete $app_name
+# Helpers to wait for stack and deployment status
+# Timeout after 10m
+wait_for_app_not_found() {
+  local c=0
 
-  # waiting for app delete
-  while convox apps info --app $app_name | grep -i deleting; do
-    echo "app deleting"
+  sleep 10
+
+  while convox apps info --app $1 2>&1 | grep -v "ERROR: no such app"; do
+    let c=c+1
+    [ $c -gt 30 ] && exit 1
+
     sleep 20
   done
+}
+
+# convox apps doesn't have cli-formatted output
+# kick off deletes
+for app_name in $(convox api get /apps | jq -r '.[].name'); do
+  convox apps delete $app_name
+done
+
+# wait for app to 404 because the stack is gone
+for app_name in $(convox api get /apps | jq -r '.[].name'); do
+  wait_for_app_not_found $app_name
 done
