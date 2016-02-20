b4474f7d3b521e964d3b35d23c45bf1d8b6c0b59
David Dollar <david@dollar.io>
2016-02-20 18:05:11 -0500

    fail ci if "Created Unknown" shows up in the installer log

--- ci/install.sh
+++ ci/install.sh
@@ -19,4 +19,6 @@ case $CIRCLE_NODE_INDEX in
 	;;
 esac
 
-convox install
+convox install | tee $CIRCLE_ARTIFACTS/convox-installer.log
+
+grep "Created Unknown" $CIRCLE_ARTIFACTS/convox-installer.log
