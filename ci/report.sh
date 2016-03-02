79394172ac5a317fc29b7ed49714cdb660e23cc9
Noah Zoschke <nzoschke@gmail.com>
2016-03-02 12:07:44 -0800

    fix region typos

--- ci/report.sh
+++ ci/report.sh
@@ -13,8 +13,8 @@ case $CIRCLE_NODE_INDEX in
   export AWS_REGION=us-west-2
   ;;
   2)
-  export AWS_DEFAULT_REGION=us-west-2
-  export AWS_REGION=us-west-2
+  export AWS_DEFAULT_REGION=eu-west-1
+  export AWS_REGION=eu-west-1
   ;;
   *)
   export AWS_DEFAULT_REGION=us-east-1
