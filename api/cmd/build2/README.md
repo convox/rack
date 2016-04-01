fc64771858c3a73aecb0c0805b2937ebc8dda04a
Noah Zoschke <nzoschke@gmail.com>
2016-04-01 13:42:50 -0700

    hook up build2 to manifest to build and push

---
+++ api/cmd/build2/README.md
@@ -0,0 +1,53 @@
+# build2
+
+Build, tag and push Docker images from an app's source.
+
+Source is either a .tgz snapshot of an app directory or an URL to a git
+repository and optional commit.
+
+This command is designed to run in the convox/rack Docker image which is a
+basic Linux environment with `docker`, `git`, and `ssh` available for extracting
+or cloning the app source.
+
+## Prerequisites
+
+Usage:
+
+```
+$ build2 -
+$ build2 github.com/convox-examples/sinatra.git
+```
+
+Docker Run Flags:
+
+* `-v /var/run/docker.sock:/var/run/docker.sock` so it can acess the Docker daemon and execute `docker build`
+* `-i`
+
+Environment:
+
+* APP - Name of the app we are building for
+* BUILD - Id of the build
+* RACK_HOST - Hostname to call back on build success or failure
+* RACK_PASSWORD - Password to call back on build success or failure
+* REGISTRY_EMAIL - Credentials to `docker push`
+* REGISTRY_USERNAME - Credentials to `docker push`
+* REGISTRY_PASSWORD - Credentials to `docker push`
+* REGISTRY_ADDRESS - Credentials to `docker push`
+* MANIFEST_PATH - Optional path if not docker-compose.yml
+* REPOSITORY - Optional namespace that every image should use
+* NO_CACHE - Option to build without reusing cache
+
+## Development
+
+    # rebuild the API image and build cmd
+
+    $ cd convox/rack
+    $ docker build -t rack/api .
+    ...
+    RUN go install ./...
+
+    # build a tarball on stdin
+
+    $ cd httpd
+    $ tar cz . | docker run -i -v /var/run/docker.sock:/var/run/docker.sock rack/api \
+      build2 httpd -
--- api/cmd/build2/main.go
+++ api/cmd/build2/main.go
@@ -3,12 +3,58 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
-	"github.com/convox/rack/client"
+	"github.com/convox/rack/api/manifest"
 )
 
+var (
+	manifestPath    string
+	app             string
+	cache           = true
+	registryAddress string
+	buildId         string
+	repository      string
+)
+
+func init() {
+	app = os.Getenv("APP")
+	buildId = os.Getenv("BUILD")
+	registryAddress = os.Getenv("REGISTRY_ADDRESS")
+	repository = os.Getenv("REPOSITORY")
+
+	manifestPath = os.Getenv("MANIFEST_PATH")
+	if manifestPath == "" {
+		manifestPath = "docker-compose.yml"
+	}
+
+	if os.Getenv("NO_CACHE") != "" {
+		cache = false
+	}
+}
+
 func main() {
-	fmt.Printf("konichiwa")
+	fmt.Printf("ARGS: %+v\n", os.Args)
+	fmt.Printf("Environ: %+v", os.Environ())
+
+	if len(os.Args) != 2 {
+		fmt.Println("usage: build2 <src>")
+		os.Exit(1)
+	}
+
+	src := os.Args[1]
+
+	if src == "-" {
+		extractTar()
+	} else {
+		cloneGit(src)
+	}
+
+	m, err := manifest.Read("src", manifestPath)
+	handleError(err)
+
+	handleErrors(m.Build(app, "src", cache))
+	handleErrors(m.Push(app, registryAddress, buildId, repository))
 
 	// report back
 	//   manifest
@@ -16,11 +62,43 @@ func main() {
 	//
 	// or error status / reason
 
-	fmt.Printf("Environ: %+v", os.Environ())
+	// c := client.New(os.Getenv("RACK_HOST"), os.Getenv("RACK_PASSWORD"), "build")
+	// _, err := c.UpdateBuild(os.Getenv("APP"), os.Getenv("BUILD"), "web:", "complete", "")
+	// if err != nil {
+	// 	panic(err)
+	// }
+}
 
-	c := client.New(os.Getenv("RACK_HOST"), os.Getenv("RACK_PASSWORD"), "build")
-	_, err := c.UpdateBuild(os.Getenv("APP"), os.Getenv("BUILD"), "web:", "complete", "")
+func handleError(err error) {
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
+}
+
+func handleErrors(errs []error) {
+	for _, err := range errs {
+		handleError(err)
+	}
+}
+
+// buildTar reads a .tgz from stdin, decompresses it, then builds images
+func extractTar() {
+	cwd, err := os.Getwd()
+	handleError(err)
+	defer os.Chdir(cwd)
+
+	handleError(os.MkdirAll("src", 0755))
+
+	handleError(os.Chdir("src"))
+
+	cmd := exec.Command("tar", "xzv")
+	cmd.Stdin = os.Stdin
+	handleError(cmd.Run())
+}
+
+// buildGitURL takes a URL to a git repo with an optional "commit-ish" hash,
+// clones it, checks out the right commit-ish, then builds images
+func cloneGit(url string) {
+
 }
