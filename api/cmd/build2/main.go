8a737064f751e21015cb0deae6190537902a25b6
Noah Zoschke <nzoschke@gmail.com>
2016-03-31 18:43:16 -0700

    build2 uses the client

--- api/cmd/build2/main.go
+++ api/cmd/build2/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/convox/rack/client"
+)
 
 func main() {
 	fmt.Printf("konichiwa")
+
+	// report back
+	//   manifest
+	// success or error
+	//
+	// or error status / reason
+
+	fmt.Printf("Environ: %+v", os.Environ())
+
+	c := client.New(os.Getenv("RACK_HOST"), os.Getenv("RACK_PASSWORD"), "build")
+	_, err := c.UpdateBuild(os.Getenv("APP"), os.Getenv("BUILD"), "web:", "complete", "")
+	if err != nil {
+		panic(err)
+	}
 }
