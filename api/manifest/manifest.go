316799c4fd70e6dc50bd1ec38b0f6f4f4ea6b066
Luke Roberts <email@luke-roberts.co.uk>
2016-04-01 15:49:45 -0700

    retry push and pull

--- api/manifest/manifest.go
+++ api/manifest/manifest.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net"
 	"net/url"
@@ -263,10 +264,24 @@ func (m *Manifest) Build(app, dir string, cache bool) []error {
 	}
 
 	for _, image := range pulls {
-		err := pullSync(image)
+		var pullErr error
+		var backOff = 1
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
 
-		if err != nil {
-			return []error{err}
+		for i := 0; i < 5; i++ {
+			pullErr := pullSync(image)
+			if pullErr == nil {
+				break
+			}
+			log.Println("A pull error occurred...")
+			log.Printf("Retrying in %d seconds...\n", backOff)
+			time.Sleep(time.Duration(backOff) * time.Second)
+			backOff = ((backOff + r1.Intn(10)) * (i + 1))
+		}
+
+		if pullErr != nil {
+			return []error{pullErr}
 		}
 	}
 
@@ -499,10 +514,24 @@ func (m *Manifest) Push(app, registry, tag string, flatten string) []error {
 			remote = fmt.Sprintf("%s/%s:%s", registry, flatten, fmt.Sprintf("%s.%s", name, tag))
 		}
 
-		err := pushSync(local, remote)
+		var pushErr error
+		var backOff = 1
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
 
-		if err != nil {
-			return []error{err}
+		for i := 0; i < 5; i++ {
+			pushErr = pushSync(local, remote)
+			if pushErr == nil {
+				break
+			}
+			log.Println("A push error occurred...")
+			log.Printf("Retrying in %d seconds...\n", backOff)
+			time.Sleep(time.Duration(backOff) * time.Second)
+			backOff = ((backOff + r1.Intn(10)) * (i + 1))
+		}
+
+		if pushErr != nil {
+			return []error{pushErr}
 		}
 	}
 
