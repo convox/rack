c79b260d64633ab95e94d4e65faef939a81e11e4
Noah Zoschke <nzoschke@gmail.com>
2016-04-09 16:02:44 -0700

    libcompose project

--- composure/compose_test.go
+++ composure/compose_test.go
@@ -1,12 +1,13 @@
 package composure
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/convox/rack/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+	"github.com/docker/libcompose/config"
 	"github.com/docker/libcompose/docker"
 	"github.com/docker/libcompose/project"
+	"github.com/docker/libcompose/yaml"
 )
 
 func TestNewProject(t *testing.T) {
@@ -16,8 +17,57 @@ func TestNewProject(t *testing.T) {
 			ProjectName:  "httpd",
 		},
 	})
-
 	assert.Nil(t, err)
 
-	fmt.Printf("PROJECT: %+v\n", project)
+	s, ok := project.Configs.Get("web")
+	assert.True(t, ok)
+	assert.EqualValues(t, &config.ServiceConfig{
+		Build:         "",
+		CapAdd:        []string(nil),
+		CapDrop:       []string(nil),
+		CgroupParent:  "",
+		CPUQuota:      0,
+		CPUSet:        "",
+		CPUShares:     0,
+		Command:       yaml.Command{},
+		ContainerName: "",
+		Devices:       []string(nil),
+		DNS:           yaml.Stringorslice{},
+		DNSSearch:     yaml.Stringorslice{},
+		Dockerfile:    "",
+		DomainName:    "",
+		Entrypoint:    yaml.Command{},
+		EnvFile:       yaml.Stringorslice{},
+		Environment:   yaml.MaporEqualSlice{},
+		Hostname:      "",
+		Image:         "httpd",
+		Labels:        yaml.SliceorMap{},
+		Links:         yaml.MaporColonSlice{},
+		LogDriver:     "",
+		MacAddress:    "",
+		MemLimit:      0,
+		MemSwapLimit:  0,
+		Name:          "",
+		Net:           "",
+		Pid:           "",
+		Uts:           "",
+		Ipc:           "",
+		Ports:         []string{"80:80"},
+		Privileged:    false,
+		Restart:       "",
+		ReadOnly:      false,
+		StdinOpen:     false,
+		SecurityOpt:   []string(nil),
+		Tty:           false,
+		User:          "",
+		VolumeDriver:  "",
+		Volumes:       []string(nil),
+		VolumesFrom:   []string(nil),
+		WorkingDir:    "",
+		Expose:        []string(nil),
+		ExternalLinks: []string(nil),
+		LogOpt:        map[string]string(nil),
+		ExtraHosts:    []string(nil),
+		Ulimits:       yaml.Ulimits{Elements: []yaml.Ulimit(nil)},
+	}, s)
 }
