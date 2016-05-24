13f3866262e4381ef59d98507bfa1f66f55b5c8d
Matthew Manning <matt.manning@gmail.com>
2016-05-24 18:54:49 -0400

    update error message

--- cmd/convox/init.go
+++ cmd/convox/init.go
@@ -40,7 +40,7 @@ func cmdInit(c *cli.Context) {
 
 	// TODO parse the Dockerfile and build a docker-compose.yml
 	if exists("docker-compose.yml") {
-		stdcli.Error(fmt.Errorf("Cannot initialize a project that already contains a Dockerfile or docker-compose.yml"))
+		stdcli.Error(fmt.Errorf("Cannot initialize a project that already contains a docker-compose.yml"))
 		return
 	}
 
