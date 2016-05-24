7690b52b0520dc40015bfc7b65f5dc8ade42fdc7
Noah Zoschke <noah@convox.com>
2016-05-23 17:23:23 -0700

    use credentials to describe all the stacks, collect ones that go with the rack, verify their status, and display helpful confirmations

--- cmd/convox/uninstall.go
+++ cmd/convox/uninstall.go
@@ -17,6 +17,13 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
+type Stack struct {
+	Name      string
+	StackName string
+	Status    string
+	Type      string
+}
+
 func init() {
 	stdcli.RegisterCommand(cli.Command{
 		Name:        "uninstall",
@@ -63,10 +70,96 @@ func cmdUninstall(c *cli.Context) error {
 		return stdcli.ExitError(fmt.Errorf("error reading credentials"))
 	}
 
+	// use credentials to describe CF service, app and rack stacks that belong to the rack name and region
+	CloudFormation := cloudformation.New(session.New(), awsConfig(region, creds))
+	res, err := CloudFormation.DescribeStacks(&cloudformation.DescribeStacksInput{})
+	if err != nil {
+		stdcli.QOSEventSend("cli-uninstall", distinctId, stdcli.QOSEventProperties{Error: err})
+	}
+
+	all := []Stack{}
+	apps := []Stack{}
+	rack := Stack{}
+	services := []Stack{}
+
+	for _, stack := range res.Stacks {
+		outputs := map[string]string{}
+		tags := map[string]string{}
+
+		for _, output := range stack.Outputs {
+			outputs[*output.OutputKey] = *output.OutputValue
+		}
+
+		for _, tag := range stack.Tags {
+			tags[*tag.Key] = *tag.Value
+		}
+
+		name := tags["Name"]
+		if name == "" {
+			name = *stack.StackName
+		}
+
+		s := Stack{
+			Name:      name,
+			StackName: *stack.StackName,
+			Status:    *stack.StackStatus,
+			Type:      tags["Type"],
+		}
+
+		// collect stacks that are explicitly related to the rack
+		if tags["Rack"] == stackName {
+			switch tags["Type"] {
+			case "app":
+				apps = append(apps, s)
+				all = append(all, s)
+			case "service":
+				services = append(services, s)
+				all = append(all, s)
+			}
+		}
+
+		// collect stack that is explicitly the rack
+		if *stack.StackName == stackName && outputs["Rack"] == stackName {
+			rack = s
+			all = append(all, s)
+		}
+	}
+
+	// verify that rack was detected
+	if rack.StackName != stackName {
+		stdcli.Error(fmt.Errorf("Can not find rack named %s. Aborting uninstall.", rack.StackName))
+	}
+
+	fmt.Println("Resources to uninstall:\n")
+
+	// display all the services, apps, then rack
+	t := stdcli.NewTable("STACK", "TYPE", "STATUS")
+
+	for _, s := range services {
+		t.AddRow(s.Name, s.Type, s.Status)
+	}
+
+	for _, s := range apps {
+		t.AddRow(s.Name, s.Type, s.Status)
+	}
+
+	t.AddRow(rack.Name, "rack", rack.Status)
+
+	t.Print()
+	fmt.Println()
+
+	// verify that no stack is being updated
+	for _, s := range all {
+		if strings.HasSuffix(s.Status, "IN_PROGRESS") {
+			stdcli.Error(fmt.Errorf("Can not uninstall while %s is updating. Aborting uninstall.", s.StackName))
+		}
+	}
+
+	// prompt to confirm rack name
 	reader := bufio.NewReader(os.Stdin)
 
 	if !c.Bool("force") && terminal.IsTerminal(int(os.Stdin.Fd())) {
-		fmt.Printf("Confirm stack name to delete: ")
+		fmt.Printf("Confirm rack name to delete all the stacks, apps and rack. WARNING, this is irreversable: ")
 
 		n, err := reader.ReadString('\n')
 		if err != nil {
@@ -90,9 +183,7 @@ func cmdUninstall(c *cli.Context) error {
 		}
 	}()
 
-	CloudFormation := cloudformation.New(session.New(), awsConfig(region, creds))
-
-	res, err := CloudFormation.DescribeStacks(&cloudformation.DescribeStacksInput{
+	res, err = CloudFormation.DescribeStacks(&cloudformation.DescribeStacksInput{
 		StackName: aws.String(stackName),
 	})
 	if err != nil {
