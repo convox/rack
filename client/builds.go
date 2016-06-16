ab988df6e28578c761cf497603733f6b6f4d2d14
Luke Roberts <email@luke-roberts.co.uk>
2016-06-16 17:49:55 +0100

    fix whitespace

--- client/builds.go
+++ client/builds.go
@@ -27,7 +27,6 @@ func (c *Client) GetBuilds(app string) (Builds, error) {
 	var builds Builds
 
 	err := c.Get(fmt.Sprintf("/apps/%s/builds", app), &builds)
-
 	if err != nil {
 		return nil, err
 	}
@@ -39,7 +38,6 @@ func (c *Client) GetBuildsWithLimit(app string, limit int) (Builds, error) {
 	var builds Builds
 
 	err := c.Get(fmt.Sprintf("/apps/%s/builds?limit=%d", app, limit), &builds)
-
 	if err != nil {
 		return nil, err
 	}
@@ -51,7 +49,6 @@ func (c *Client) CreateBuildIndex(app string, index Index, cache bool, manifest 
 	var build Build
 
 	data, err := json.Marshal(index)
-
 	if err != nil {
 		return nil, err
 	}
@@ -64,7 +61,6 @@ func (c *Client) CreateBuildIndex(app string, index Index, cache bool, manifest 
 	}
 
 	err = c.Post(fmt.Sprintf("/apps/%s/builds", app), params, &build)
-
 	if err != nil {
 		return nil, err
 	}
@@ -117,7 +113,6 @@ func (c *Client) GetBuild(app, id string) (*Build, error) {
 	var build Build
 
 	err := c.Get(fmt.Sprintf("/apps/%s/builds/%s", app, id), &build)
-
 	if err != nil {
 		return nil, err
 	}
@@ -163,7 +158,6 @@ func (c *Client) UpdateBuild(app, id, manifest, status, reason string) (*Build, 
 	var build Build
 
 	err := c.Put(fmt.Sprintf("/apps/%s/builds/%s", app, id), params, &build)
-
 	if err != nil {
 		return nil, err
 	}
