04b778dc76c984026d1c6689e9000fa43baa0560
David Dollar <david@dollar.io>
2016-06-27 16:38:12 -0400

    handle non-root build contexts and relative destination dirs in copy
    closes #813 and #814

--- api/manifest/manifest.go
+++ api/manifest/manifest.go
@@ -1054,6 +1054,14 @@ func (me ManifestEntry) syncAdds(app, process string) error {
 		return err
 	}
 
+	wdb, err := Execer("docker", "inspect", "-f", "{{.Config.WorkingDir}}", containerName(app, process)).Output()
+
+	if err != nil {
+		return err
+	}
+
+	wd := strings.TrimSpace(string(wdb))
+
 	for _, line := range strings.Split(string(data), "\n") {
 		parts := strings.Fields(line)
 
@@ -1067,7 +1075,7 @@ func (me ManifestEntry) syncAdds(app, process string) error {
 				continue
 			}
 
-			registerSync(containerName(app, process), parts[1], parts[2])
+			registerSync(containerName(app, process), filepath.Join(me.Build, parts[1]), filepath.Join(wd, parts[2]))
 		}
 	}
 
