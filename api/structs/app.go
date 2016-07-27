187b57a8e8f335e564c60f9052e4439622bc91bb
Miguel Moll <miguel@convox.com>
2016-07-27 11:19:11 -0400

    Prevent unbound apps from being promoted

--- api/structs/app.go
+++ api/structs/app.go
@@ -11,3 +11,18 @@ type App struct {
 }
 
 type Apps []App
+
+func (a *App) IsBound() bool {
+	if a.Tags == nil {
+		// Default to bound.
+		return true
+	}
+
+	if _, ok := a.Tags["Name"]; ok {
+		// Bound apps MUST have a "Name" tag.
+		return true
+	}
+
+	// Tags are present but "Name" tag is not, so we have an unbound app.
+	return false
+}
