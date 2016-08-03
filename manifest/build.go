05f677f8c320c738129049a1fb3dd055e3769cb6
David Dollar <david@dollar.io>
2016-08-03 10:18:05 -0400

    clean up push retry logic, tag failures should not be retried

--- manifest/build.go
+++ manifest/build.go
@@ -2,8 +2,6 @@ package manifest
 
 import (
 	"fmt"
-	"log"
-	"math/rand"
 	"time"
 )
 
@@ -68,7 +66,12 @@ func pushSync(s Stream, local, remote string) error {
 	return run(s, Docker("push", remote))
 }
 
-func (m *Manifest) Push(s Stream, app, registry, tag string, flatten string) []error {
+const (
+	pushRetryLimit = 5
+	pushRetryDelay = 30
+)
+
+func (m *Manifest) Push(s Stream, app, registry, tag string, flatten string) error {
 	if tag == "" {
 		tag = "latest"
 	}
@@ -81,28 +84,20 @@ func (m *Manifest) Push(s Stream, app, registry, tag string, flatten string) []e
 			remote = fmt.Sprintf("%s/%s:%s", registry, flatten, fmt.Sprintf("%s.%s", name, tag))
 		}
 
-		var pushErr error
-		var backOff = 1
-		s1 := rand.NewSource(time.Now().UnixNano())
-		r1 := rand.New(s1)
-
-		for i := 0; i < 5; i++ {
-			if i != 0 {
-				log.Printf("A push error occurred for %s/%s\n", app, name)
-				log.Printf("Retrying in %d seconds...\n", backOff)
-				time.Sleep(time.Duration(backOff) * time.Second)
-				backOff = ((backOff + r1.Intn(10)) * (i))
+		for i := 1; i <= pushRetryLimit; i++ {
+			if err := run(s, Docker("tag", local, remote)); err != nil {
+				return fmt.Errorf("could not tag build: %s", err)
 			}
-			pushErr = pushSync(s, local, remote)
-			if pushErr == nil {
-				break
-			}
-		}
 
-		if pushErr != nil {
-			return []error{pushErr}
+			if err := run(s, Docker("push", remote)); err == nil {
+				continue
+			}
+
+			fmt.Printf("An error occurred while trying to push %s/%s\n", app, name)
+			fmt.Printf("Retrying in %d seconds (attempt %d/%d)\n", pushRetryDelay, i, pushRetryLimit)
+			time.Sleep(pushRetryDelay * time.Second)
 		}
 	}
 
-	return []error{}
+	return nil
 }
