3ddf3b62c44b9f4d967c5c5f686b9cd39cac92e6
David Dollar <david@dollar.io>
2016-08-17 19:50:15 -0400

    formation tests

---
+++ api/controllers/formation_test.go
@@ -0,0 +1,46 @@
+package controllers_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/convox/rack/api/controllers"
+	"github.com/convox/rack/api/models"
+	"github.com/convox/rack/api/structs"
+	"github.com/convox/rack/provider"
+	"github.com/convox/rack/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormationList(t *testing.T) {
+	formation := structs.Formation{
+		structs.ProcessFormation{Name: "web", Count: 2, CPU: 128, Memory: 1024, Ports: []int{3000, 3001}},
+		structs.ProcessFormation{Name: "worker", Count: 3, CPU: 129, Memory: 1025, Ports: []int{4000, 4001}},
+	}
+
+	models.TestProvider = &provider.TestProvider{}
+	models.TestProvider.On("FormationList", "myapp").Return(formation, nil)
+
+	hf := test.NewHandlerFunc(controllers.HandlerFunc)
+
+	if assert.Nil(t, hf.Request("GET", "/apps/myapp/formation", nil)) {
+		hf.AssertCode(t, 200)
+		hf.AssertBodyObject(t, formation)
+	}
+
+	models.TestProvider.AssertExpectations(t)
+}
+
+func TestFormationListError(t *testing.T) {
+	models.TestProvider = &provider.TestProvider{}
+	models.TestProvider.On("FormationList", "myapp").Return(nil, fmt.Errorf("some error"))
+
+	hf := test.NewHandlerFunc(controllers.HandlerFunc)
+
+	if assert.Nil(t, hf.Request("GET", "/apps/myapp/formation", nil)) {
+		hf.AssertCode(t, 500)
+		hf.AssertError(t, "some error")
+	}
+
+	models.TestProvider.AssertExpectations(t)
+}
