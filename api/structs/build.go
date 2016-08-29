fd7a564cc9890c90b3266e9b4aaea4d35efd012e
Miguel Moll <miguel@convox.com>
2016-08-29 09:10:16 -0400

    Build export now part of the provider interface

--- api/structs/build.go
+++ api/structs/build.go
@@ -1,16 +1,8 @@
 package structs
 
 import (
-	"archive/tar"
-	"bytes"
-	"compress/gzip"
-	"encoding/json"
-	"fmt"
 	"math/rand"
-	"os/exec"
 	"time"
-
-	"github.com/convox/rack/manifest"
 )
 
 type Build struct {
@@ -37,76 +29,6 @@ func NewBuild(app string) *Build {
 		Id:     generateId("B", 10),
 		Status: "created",
 	}
-}
-
-// Export returns a build artifact
-func (b *Build) Export(imageRepo string) ([]byte, error) {
-
-	buf := &bytes.Buffer{}
-	gz := gzip.NewWriter(buf)
-	tw := tar.NewWriter(gz)
-
-	m, err := manifest.Load([]byte(b.Manifest))
-	if err != nil {
-		return nil, fmt.Errorf("manifest error: %s", err)
-	}
-
-	if len(m.Services) < 1 {
-		return nil, fmt.Errorf("no services found to export")
-	}
-
-	bjson, err := json.Marshal(b)
-	if err != nil {
-		return nil, err
-	}
-
-	dataHeader := &tar.Header{
-		Typeflag: tar.TypeReg,
-		Name:     "builddata.json",
-		Mode:     0600,
-		Size:     int64(len(bjson)),
-	}
-
-	if err := tw.WriteHeader(dataHeader); err != nil {
-		return nil, err
-	}
-
-	if _, err := tw.Write(bjson); err != nil {
-		return nil, err
-	}
-
-	for service := range m.Services {
-
-		image, err := exec.Command("docker", "save", fmt.Sprintf("%s:%s.%s", imageRepo, service, b.Id)).Output()
-		if err != nil {
-			return nil, err
-		}
-
-		header := &tar.Header{
-			Typeflag: tar.TypeReg,
-			Name:     fmt.Sprintf("%s.%s.tar", service, b.Id),
-			Mode:     0600,
-			Size:     int64(len(image)),
-		}
-
-		if err := tw.WriteHeader(header); err != nil {
-			return nil, err
-		}
-
-		if _, err := tw.Write(image); err != nil {
-			return nil, err
-		}
-	}
-
-	if err := tw.Close(); err != nil {
-		return nil, err
-	}
-
-	if err := gz.Close(); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), err
 }
 
 var idAlphabet = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
