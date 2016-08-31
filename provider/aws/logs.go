fae07d1f4fccea7ff813b8245609979dc3d72b2c
David Dollar <david@dollar.io>
2016-08-31 18:04:21 -0400

    refactor logs

--- provider/aws/logs.go
+++ provider/aws/logs.go
@@ -3,9 +3,11 @@ package aws
 import (
 	"fmt"
 	"io"
+	"sort"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/convox/rack/api/structs"
 )
@@ -16,41 +18,24 @@ func (p *AWSProvider) LogStream(app string, w io.Writer, opts structs.LogStreamO
 		return err
 	}
 
+	return p.subscribeLogs(w, a.Outputs["LogGroup"], opts)
+}
+
+func (p *AWSProvider) subscribeLogs(w io.Writer, group string, opts structs.LogStreamOptions) error {
+	if opts.Since.Nanoseconds() == 0 {
+		opts.Since = 2 * time.Minute
+	}
+
 	since := 2 * time.Minute
 	if opts.Since.Nanoseconds() > 0 {
 		since = opts.Since
 	}
 
-	startTime := aws.Int64(time.Now().Add(-since).UnixNano() / int64(time.Millisecond)) // number of milliseconds since Jan 1, 1970 00:00:00 UTC
-	nextToken := aws.String("")
+	// number of milliseconds since Jan 1, 1970 00:00:00 UTC
+	start := time.Now().Add(-since).UnixNano() / int64(time.Millisecond)
 
 	for {
-		req := &cloudwatchlogs.FilterLogEventsInput{
-			Interleaved:  aws.Bool(true),
-			LogGroupName: aws.String(a.Outputs["LogGroup"]),
-			StartTime:    startTime,
-		}
-
-		if opts.Filter != "" {
-			req.FilterPattern = aws.String(opts.Filter)
-		}
-
-		startTime, nextToken, err = p.writeLogEvents(req, w)
-		if err != nil {
-			return err
-		}
-
-		for nextToken != nil {
-			req.NextToken = nextToken
-
-			startTime, nextToken, err = p.writeLogEvents(req, w)
-			if err != nil {
-				return err
-			}
-		}
-
-		// assert that websocket is still alive
-		_, err = w.Write([]byte{})
+		s, err := p.fetchLogs(w, group, opts.Filter, start)
 		if err != nil {
 			return err
 		}
@@ -59,46 +44,94 @@ func (p *AWSProvider) LogStream(app string, w io.Writer, opts structs.LogStreamO
 			return nil
 		}
 
-		// According to http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/cloudwatch_limits.html
-		// the maximum rate of a GetLogEvents request is 10 requests per second per AWS account.
-		// Aim for 5 reqs / sec so two clients can tail.
+		start = s + 1
+
 		time.Sleep(200 * time.Millisecond)
 	}
-
-	return nil
 }
 
-func (p *AWSProvider) writeLogEvents(req *cloudwatchlogs.FilterLogEventsInput, w io.Writer) (*int64, *string, error) {
-	resp, err := p.cloudwatchlogs().FilterLogEvents(req)
-	if code := awsError(err); code == "ThrottlingException" {
-		// Backoff but don't return an error
-		fmt.Printf("logs writeLogEvents err=%q\n", code)
-		time.Sleep(1 * time.Second)
-		return req.StartTime, req.NextToken, nil
-	}
-	if err != nil {
-		fmt.Printf("logs writeLogEvents err=%q\n", err)
-		return req.StartTime, req.NextToken, err
+// fetch logs until we run out of NextTokens, writing them the whole way
+func (p *AWSProvider) fetchLogs(w io.Writer, group, filter string, start int64) (int64, error) {
+	log := Logger.At("fetchLogs").Namespace("start=%d", start).Start()
+
+	req := &cloudwatchlogs.FilterLogEventsInput{
+		Interleaved:  aws.Bool(true),
+		LogGroupName: aws.String(group),
+		StartTime:    aws.Int64(start),
 	}
 
-	ts := req.StartTime
+	if filter != "" {
+		req.FilterPattern = aws.String(filter)
+	}
 
-	for _, e := range resp.Events {
-		if *e.Timestamp >= *ts {
-			*ts = *e.Timestamp + int64(1)
+	for {
+		res, err := p.cloudwatchlogs().FilterLogEvents(req)
+		if ae, ok := err.(awserr.Error); ok && ae.Code() == "ThrottlingException" {
+			// backoff
+			log.Error(err)
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		if err != nil {
+			log.Error(err)
+			return 0, err
+		}
+
+		latest, err := p.writeLogEvents(w, res.Events)
+		if err != nil {
+			log.Error(err)
+			return 0, err
+		}
+
+		if latest > start {
+			start = latest
+		}
+
+		if res.NextToken == nil {
+			break
+		}
+
+		req.NextToken = res.NextToken
+	}
+
+	log.Successf("end=%d", start)
+	return start, nil
+}
+
+func (p *AWSProvider) writeLogEvents(w io.Writer, events []*cloudwatchlogs.FilteredLogEvent) (int64, error) {
+	if len(events) == 0 {
+		return 0, nil
+	}
+
+	log := Logger.At("writeLogEvents").Namespace("events=%d", len(events)).Start()
+
+	sorted := cloudwatchEvents(events)
+	sort.Sort(sorted)
+
+	latest := int64(0)
+
+	for _, e := range sorted {
+		if *e.Timestamp > latest {
+			latest = *e.Timestamp
 		}
 
 		sec := *e.Timestamp / 1000
 		nsec := *e.Timestamp - (sec * 1000)
 		t := time.Unix(sec, nsec)
-
 		line := fmt.Sprintf("%s %s\n", t.Format(time.RFC3339), *e.Message)
-		_, err := w.Write([]byte(line))
-		if err != nil {
-			return ts, req.NextToken, err
+
+		if _, err := w.Write([]byte(line)); err != nil {
+			log.Error(err)
+			return 0, err
 		}
 	}
 
-	// return latest timestamp and NextToken seen in the response
-	return ts, resp.NextToken, nil
+	log.Success()
+	return latest, nil
 }
+
+type cloudwatchEvents []*cloudwatchlogs.FilteredLogEvent
+
+func (e cloudwatchEvents) Len() int           { return len(e) }
+func (e cloudwatchEvents) Less(i, j int) bool { return *(e[i].Timestamp) < *(e[j].Timestamp) }
+func (e cloudwatchEvents) Swap(i, j int)      { e[i], e[j] = e[j], e[i] }
