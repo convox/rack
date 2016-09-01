a682d87c33c77a9e1ea46fd82039f3e5bef81867
David Dollar <david@dollar.io>
2016-09-01 14:17:54 -0400

    fix deploy output

--- cmd/convox/deploy.go
+++ cmd/convox/deploy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"gopkg.in/urfave/cli.v1"
@@ -52,8 +53,12 @@ func cmdDeploy(c *cli.Context) error {
 		return stdcli.ExitError(fmt.Errorf("unable to build app: %s", app))
 	}
 
+	// set up a pipe so that we dont close stdout
+	r, w := io.Pipe()
+	go io.Copy(os.Stdout, r)
+
 	// build
-	_, release, err := executeBuild(c, dir, app, c.String("file"), c.String("description"), os.Stdout)
+	_, release, err := executeBuild(c, dir, app, c.String("file"), c.String("description"), w)
 	if err != nil {
 		return stdcli.ExitError(err)
 	}
