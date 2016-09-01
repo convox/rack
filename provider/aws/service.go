4c6102424fbb1ca3d8d1156e40170c28d29e33ad
Miguel Moll <miguel@convox.com>
2016-09-01 10:04:10 -0400

    Use notification host instead of arn ID

--- provider/aws/service.go
+++ provider/aws/service.go
@@ -49,7 +49,7 @@ func (p *AWSProvider) ServiceCreate(name, kind string, params map[string]string)
 	case "syslog":
 		req, err = p.createServiceURL(s, "tcp", "tcp+tls", "udp")
 	case "webhook":
-		s.Parameters["Url"] = fmt.Sprintf("http://%s/sns?endpoint=%s", p.NotificationTopic, url.QueryEscape(s.Parameters["Url"]))
+		s.Parameters["Url"] = fmt.Sprintf("http://%s/sns?endpoint=%s", p.NotificationHost, url.QueryEscape(s.Parameters["Url"]))
 		req, err = p.createServiceURL(s, "http", "https")
 	default:
 		err = fmt.Errorf("Invalid service type: %s", s.Type)
