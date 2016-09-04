c8013cb4235bcfa5656d74dff8023c865fb46e63
David Dollar <david@dollar.io>
2016-09-04 19:06:05 -0400

    helper for docker tests

---
+++ provider/aws/docker_test.go
@@ -0,0 +1,17 @@
+package aws_test
+
+import (
+	"net/http/httptest"
+	"os"
+
+	"github.com/convox/rack/api/awsutil"
+)
+
+func stubDocker(cycles ...awsutil.Cycle) *httptest.Server {
+	handler := awsutil.NewHandler(cycles)
+	s := httptest.NewServer(handler)
+
+	os.Setenv("DOCKER_HOST", s.URL[7:])
+
+	return s
+}
