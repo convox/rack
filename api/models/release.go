10a7850a9c8114f148fc2babcdaefead11dc525f
David Dollar <david@dollar.io>
2016-09-16 09:52:38 -0400

    remove port randomization
    increase rack size or use autoscale for this boundary

--- api/models/release.go
+++ api/models/release.go
@@ -161,8 +161,6 @@ func (r *Release) Promote() error {
 		return err
 	}
 
-	oldVersion := app.Parameters["Version"]
-
 	app.Parameters["Environment"] = r.EnvironmentUrl()
 	app.Parameters["Kernel"] = CustomTopic
 	app.Parameters["Release"] = r.Id
@@ -285,15 +283,6 @@ func (r *Release) Promote() error {
 
 					app.Parameters[certParam] = *res.ServerCertificateMetadata.Arn
 				}
-			}
-		}
-	}
-
-	// randomize the instance ports for older apps so we can upgrade smoothly
-	if oldVersion < "20160818013241" {
-		for key := range app.Parameters {
-			if strings.HasSuffix(key, "Host") {
-				app.Parameters[key] = strconv.Itoa(rand.Intn(50000) + 10000)
 			}
 		}
 	}
