0595da2d8a13b67edba93407b29bc3864c1f5a8f
David Dollar <david@dollar.io>
2016-09-15 23:08:35 -0400

    clean up test cycles

--- provider/aws/builds_test.go
+++ provider/aws/builds_test.go
@@ -219,12 +219,8 @@ func TestBuildImport(t *testing.T) {
 
 func TestBuildList(t *testing.T) {
 	provider := StubAwsProvider(
-		describeStacksCycle,
-
-		buildsQueryCycle,
-
-		build1GetObjectCycle,
-		build2GetObjectCycle,
+		cycleBuildDescribeStacks,
+		cycleBuildQuery,
 	)
 	defer provider.Close()
 
@@ -250,8 +246,8 @@ func TestBuildList(t *testing.T) {
 			Manifest: "web:\n  image: httpd\n  ports:\n  - 80:80\n",
 			Release:  "RFVZFLKVTYO",
 			Status:   "complete",
-			Started:  time.Unix(1459709087, 472025215).UTC(),
-			Ended:    time.Unix(1459709198, 984281955).UTC(),
+			Started:  time.Unix(1459780456, 178278576).UTC(),
+			Ended:    time.Unix(1459780542, 440881687).UTC(),
 			Tags:     map[string]string{},
 		},
 	}, b)
@@ -413,13 +409,9 @@ var cycleBuildDescribeRepositories = awsutil.Cycle{
 }
 
 var cycleBuildDescribeStacks = awsutil.Cycle{
-	awsutil.Request{
-		RequestURI: "/",
-		Body:       `Action=DescribeStacks&StackName=convox-httpd&Version=2010-05-15`,
-	},
-	awsutil.Response{
-		StatusCode: 200,
-		Body: `<DescribeStacksResponse xmlns="http://cloudformation.amazonaws.com/doc/2010-05-15/">
+	awsutil.Request{"/", "", `Action=DescribeStacks&StackName=convox-httpd&Version=2010-05-15`},
+	awsutil.Response{200, `
+		<DescribeStacksResponse xmlns="http://cloudformation.amazonaws.com/doc/2010-05-15/">
 			<DescribeStacksResult>
 				<Stacks>
 					<member>
@@ -774,6 +766,43 @@ var cycleBuildPutItem = awsutil.Cycle{
 	},
 }
 
+var cycleBuildQuery = awsutil.Cycle{
+	Request: awsutil.Request{
+		RequestURI: "/",
+		Operation:  "DynamoDB_20120810.Query",
+		Body:       `{"IndexName":"app.created","KeyConditions":{"app":{"AttributeValueList":[{"S":"httpd"}],"ComparisonOperator":"EQ"}},"Limit":20,"ScanIndexForward":false,"TableName":"convox-builds"}`,
+	},
+	Response: awsutil.Response{
+		StatusCode: 200,
+		Body: `{
+			"Count": 2,
+			"Items": [
+				{
+					"id": {"S":"BHINCLZYYVN"},
+					"app": {"S":"httpd"},
+					"created": {"S":"20160404.143416.178278576"},
+					"ended": {"S":"20160404.143542.440881687"},
+					"env": {"S":"foo=bar"},
+					"manifest": {"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},
+					"release": {"S":"RVFETUHHKKD"},
+					"status": {"S":"complete"}
+				},
+				{
+					"id": {"S":"BNOARQMVHUO"},
+					"app": {"S":"httpd"},
+					"created": {"S":"20160404.143416.178278576"},
+					"ended": {"S":"20160404.143542.440881687"},
+					"env": {"S":"foo=bar"},
+					"manifest": {"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},
+					"release": {"S":"RFVZFLKVTYO"},
+					"status": {"S":"complete"}
+				}
+			],
+			"ScannedCount":2
+		}`,
+	},
+}
+
 var cycleBuildReleasePutItem = awsutil.Cycle{
 	Request: awsutil.Request{
 		RequestURI: "/",
@@ -923,288 +952,5 @@ var cycleEnvironmentPut = awsutil.Cycle{
 	},
 	Response: awsutil.Response{
 		StatusCode: 200,
-	},
-}
-
-var describeStacksCycle = awsutil.Cycle{
-	awsutil.Request{"/", "", `Action=DescribeStacks&StackName=convox-httpd&Version=2010-05-15`},
-	awsutil.Response{200, `<DescribeStacksResponse xmlns="http://cloudformation.amazonaws.com/doc/2010-05-15/">
-  <DescribeStacksResult>
-    <Stacks>
-      <member>
-        <Tags>
-          <member>
-            <Value>httpd</Value>
-            <Key>Name</Key>
-          </member>
-          <member>
-            <Value>app</Value>
-            <Key>Type</Key>
-          </member>
-          <member>
-            <Value>convox</Value>
-            <Key>System</Key>
-          </member>
-          <member>
-            <Value>convox</Value>
-            <Key>Rack</Key>
-          </member>
-        </Tags>
-        <StackId>arn:aws:cloudformation:us-east-1:132866487567:stack/convox-httpd/53df3c30-f763-11e5-bd5d-50d5cd148236</StackId>
-        <StackStatus>UPDATE_COMPLETE</StackStatus>
-        <StackName>convox-httpd</StackName>
-        <LastUpdatedTime>2016-03-31T17:12:16.275Z</LastUpdatedTime>
-        <NotificationARNs/>
-        <CreationTime>2016-03-31T17:09:28.583Z</CreationTime>
-        <Parameters>
-          <member>
-            <ParameterValue>https://convox-httpd-settings-139bidzalmbtu.s3.amazonaws.com/releases/RVFETUHHKKD/env</ParameterValue>
-            <ParameterKey>Environment</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue/>
-            <ParameterKey>WebPort80Certificate</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>No</ParameterValue>
-            <ParameterKey>WebPort80ProxyProtocol</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>256</ParameterValue>
-            <ParameterKey>WebCpu</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>256</ParameterValue>
-            <ParameterKey>WebMemory</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>arn:aws:kms:us-east-1:132866487567:key/d9f38426-9017-4931-84f8-604ad1524920</ParameterValue>
-            <ParameterKey>Key</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue/>
-            <ParameterKey>Repository</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>80</ParameterValue>
-            <ParameterKey>WebPort80Balancer</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>56694</ParameterValue>
-            <ParameterKey>WebPort80Host</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>vpc-f8006b9c</ParameterValue>
-            <ParameterKey>VPC</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>1</ParameterValue>
-            <ParameterKey>WebDesiredCount</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>convox-Cluster-1E4XJ0PQWNAYS</ParameterValue>
-            <ParameterKey>Cluster</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>subnet-d4e85cfe,subnet-103d5a66,subnet-57952a0f</ParameterValue>
-            <ParameterKey>SubnetsPrivate</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>RVFETUHHKKD</ParameterValue>
-            <ParameterKey>Release</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>No</ParameterValue>
-            <ParameterKey>WebPort80Secure</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>subnet-13de3139,subnet-b5578fc3,subnet-21c13379</ParameterValue>
-            <ParameterKey>Subnets</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>20160330143438-command-exec-form</ParameterValue>
-            <ParameterKey>Version</ParameterKey>
-          </member>
-          <member>
-            <ParameterValue>Yes</ParameterValue>
-            <ParameterKey>Private</ParameterKey>
-          </member>
-        </Parameters>
-        <DisableRollback>false</DisableRollback>
-        <Capabilities>
-          <member>CAPABILITY_IAM</member>
-        </Capabilities>
-        <Outputs>
-          <member>
-            <OutputValue>httpd-web-7E5UPCM-1241527783.us-east-1.elb.amazonaws.com</OutputValue>
-            <OutputKey>BalancerWebHost</OutputKey>
-          </member>
-          <member>
-            <OutputValue>convox-httpd-Kinesis-1MAP0GJ6RITJF</OutputValue>
-            <OutputKey>Kinesis</OutputKey>
-          </member>
-          <member>
-            <OutputValue>convox-httpd-LogGroup-L4V203L35WRM</OutputValue>
-            <OutputKey>LogGroup</OutputKey>
-          </member>
-          <member>
-            <OutputValue>132866487567</OutputValue>
-            <OutputKey>RegistryId</OutputKey>
-          </member>
-          <member>
-            <OutputValue>convox-httpd-hqvvfosgxt</OutputValue>
-            <OutputKey>RegistryRepository</OutputKey>
-          </member>
-          <member>
-            <OutputValue>convox-httpd-settings-139bidzalmbtu</OutputValue>
-            <OutputKey>Settings</OutputKey>
-          </member>
-          <member>
-            <OutputValue>80</OutputValue>
-            <OutputKey>WebPort80Balancer</OutputKey>
-          </member>
-          <member>
-            <OutputValue>httpd-web-7E5UPCM</OutputValue>
-            <OutputKey>WebPort80BalancerName</OutputKey>
-          </member>
-        </Outputs>
-      </member>
-    </Stacks>
-  </DescribeStacksResult>
-  <ResponseMetadata>
-    <RequestId>d5220387-f76d-11e5-912c-531803b112a4</RequestId>
-  </ResponseMetadata>
-</DescribeStacksResponse>`},
-}
-
-var buildsQueryCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.Query",
-		Body:       `{"IndexName":"app.created","KeyConditions":{"app":{"AttributeValueList":[{"S":"httpd"}],"ComparisonOperator":"EQ"}},"Limit":20,"ScanIndexForward":false,"TableName":"convox-builds"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"Count":2,"Items":[{"id":{"S":"BHINCLZYYVN"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"release":{"S":"RVFETUHHKKD"},"ended":{"S":"20160404.143542.440881687"},"app":{"S":"httpd"},"created":{"S":"20160404.143416.178278576"},"status":{"S":"complete"}},{"id":{"S":"BNOARQMVHUO"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"release":{"S":"RFVZFLKVTYO"},"ended":{"S":"20160403.184638.984281955"},"app":{"S":"httpd"},"created":{"S":"20160403.184447.472025215"},"status":{"S":"complete"}}],"ScannedCount":2}`,
-	},
-}
-
-var build1GetItemCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.GetItem",
-		Body:       `{"ConsistentRead":true,"Key":{"id":{"S":"BHINCLZYYVN"}},"TableName":"convox-builds"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"Item":{"id":{"S":"BHINCLZYYVN"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"ended":{"S":"20160404.143542.440881687"},"release":{"S":"RVFETUHHKKD"},"app":{"S":"httpd"},"created":{"S":"20160404.143416.178278576"},"status":{"S":"complete"}}}`,
-	},
-}
-
-var build1GetObjectCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/convox-httpd-settings-139bidzalmbtu/builds/BHINCLZYYVN.log",
-		Operation:  "",
-		Body:       ``,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `RUNNING: docker pull httpd`,
-	},
-}
-
-var build2BatchDeleteImageCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "AmazonEC2ContainerRegistry_V20150921.BatchDeleteImage",
-		Body:       `{"imageIds":[{"imageTag":"web.BNOARQMVHUO"}],"registryId":"132866487567","repositoryName":"convox-httpd-hqvvfosgxt"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"failures":[],"imageIds":[{"imageDigest":"sha256:77f27a1381e53241cd230ca1abf74e33ece2715a51e89ba8bdf8908b9a75aa3d","imageTag":"web.BNOARQMVHUO"}]}`,
-	},
-}
-
-var build2GetItemCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.GetItem",
-		Body:       `{"ConsistentRead":true,"Key":{"id":{"S":"BNOARQMVHUO"}},"TableName":"convox-builds"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"Item":{"id":{"S":"BNOARQMVHUO"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"ended":{"S":"20160403.184638.984281955"},"release":{"S":"RFVZFLKVTYO"},"app":{"S":"httpd"},"created":{"S":"20160403.184447.472025215"},"status":{"S":"complete"}}}`,
-	},
-}
-
-var build2GetObjectCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/convox-httpd-settings-139bidzalmbtu/builds/BNOARQMVHUO.log",
-		Operation:  "",
-		Body:       ``,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `RUNNING: docker pull httpd`,
-	},
-}
-
-var build2DeleteItemCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.DeleteItem",
-		Body:       `{"Key":{"id":{"S":"BNOARQMVHUO"}},"TableName":"convox-builds"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{}`,
-	},
-}
-
-var releasesQueryCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.Query",
-		Body:       `{"IndexName":"app.created","KeyConditions":{"app":{"AttributeValueList":[{"S":"httpd"}],"ComparisonOperator":"EQ"}},"Limit":20,"ScanIndexForward":false,"TableName":"convox-releases"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"Count":2,"Items":[{"id":{"S":"RVFETUHHKKD"},"build":{"S":"BHINCLZYYVN"},"app":{"S":"httpd"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"env":{"S":"foo=bar"},"created":{"S":"20160404.143542.627770380"}},{"id":{"S":"RFVZFLKVTYO"},"build":{"S":"BNOARQMVHUO"},"app":{"S":"httpd"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"env":{"S":"foo=bar"},"created":{"S":"20160403.184639.166694813"}}],"ScannedCount":2}`,
-	},
-}
-
-var releasesBuild1DeleteItemCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.DeleteItem",
-		Body:       `{"Key":{"id":{"S": "BHINCLZYYVN"}},"TableName": "convox-builds"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"Count":1,"Items":[{"id":{"S":"RVFETUHHKKD"},"build":{"S":"BHINCLZYYVN"},"app":{"S":"httpd"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"env":{"S":"foo=bar"},"created":{"S":"20160404.143542.627770380"}}],"ScannedCount":2}`,
-	},
-}
-
-var releasesBuild2DeleteItemCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.DeleteItem",
-		Body:       `{"Key": {"id":{"S":"BNOARQMVHUO"}},"TableName":"convox-builds"}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"Count":1,"Items":[{"id":{"S":"RFVZFLKVTYO"},"build":{"S":"BNOARQMVHUO"},"app":{"S":"httpd"},"manifest":{"S":"web:\n  image: httpd\n  ports:\n  - 80:80\n"},"env":{"S":"foo=bar"},"created":{"S":"20160403.184639.166694813"}}],"ScannedCount":2}`,
-	},
-}
-
-var releasesBuild2BatchWriteItemCycle = awsutil.Cycle{
-	Request: awsutil.Request{
-		RequestURI: "/",
-		Operation:  "DynamoDB_20120810.BatchWriteItem",
-		Body:       `{"RequestItems":{"convox-releases":[{"DeleteRequest":{"Key":{"id":{"S":"RFVZFLKVTYO"}}}}]}}`,
-	},
-	Response: awsutil.Response{
-		StatusCode: 200,
-		Body:       `{"UnprocessedItems":{}}`,
 	},
 }
