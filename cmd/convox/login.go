23275ed28dd1f31b7b4acd349764e262e3d76db9
Ian Malott <ian@ianmalott.com>
2016-11-03 09:33:03 -0400

    Rephrase /signup suggestion

--- cmd/convox/login.go
+++ cmd/convox/login.go
@@ -102,7 +102,7 @@ func cmdLogin(c *cli.Context) error {
 
 	if err != nil {
 		if strings.Contains(err.Error(), "401") {
-			return stdcli.Error(fmt.Errorf("invalid login. Create an account at https://convox.com/signup"))
+			return stdcli.Error(fmt.Errorf("invalid login\nHave you created an account at https://convox.com/signup?"))
 		} else {
 			return stdcli.Error(err)
 		}
