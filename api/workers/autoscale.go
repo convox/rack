3a1600a87f595127b7ff22c505dfc44482a9e644
Miguel Moll <miguel.moll@gmail.com>
2016-12-16 14:39:40 -0500

    feedback changes

--- api/workers/autoscale.go
+++ api/workers/autoscale.go
@@ -83,18 +83,22 @@ func autoscaleRack() {
 		return
 	}
 
-	log.Logf("change=%d", (desired - system.Count))
+	oldCount := system.Count
 
 	// ok to start multiple instances in one pass
 	// when shutting down go one at a time but only if current status is "running"
 	if desired < system.Count {
 		if system.Status == "running" {
 			system.Count--
-		} else {
-			return
 		}
 	} else {
 		system.Count = desired
+	}
+
+	log.Logf("change=%d", (system.Count - oldCount))
+	// nothing changed, return
+	if system.Count == oldCount {
+		return
 	}
 
 	err = models.Provider().SystemSave(*system)
