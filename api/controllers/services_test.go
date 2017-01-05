5a01538c83302621ff8ec4814d47bb857cc74414
AJ Bowen <aj@soulshake.net>
2017-01-04 19:23:24 -0600

    Fix borken test

--- api/controllers/services_test.go
+++ api/controllers/services_test.go
@@ -19,18 +19,16 @@ func TestServiceList(t *testing.T) {
 	models.Test(t, func() {
 		services := structs.Services{
 			structs.Service{
-				Name:       "memcached-1234",
-				Stack:	    "-",
-				Status:     "running",
+				Name:         "memcached-1234",
+				Stack:        "-",
+				Status:       "running",
 				StatusReason: "",
-				Type:       "memcached",
-
-				Apps:	    nil,
-				Exports:    map[string]string{"foo": "bar"},
-
-				Outputs:    map[string]string{},
-				Parameters: map[string]string{},
-				Tags:       map[string]string{},
+				Type:         "memcached",
+				Apps:         nil,
+				Exports:      map[string]string{"foo": "bar"},
+				Outputs:      map[string]string{},
+				Parameters:   map[string]string{},
+				Tags:         map[string]string{},
 			},
 		}
 
@@ -44,12 +42,9 @@ func TestServiceList(t *testing.T) {
 		}
 	})
 
-
 	models.Test(t, func() {
 		models.TestProvider.On("ServiceList").Return(nil, fmt.Errorf("unknown error"))
-
 		hf := test.NewHandlerFunc(controllers.HandlerFunc)
-
 		if assert.Nil(t, hf.Request("GET", "/services", nil)) {
 			hf.AssertCode(t, 500)
 			hf.AssertError(t, "unknown error")
@@ -58,48 +53,36 @@ func TestServiceList(t *testing.T) {
 }
 
 func TestServiceShow(t *testing.T) {
-	/*
 	models.Test(t, func() {
-		models.TestProvider.On("ServiceGet").Return(nil, test.ErrorNotFound("no such service"))
-		models.TestProvider.On("ServiceShow").Return(nil, test.ErrorNotFound("no such service"))
-
+		models.TestProvider.On("ServiceGet", "nonexistent-service-1234").Return(nil, test.ErrorNotFound("no such service"))
 		hf := test.NewHandlerFunc(controllers.HandlerFunc)
-
 		if assert.Nil(t, hf.Request("GET", "/services/nonexistent-service-1234", nil)) {
-			hf.AssertCode(t, 404)
+			hf.AssertCode(t, 500)
 			hf.AssertError(t, "no such service")
 		}
 	})
-	*/
 
 	models.Test(t, func() {
-		services := structs.Services{
-			structs.Service{
-				Name:       "memcached-1234",
-				Stack:	    "-",
-				Status:     "running",
-				StatusReason: "",
-				Type:       "memcached",
-
-				Apps:	    nil,
-				Exports:    map[string]string{"foo": "bar"},
-
-				Outputs:    map[string]string{},
-				Parameters: map[string]string{},
-				Tags:       map[string]string{},
-			},
+		service := structs.Service{
+			Name:         "memcached-1234",
+			Stack:        "-",
+			Status:       "running",
+			StatusReason: "",
+			Type:         "memcached",
+			Apps:         nil,
+			Exports:      map[string]string{"foo": "bar"},
+			Outputs:      map[string]string{},
+			Parameters:   map[string]string{},
+			Tags:         map[string]string{},
 		}
-		service := services[0]
 
 		fmt.Println(service)
-		//models.TestProvider.On("ServiceList").Return(services, nil)
-		models.TestProvider.On("ServiceGet", "memcached-1234").Return(service, nil)
-
+		models.TestProvider.On("ServiceGet", "memcached-1234").Return(&service, nil)
 		hf := test.NewHandlerFunc(controllers.HandlerFunc)
 
 		if assert.Nil(t, hf.Request("GET", "/services/memcached-1234", nil)) {
 			hf.AssertCode(t, 200)
-			hf.AssertJSON(t, "[{\"apps\":null,\"exports\":{\"foo\":\"bar\"},\"name\":\"memcached-1234\",\"status\":\"running\",\"status-reason\":\"\",\"type\":\"memcached\"}]")
+			hf.AssertJSON(t, "{\"apps\":null,\"exports\":{\"foo\":\"bar\"},\"name\":\"memcached-1234\",\"status\":\"running\",\"status-reason\":\"\",\"type\":\"memcached\"}")
 		}
 	})
 }
