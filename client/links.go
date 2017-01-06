0e5bc5bb32002f094c37846f28d445f6d539be75
AJ Bowen <aj@soulshake.net>
2017-01-06 15:56:46 -0600

    Update routes and client-side use of services vs. resources. Fix docstrings and formatting.
    Add note to createResource() as per @MiguelMoll feedback.

--- client/links.go
+++ client/links.go
@@ -2,30 +2,32 @@ package client
 
 import "fmt"
 
-func (c *Client) CreateLink(app, name string) (*Service, error) {
+// CreateLink creates a link between an app and a resource.
+func (c *Client) CreateLink(app, name string) (*Resource, error) {
 	params := Params{
 		"app": app,
 	}
 
-	var service Service
+	var resource Resource
 
-	err := c.Post(fmt.Sprintf("/services/%s/links", name), params, &service)
+	err := c.Post(fmt.Sprintf("/resources/%s/links", name), params, &resource)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &service, nil
+	return &resource, nil
 }
 
-func (c *Client) DeleteLink(app, name string) (*Service, error) {
-	var service Service
+// DeleteLink deletes a link between an app and a resource.
+func (c *Client) DeleteLink(app, name string) (*Resource, error) {
+	var resource Resource
 
-	err := c.Delete(fmt.Sprintf("/services/%s/links/%s", name, app), &service)
+	err := c.Delete(fmt.Sprintf("/resources/%s/links/%s", name, app), &resource)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &service, nil
+	return &resource, nil
 }
