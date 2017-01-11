da53acc954a82393c4af291ed86263d64486c070
David Dollar <david@dollar.io>
2017-01-11 16:30:21 -0500

    reset terminal after raw session ends

--- client/processes.go
+++ client/processes.go
@@ -148,8 +148,9 @@ func copyWithExit(w io.Writer, r io.Reader, ch chan int) {
 		}
 
 		if !isTerminalRaw {
-			terminal.MakeRaw(int(os.Stdin.Fd()))
+			state, _ := terminal.MakeRaw(int(os.Stdin.Fd()))
 			isTerminalRaw = true
+			defer terminal.Restore(int(os.Stdin.Fd()), state)
 		}
 
 		if s := string(buf[0:n]); strings.HasPrefix(s, StatusCodePrefix) {
