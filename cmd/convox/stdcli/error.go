730c2f14d1fe48e3c5502176406d55f89294a2e1
Luke Roberts <email@luke-roberts.co.uk>
2017-01-31 13:57:38 +0000

    satisfy the hound

--- cmd/convox/stdcli/error.go
+++ cmd/convox/stdcli/error.go
@@ -2,6 +2,7 @@ package stdcli
 
 import "fmt"
 
+//Error returns an error with custom formatting for output via the cli package writer
 func Error(err error) error {
 	return fmt.Errorf(renderTags("<error>%s</error>"), err)
 }
