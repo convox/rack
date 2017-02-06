45a286e06d26f8f2c031cff0bd3a10f890954d54
Noah Zoschke <noah@convox.com>
2017-02-06 12:20:10 -0800

    test that rack arg is always parsed and set as header

--- cmd/convox/api_test.go
+++ cmd/convox/api_test.go
@@ -20,3 +20,42 @@ func TestApiGet(t *testing.T) {
 		},
 	)
 }
+
+func TestRackFlag(t *testing.T) {
+	ts := testServer(t,
+		test.Http{Method: "GET", Path: "/foo", Code: 200, Response: "bar", Headers: map[string]string{"Rack": "myorg/staging"}},
+	)
+	defer ts.Close()
+
+	test.Runs(t,
+		test.ExecRun{
+			Command:  "convox --rack myorg/staging api get /foo",
+			Exit:     0,
+			OutMatch: "bar",
+		},
+	)
+
+	test.Runs(t,
+		test.ExecRun{
+			Command: "convox api --rack myorg/staging get /foo",
+			Exit:    0,
+			Stdout:  "bar",
+		},
+	)
+
+	test.Runs(t,
+		test.ExecRun{
+			Command:  "convox api get --rack myorg/staging /foo",
+			Exit:     0,
+			OutMatch: "bar",
+		},
+	)
+
+	test.Runs(t,
+		test.ExecRun{
+			Command:  "convox api get /foo --rack myorg/staging",
+			Exit:     0,
+			OutMatch: "bar",
+		},
+	)
+}
