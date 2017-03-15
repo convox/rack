160b7fd80af295b4e0d0fd774dd30d7e94d251e5
AJ Bowen <aj@convox.com>
2017-03-15 11:25:44 -0500

    Add tests

--- cmd/convox/main_test.go
+++ cmd/convox/main_test.go
@@ -10,6 +10,13 @@ import (
 	"github.com/convox/rack/test"
 )
 
+var configlessEnv = map[string]string{
+	// reset HOME to a location where there's not likely to be a convox config on the host
+	"HOME":                  "/tmp/probablyNoConfigFileHere",
+	"AWS_SECRET_ACCESS_KEY": "",
+	"AWS_ACCESS_KEY_ID":     "",
+}
+
 func testServer(t *testing.T, stubs ...test.Http) *httptest.Server {
 	stubs = append(stubs, test.Http{Method: "GET", Path: "/system", Code: 200, Response: client.System{
 		Version: "latest",
@@ -23,4 +30,15 @@ func testServer(t *testing.T, stubs ...test.Http) *httptest.Server {
 	os.Setenv("CONVOX_PASSWORD", "test")
 
 	return server
+}
+
+func TestVersion(t *testing.T) {
+	// Ensure we don't segfault if user is not logged in
+	test.Runs(t, test.ExecRun{
+		Command: "convox -v",
+		Env:     configlessEnv,
+		Exit:    0,
+		Stdout:  "client: dev\n",
+		Stderr:  "ERROR: no host config found, try `convox login`\nERROR: Get https:///system: http: no Host in request URL\n",
+	})
 }
