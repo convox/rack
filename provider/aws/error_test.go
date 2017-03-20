94dbd411de87d2631f9a7f2761803e9c639cc438
SonOfBytes <SonOfBytes@users.noreply.github.com>
2017-03-20 13:51:40 +0000

    add test for formatting of NoSuchBuild error

--- provider/aws/error_test.go
+++ provider/aws/error_test.go
@@ -1,5 +1,12 @@
 package aws_test
 
+import (
+	"errors"
+	"github.com/convox/rack/provider/aws"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 type errorNotFound string
 
 func (e errorNotFound) Error() string {
@@ -8,4 +15,11 @@ func (e errorNotFound) Error() string {
 
 func (e errorNotFound) NotFound() bool {
 	return true
+}
+
+func TestNoSuchBuild_Error(t *testing.T) {
+	err := aws.NoSuchBuild("B12345")
+	if assert.Error(t, err) {
+		assert.Equal(t, errors.New("no such build: B12345").Error(), err.Error())
+	}
 }
