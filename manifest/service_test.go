ab11eed74bbb7416199b54d3c8a8a1a2f0405930
Reuven Gonzales <reuven@virtru.com>
2017-03-27 11:48:47 -0700

    Group name tests

--- manifest/service_test.go
+++ manifest/service_test.go
@@ -104,3 +104,16 @@ func TestUseSecureEnvironment(t *testing.T) {
 	assert.True(t, secureService.UseSecureEnvironment())
 	assert.False(t, notSecureService.UseSecureEnvironment())
 }
+
+func TestGroupName(t *testing.T) {
+	m, err := manifestFixture("group")
+	if err != nil {
+		assert.FailNow(t, fmt.Sprintf("failed to read fixture: %s", err.Error()))
+	}
+	webService := m.Services["web"]
+	reverseProxyService := m.Services["reverse-proxy"]
+	workerService := m.Services["worker"]
+	assert.Equal(t, "web", webService.GroupName())
+	assert.Equal(t, "web", reverseProxyService.GroupName())
+	assert.Equal(t, "worker", workerService.GroupName())
+}
