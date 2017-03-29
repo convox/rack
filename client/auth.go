dc31c8f7a6fc8716eae17d52c82667d5f45629b4
David Dollar <david@dollar.io>
2017-03-29 14:01:16 -0400

    allow auth with old console

--- client/auth.go
+++ client/auth.go
@@ -42,7 +42,7 @@ func (c *Client) Auth() (*Auth, error) {
 	auth := &Auth{}
 
 	//legacy racks used to return a plain text string 'OK\n'
-	if string(body) == "OK\n" {
+	if string(body) == "OK\n" || len(body) == 0 {
 		return auth, nil
 	}
 
