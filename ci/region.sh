2072fe339168d86de19511ee174c47167112ee62
David Dollar <david@dollar.io>
2017-04-03 15:26:02 -0400

    reorder regions for ci

--- ci/region.sh
+++ ci/region.sh
@@ -4,30 +4,30 @@ case "$1" in
   0)
     echo AWS_DEFAULT_REGION=us-east-1
     echo AWS_REGION=us-east-1
-    echo RACK_BUILD_INSTANCE=m4.large
+    echo RACK_BUILD_INSTANCE=t2.small
     ;;
   1)
-    echo AWS_DEFAULT_REGION=us-east-2
-    echo AWS_REGION=us-east-2
-    ;;
-  2)
-    echo AWS_DEFAULT_REGION=us-west-1
-    echo AWS_REGION=us-west-1
-    echo RACK_BUILD_INSTANCE=m4.xlarge # different value for testing
-    ;;
-  3)
     echo AWS_DEFAULT_REGION=us-west-2
     echo AWS_REGION=us-west-2
     echo RACK_PRIVATE=true
     ;;
-  4)
-    echo AWS_DEFAULT_REGION=eu-central-1
-    echo AWS_REGION=eu-central-1
-    ;;
-  5)
+  2)
     echo AWS_DEFAULT_REGION=eu-west-1
     echo AWS_REGION=eu-west-1
-    echo RACK_BUILD_INSTANCE=m4.large
+    echo RACK_BUILD_INSTANCE=t2.small
+    ;;
+  3)
+    echo AWS_DEFAULT_REGION=us-east-2
+    echo AWS_REGION=us-east-2
+    ;;
+  4)
+    echo AWS_DEFAULT_REGION=us-west-1
+    echo AWS_REGION=us-west-1
+    echo RACK_BUILD_INSTANCE=m4.xlarge # different value for testing
+    ;;
+  5)
+    echo AWS_DEFAULT_REGION=eu-central-1
+    echo AWS_REGION=eu-central-1
     ;;
   6)
     echo AWS_DEFAULT_REGION=eu-west-2
