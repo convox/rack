860920dce51674c3d23b9a09b74caeec6dc22737
David Dollar <david@dollar.io>
2017-04-05 09:50:48 -0400

    dont proxy docker to local instances

--- provider/aws/docker.go
+++ provider/aws/docker.go
@@ -2,7 +2,10 @@ package aws
 
 import (
 	"fmt"
+	"net"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/fsouza/go-dockerclient"
 )
@@ -28,5 +31,23 @@ func (p *AWSProvider) dockerInstance(id string) (*docker.Client, error) {
 		host = fmt.Sprintf("http://%s:2376", *i.PrivateIpAddress)
 	}
 
-	return p.docker(host)
+	dc, err := p.docker(host)
+	if err != nil {
+		return nil, err
+	}
+
+	// DefaultTransport without the proxy
+	dc.HTTPClient.Transport = &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	return dc, nil
 }
