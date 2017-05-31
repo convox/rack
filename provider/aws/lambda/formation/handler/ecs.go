9687df5839752b091f325f0b5ff60071ab1ec025
Noah Zoschke <noah@convox.com>
2017-05-31 09:39:17 -0700

    handle memory and memory reservation in task/container definition

--- provider/aws/lambda/formation/handler/ecs.go
+++ provider/aws/lambda/formation/handler/ecs.go
@@ -162,11 +162,6 @@ func ECSTaskDefinitionCreate(req Request) (string, map[string]string, error) {
 			}
 		}
 
-		memory, err := strconv.Atoi(task["Memory"].(string))
-		if err != nil {
-			return "invalid", nil, err
-		}
-
 		privileged := false
 
 		if p, ok := task["Privileged"].(string); ok && p != "" {
@@ -181,8 +176,23 @@ func ECSTaskDefinitionCreate(req Request) (string, map[string]string, error) {
 			Essential:  aws.Bool(true),
 			Image:      aws.String(task["Image"].(string)),
 			Cpu:        aws.Int64(int64(cpu)),
-			Memory:     aws.Int64(int64(memory)),
 			Privileged: aws.Bool(privileged),
+		}
+
+		if m, ok := task["Memory"].(string); ok && m != "" {
+			memory, err := strconv.Atoi(m)
+			if err != nil {
+				return "invalid", nil, err
+			}
+			r.ContainerDefinitions[i].Memory = aws.Int64(int64(memory))
+		}
+
+		if m, ok := task["MemoryReservation"].(string); ok && m != "" {
+			memory, err := strconv.Atoi(m)
+			if err != nil {
+				return "invalid", nil, err
+			}
+			r.ContainerDefinitions[i].MemoryReservation = aws.Int64(int64(memory))
 		}
 
 		// set Command from either -
