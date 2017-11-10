638a7eaa585b51c4f087f3e65ebe043e717a7549
Feifei Jia <feifei.j@gmail.com>
2017-11-10 11:47:20 +0800

    add PrivateIP in cli.

--- cmd/convox/instances.go
+++ cmd/convox/instances.go
@@ -60,7 +60,7 @@ func cmdInstancesList(c *cli.Context) error {
 		return stdcli.Error(err)
 	}
 
-	t := stdcli.NewTable("ID", "AGENT", "STATUS", "STARTED", "PS", "CPU", "MEM")
+	t := stdcli.NewTable("ID", "PrivateIp", "AGENT", "STATUS", "STARTED", "PS", "CPU", "MEM")
 
 	for _, i := range instances {
 		agent := "off"
@@ -68,7 +68,7 @@ func cmdInstancesList(c *cli.Context) error {
 			agent = "on"
 		}
 
-		t.AddRow(i.Id, agent, i.Status,
+		t.AddRow(i.Id, i.PrivateIp, agent, i.Status,
 			helpers.HumanizeTime(i.Started),
 			strconv.Itoa(i.Processes),
 			fmt.Sprintf("%0.2f%%", i.Cpu*100),
