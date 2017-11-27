72e8a1eac3955d672cf8289788c035dc53d61d2c
David Dollar <david@dollar.io>
2017-11-27 15:50:40 -0500

    add term support

---
+++ cmd/convox/term_windows.go
@@ -0,0 +1,24 @@
+package main
+
+import "syscall"
+
+const enableVirtualTerminalProcessing = 0x0004
+
+func terminalSetup() error {
+	hnd, err := syscall.GetStdHandle(syscall.STD_OUTPUT_HANDLE)
+	if err != nil {
+		return err
+	}
+
+	var mode uint32
+
+	if err := syscall.GetConsoleMode(hnd, &mode); err != nil {
+		return err
+	}
+
+	if err := winterm.SetConsoleMode(uintptr(hnd), mode|enableVirtualTerminalProcessing); err != nil {
+		return err
+	}
+
+	return nil
+}
