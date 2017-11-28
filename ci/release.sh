6b2c1890b690f07574e39bd5f27db285f4afe5e9
Bob Zoller <bob@zoller.us>
2017-11-28 10:09:24 -1000

    deploy from travis

--- ci/release.sh
+++ ci/release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -x
+set -e
 
-# don't auto-publish until upgrade path is automatically tested
-# curl -vik -X POST $RELEASE_URL/publish -d token=$RELEASE_TOKEN -d version=$VERSION
+VERSION=${TRAVIS_COMMIT:7}
+VERSION=${VERSION:-"$(git rev-parse --short HEAD)"}
+export VERSION
+
+make release
