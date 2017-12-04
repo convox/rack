dbaa5498799102fb1d3214f7a1d3235c00661ece
David Dollar <david@dollar.io>
2017-12-04 11:06:17 -0500

    fix artifacts

--- ci/uninstall.sh
+++ ci/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex -o pipefail
 
-export CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS:-/tmp}
+export CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS:-/tmp/artifacts}
 export CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
 export STACK_NAME=convox-${CIRCLE_BUILD_NUM}
 export $($(dirname $0)/region.sh ${CIRCLE_NODE_INDEX})
