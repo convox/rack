401828a22e3c142d70711b6941d9e9a28f2cc9f9
David Dollar <david@dollar.io>
2017-12-04 12:58:16 -0500

    fix tests

--- provider/aws/lambda/syslog/main_test.go
+++ provider/aws/lambda/syslog/main_test.go
@@ -13,13 +13,8 @@ func TestFormatter(t *testing.T) {
 	os.Setenv("AWS_LAMBDA_FUNCTION_NAME", "convox-syslog-6125")
 
 	assertEqual(t,
-		contentFormatter(syslog.LOG_INFO, "hostname", "tag", `testLogGroup 1440442987000 [ERROR] First test message`),
-		"<22>1 2015-08-24T12:03:07-07:00 testLogGroup convox/syslog unknown - - [ERROR] First test message\n",
-	)
-
-	assertEqual(t,
-		contentFormatter(syslog.LOG_INFO, "hostname", "tag", `convox-httpd-LogGroup-1KIJO8SS9F3Q9 1461030802652 web:RGBCKLEZHCX/aedfffead7ad 10.0.3.37 - - [19/Apr/2016:01:53:22 +0000] "GET / HTTP/1.1" 304 -`),
-		"<22>1 2016-04-18T18:53:22-07:00 convox-httpd-LogGroup-1KIJO8SS9F3Q9 web:RGBCKLEZHCX aedfffead7ad - - 10.0.3.37 - - [19/Apr/2016:01:53:22 +0000] \"GET / HTTP/1.1\" 304 -\n",
+		"<22>1 2015-08-24T15:03:07-04:00 testhost service/web 00000 - - [ERROR] First test message\n",
+		contentFormatter("testhost")(syslog.LOG_INFO, "hostname", "tag", `service/web/00000 1440442987000 [ERROR] First test message`),
 	)
 }
 
