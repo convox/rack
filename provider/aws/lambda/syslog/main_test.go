e24f1134f919bb63aa4864a78f11a3935f63a848
David Dollar <david@dollar.io>
2017-12-04 13:48:49 -0500

    use utc time in syslog

--- provider/aws/lambda/syslog/main_test.go
+++ provider/aws/lambda/syslog/main_test.go
@@ -13,7 +13,7 @@ func TestFormatter(t *testing.T) {
 	os.Setenv("AWS_LAMBDA_FUNCTION_NAME", "convox-syslog-6125")
 
 	assertEqual(t,
-		"<22>1 2015-08-24T15:03:07-04:00 testhost service/web 00000 - - [ERROR] First test message\n",
+		"<22>1 2015-08-24T19:03:07Z testhost service/web 00000 - - [ERROR] First test message\n",
 		contentFormatter("testhost")(syslog.LOG_INFO, "hostname", "tag", `service/web/00000 1440442987000 [ERROR] First test message`),
 	)
 }
