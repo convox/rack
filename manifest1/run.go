052471db2c3c2b15723245d1a945a349d8db0284
David Dollar <david@dollar.io>
2018-01-25 22:43:48 -0500

    correct sync prefix when using context

--- manifest1/run.go
+++ manifest1/run.go
@@ -183,7 +183,7 @@ func (r *Run) Start() error {
 
 			for local, remote := range sp {
 				if remote == "." || strings.HasSuffix(remote, "/") {
-					stat, err := os.Stat(filepath.Join(s.Build.Context, local))
+					stat, err := os.Stat(local)
 					if err == nil && !stat.IsDir() {
 						remote = filepath.Join(remote, filepath.Base(local))
 					}
