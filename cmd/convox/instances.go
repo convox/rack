51f582284e103098224e017703b06f7c7f9a04ad
Feifei Jia <fjia@appannie.com>
2018-01-29 10:19:04 +0800

    add public IP.

--- cmd/convox/instances.go
+++ cmd/convox/instances.go
@@ -60,7 +60,7 @@ func cmdInstancesList(c *cli.Context) error {
 		return stdcli.Error(err)
 	}
 
-	t := stdcli.NewTable("ID", "PrivateIp", "AGENT", "STATUS", "STARTED", "PS", "CPU", "MEM")
+	t := stdcli.NewTable("ID", "AGENT", "STATUS", "STARTED", "PS", "CPU", "MEM", "PUBLIC", "PRIVATE")
 
 	for _, i := range instances {
 		agent := "off"
@@ -68,11 +68,13 @@ func cmdInstancesList(c *cli.Context) error {
 			agent = "on"
 		}
 
-		t.AddRow(i.Id, i.PrivateIp, agent, i.Status,
+		t.AddRow(i.Id, agent, i.Status,
 			helpers.HumanizeTime(i.Started),
 			strconv.Itoa(i.Processes),
 			fmt.Sprintf("%0.2f%%", i.Cpu*100),
-			fmt.Sprintf("%0.2f%%", i.Memory*100))
+			fmt.Sprintf("%0.2f%%", i.Memory*100),
+			i.PublicIp,
+			i.PrivateIp)
 	}
 
 	t.Print()
