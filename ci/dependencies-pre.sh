6d3fd004c8c2f3bbcccfcd526bc44211b3eb0c37
David Dollar <david@dollar.io>
2018-02-27 11:30:33 -0500

    handle docker copies in release

--- ci/dependencies-pre.sh
+++ ci/dependencies-pre.sh
@@ -11,13 +11,8 @@ sudo apt-get install python-pip
 curl -O http://stedolan.github.io/jq/download/linux64/jq && chmod +x jq && sudo mv jq /usr/local/bin
 sudo pip install awscli --upgrade
 
-# build and install with VERSION
-go get -d github.com/convox/rack/cmd/convox
-(
-	cd ${GOPATH%%:*}/src/github.com/convox/rack/cmd/convox
-	[ -n "$CIRCLE_BRANCH" ] && git fetch && git reset --hard origin/$CIRCLE_BRANCH
-	go install -ldflags "-X main.Version=$VERSION"
-)
+# download appropriate cli version
+curl -O $GOPATH/bin/convox https://convox.s3.amazonaws.com/release/$VERSION/cli/linux/convox
 
 # configure client id if on CircleCI
 if [ ! -d "~/.convox/" ]; then
