6c681f25f63f92b74606af6fa46e7a54c1077f21
David Dollar <david@dollar.io>
2018-03-13 09:47:39 -0400

    remove debug print

--- provider/local/event.go
+++ provider/local/event.go
@@ -1,15 +1,10 @@
 package local
 
 import (
-	"fmt"
-
 	"github.com/convox/rack/structs"
 )
 
 func (p *Provider) EventSend(action string, opts structs.EventSendOptions) error {
-	fmt.Println("EventSend")
-	fmt.Printf("action = %+v\n", action)
-	fmt.Printf("opts = %+v\n", opts)
-
+	// unimplemented
 	return nil
 }
