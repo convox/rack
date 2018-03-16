50286dc72ba75e42c613ead18716ae1197bec2fa
David Dollar <david@dollar.io>
2018-03-16 16:45:03 -0400

    support both 5 and 6 terms in cron expressions

--- manifest/timer.go
+++ manifest/timer.go
@@ -1,5 +1,10 @@
 package manifest
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Timer struct {
 	Name string `yaml:"-"`
 
@@ -9,6 +14,17 @@ type Timer struct {
 }
 
 type Timers []Timer
+
+func (t Timer) Cron() (string, error) {
+	switch len(strings.Split(t.Schedule, " ")) {
+	case 5:
+		return fmt.Sprintf("%s *", t.Schedule), nil
+	case 6:
+		return t.Schedule, nil
+	default:
+		return "", fmt.Errorf("invalid schedule expression: %s", t.Schedule)
+	}
+}
 
 func (t Timer) GetName() string {
 	return t.Name
