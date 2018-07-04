88a994ce6c6da0cde37b9bd62a86b9956c733a4a
David Dollar <david@dollar.io>
2018-07-04 14:34:06 -0400

    install correct version

--- ci/install.sh
+++ ci/install.sh
@@ -5,5 +5,5 @@ export CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS:-/tmp/artifacts}
 export CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
 export $($(dirname $0)/region.sh ${CIRCLE_NODE_INDEX})
 
-convox rack install aws --name convox-${CIRCLE_BUILD_NUM} | tee $CIRCLE_ARTIFACTS/convox-installer.log
+convox rack install aws --name convox-${CIRCLE_BUILD_NUM} Version=$VERSION | tee $CIRCLE_ARTIFACTS/convox-installer.log
 convox instances
