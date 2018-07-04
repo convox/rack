61c857647e9b7a11166a57e2d40c92976fd6aa59
David Dollar <david@dollar.io>
2018-07-04 19:18:33 -0400

    fix args for uninstall

--- ci/uninstall.sh
+++ ci/uninstall.sh
@@ -5,4 +5,4 @@ export CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS:-/tmp/artifacts}
 export CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
 export $($(dirname $0)/region.sh ${CIRCLE_NODE_INDEX})
 
-convox rack uninstall aws convox-${CIRCLE_BUILD_NUM} $AWS_REGION --force
+convox rack uninstall aws convox-${CIRCLE_BUILD_NUM} --force
