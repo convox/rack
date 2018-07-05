87e1a8fc37dcdfca4eec298134762c7365919756
David Dollar <david@dollar.io>
2018-07-05 10:15:11 -0400

    bump version check for old api

--- cmd/convox/env.go
+++ cmd/convox/env.go
@@ -153,8 +153,7 @@ func EnvUnset(c *stdcli.Context) error {
 
 	var r *structs.Release
 
-	// TODO version
-	if s.Version <= "20180625222015" {
+	if s.Version <= "20180705140223" {
 		for _, e := range c.Args {
 			r, err = provider(c).EnvironmentUnset(app(c), e)
 			if err != nil {
