9dcf25c1f34de6887a5fd0ed44c26ec03a043870
David Dollar <david@dollar.io>
2018-07-08 22:31:22 -0400

    restore ci scripts

--- ci/test.sh
+++ ci/test.sh
@@ -28,65 +28,64 @@ set -ex
 convox version
 
 # rack
-# convox instances
-# # convox instances keyroll --wait
-# instance=$(convox api get /instances | jq -r '.[0].id')
-# convox instances ssh $instance "ls -la" | grep ec2-user
-# convox rack | grep elb.amazonaws.com
-# convox rack logs --no-follow | grep service/web
-# convox rack ps | grep bin/web
-# convox rack releases
+convox instances
+convox instances keyroll --wait
+instance=$(convox api get /instances | jq -r '.[0].id')
+convox instances ssh $instance "ls -la" | grep ec2-user
+convox rack | grep elb.amazonaws.com
+convox rack logs --no-follow | grep service/web
+convox rack ps | grep bin/web
+convox rack releases
 
 # gen2
 cd $root/examples/httpd
-# convox apps create ci2 --wait
-# convox apps | grep ci2
-# convox apps info ci2 | grep running
-# release=$(convox build -a ci2 -d cibuild --id) && [ -n "$release" ]
-# convox releases -a ci2 | grep $release
-# build=$(convox api get /apps/ci2/builds | jq -r ".[0].id") && [ -n "$build" ]
-# convox builds -a ci2 | grep $build
-# convox builds info $build -a ci2 | grep $build
-# convox builds info $build -a ci2 | grep cibuild
-# convox builds logs $build -a ci2 | grep "docker tag httpd"
-# convox builds export $build -a ci2 -f /tmp/build.tgz
-# releasei=$(convox builds import -a ci2 -f /tmp/build.tgz --id) && [ -n "$releasei" ]
-# buildi=$(convox api get /apps/ci2/releases/$releasei | jq -r ".build") && [ -n "$buildi" ]
-# convox builds info $buildi -a ci2 | grep cibuild
-# echo "FOO=bar" | convox env set -a ci2
-# convox env -a ci2 | grep FOO | grep bar
-# convox env get FOO -a ci2 | grep bar
-# convox env unset FOO -a ci2
-# convox env -a ci2 | grep -v FOO
-# releasee=$(convox env set FOO=bar -a ci2 --id) && [ -n "$releasee" ]
-# convox env get FOO -a ci2 | grep bar
-# convox releases -a ci2 | grep $releasee
-# convox releases info $releasee -a ci2 | grep FOO
-# convox releases manifest $releasee -a ci2 | grep "image: httpd"
-# convox releases promote $release -a ci2 --wait
-# convox logs -a ci2 --no-follow | grep service/web
-# releaser=$(convox releases rollback $release -a ci2 --wait --id)
-# convox ps -a ci2 | grep $releaser
-# ps=$(convox api get /apps/ci2/processes | jq -r '.[0].id')
-# convox ps info $ps -a ci2 | grep $releaser
-# convox scale web --count 2 --cpu 192 --memory 256 -a ci2 --wait
-# convox services -a ci2 | grep web | grep convox.site | grep 443:80
+convox apps create ci2 --wait
+convox apps | grep ci2
+convox apps info ci2 | grep running
+release=$(convox build -a ci2 -d cibuild --id) && [ -n "$release" ]
+convox releases -a ci2 | grep $release
+build=$(convox api get /apps/ci2/builds | jq -r ".[0].id") && [ -n "$build" ]
+convox builds -a ci2 | grep $build
+convox builds info $build -a ci2 | grep $build
+convox builds info $build -a ci2 | grep cibuild
+convox builds logs $build -a ci2 | grep "docker tag httpd"
+convox builds export $build -a ci2 -f /tmp/build.tgz
+releasei=$(convox builds import -a ci2 -f /tmp/build.tgz --id) && [ -n "$releasei" ]
+buildi=$(convox api get /apps/ci2/releases/$releasei | jq -r ".build") && [ -n "$buildi" ]
+convox builds info $buildi -a ci2 | grep cibuild
+echo "FOO=bar" | convox env set -a ci2
+convox env -a ci2 | grep FOO | grep bar
+convox env get FOO -a ci2 | grep bar
+convox env unset FOO -a ci2
+convox env -a ci2 | grep -v FOO
+releasee=$(convox env set FOO=bar -a ci2 --id) && [ -n "$releasee" ]
+convox env get FOO -a ci2 | grep bar
+convox releases -a ci2 | grep $releasee
+convox releases info $releasee -a ci2 | grep FOO
+convox releases manifest $releasee -a ci2 | grep "image: httpd"
+convox releases promote $release -a ci2 --wait
+convox logs -a ci2 --no-follow | grep service/web
+releaser=$(convox releases rollback $release -a ci2 --wait --id)
+convox ps -a ci2 | grep $releaser
+ps=$(convox api get /apps/ci2/processes | jq -r '.[0].id')
+convox ps info $ps -a ci2 | grep $releaser
+convox scale web --count 2 --cpu 192 --memory 256 -a ci2 --wait
+convox services -a ci2 | grep web | grep convox.site | grep 443:80
 endpoint=$(convox api get /apps/ci2/services | jq -r '.[] | select(.name == "web") | .domain')
 fetch https://$endpoint | grep "It works"
-# convox ps -a ci2 | grep web | wc -l | grep 2
-# convox run web "ls -la" -a ci2 | grep htdocs
-# ps=$(convox api get /apps/ci2/processes | jq -r '.[0].id')
-# convox exec $ps "ls -la" -a ci2 | grep htdocs
-# convox ps stop $ps
-# convox deploy -a ci2 --wait
+convox ps -a ci2 | grep web | wc -l | grep 2
+convox run web "ls -la" -a ci2 | grep htdocs
+ps=$(convox api get /apps/ci2/processes | jq -r '.[0].id')
+convox exec $ps "ls -la" -a ci2 | grep htdocs
+convox ps stop $ps
+convox deploy -a ci2 --wait
 
 # gen1
-# convox apps create ci1 -g 1 --wait
-# convox deploy -a ci1 --wait
-# convox services -a ci1 | grep web | grep elb.amazonaws.com | grep 443:80
+convox apps create ci1 -g 1 --wait
+convox deploy -a ci1 --wait
+convox services -a ci1 | grep web | grep elb.amazonaws.com | grep 443:80
 endpoint=$(convox api get /apps/ci1/services | jq -r '.[] | select(.name == "web") | .domain')
 fetch https://$endpoint | grep "It works"
-
 
 # resources
 convox resources create syslog Url=tcp://syslog.convox.com --name cilog --wait
