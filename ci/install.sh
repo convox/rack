64ea2727bb704e448f8118ed0e1f22a2824677f8
David Dollar <david@dollar.io>
2018-07-22 23:02:52 -0400

    support private racks in ci

--- ci/install.sh
+++ ci/install.sh
@@ -5,5 +5,11 @@ export CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS:-/tmp/artifacts}
 export CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
 export $($(dirname $0)/region.sh ${CIRCLE_NODE_INDEX})
 
-convox rack install aws --name convox-${CIRCLE_BUILD_NUM} --version $VERSION | tee $CIRCLE_ARTIFACTS/convox-installer.log
+args=""
+
+if [ "${RACK_PRIVATE}" == "true" ]; then
+  args="${args} Private=Yes"
+fi
+
+convox rack install aws --name convox-${CIRCLE_BUILD_NUM} --version $VERSION $args | tee $CIRCLE_ARTIFACTS/convox-installer.log
 convox instances
