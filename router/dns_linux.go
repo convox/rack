94075a22583156640afa31cca454130762191d3e
ectrimble20 <ectrimble@hotmail.com>
2018-07-30 15:10:40 +0000

    Changed to a switch format for Linux release check, changed to use /etc/os-release to poll for system data

--- router/dns_linux.go
+++ router/dns_linux.go
@@ -7,25 +7,20 @@ import (
 
 func (d *DNS) setupResolver(domain string, ip net.IP) error {
 	version, err := linuxRelease()
-
-	if err != nil {
-		return err
-	}
-
 	switch version {
 	case "ubuntu-18.04":
-		if err := installUbuntu1804(domain, ip); err != nil {
+		if err := installUbuntu1804(); err != nil {
 			return err
 		}
 	default:
-		if err := installGenericLinux(domain, ip); err != nil {
+		if err := installGenericLinux(); err != nil {
 			return err
 		}
 	}
-	return nil
 }
 
-func installUbuntu1804(domain string, ip net.IP) error {
+func installUbuntu1804() error {
+
 	data := []byte(fmt.Sprintf("[Resolve]\nDNS=%s\nDomains=~%s", ip, domain))
 
 	if err := writeFile("/usr/lib/systemd/resolved.conf.d/convox.conf", data); err != nil {
@@ -46,40 +41,22 @@ func installUbuntu1804(domain string, ip net.IP) error {
 	return nil
 }
 
-func installGenericLinux(domain string, ip net.IP) error {
-	if isUbuntu18() {
-		data := []byte(fmt.Sprintf("[Resolve]\nDNS=%s\nDomains=~%s", ip, domain))
-		if err := writeFile("/usr/lib/systemd/resolved.conf.d/convox.conf", data); err != nil {
-			return err
-		}
+func installGenericLinux() error {
 
-		if err := execute("systemctl", "daemon-reload"); err != nil {
-			return err
-		}
+	data := []byte("[main]\ndns=dnsmasq\n")
 
-		if err := execute("systemctl", "restart", "systemd-networkd"); err != nil {
-			return err
-		}
+	if err := writeFile("/etc/NetworkManager/conf.d/convox.conf", data); err != nil {
+		return err
+	}
 
-		if err := execute("systemctl", "restart", "systemd-resolved"); err != nil {
-			return err
-		}
-	} else {
-		data := []byte("[main]\ndns=dnsmasq\n")
+	data = []byte(fmt.Sprintf("server=/%s/%s\n", domain, ip))
 
-		if err := writeFile("/etc/NetworkManager/conf.d/convox.conf", data); err != nil {
-			return err
-		}
+	if err := writeFile(fmt.Sprintf("/etc/NetworkManager/dnsmasq.d/%s", domain), data); err != nil {
+		return err
+	}
 
-		data = []byte(fmt.Sprintf("server=/%s/%s\n", domain, ip))
-
-		if err := writeFile(fmt.Sprintf("/etc/NetworkManager/dnsmasq.d/%s", domain), data); err != nil {
-			return err
-		}
-
-		if err := execute("systemctl", "restart", "NetworkManager"); err != nil {
-			return err
-		}
+	if err := execute("systemctl", "restart", "NetworkManager"); err != nil {
+		return err
 	}
 	return nil
 }
