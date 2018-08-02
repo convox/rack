ddc6b4488c75be1d393fb77c6b5d22ffc67ff404
ectrimble20 <ectrimble@hotmail.com>
2018-08-02 14:32:27 +0000

    applying dns changes for ubuntu 18 compatibility

---
+++ router/helpers_linux.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+)
+
+var kvpair = regexp.MustCompile(`\s*(\w+)\s*=\s*"?([^"]*)`)
+
+func linuxReleaseAttributes() (map[string]string, error) {
+	attrs := map[string]string{}
+	data, err := ioutil.ReadFile("/etc/os-release")
+	if err != nil {
+		return nil, err
+	}
+	s := bufio.NewScanner(bytes.NewReader(data))
+	for s.Scan() {
+		p := kvpair.FindStringSubmatch(s.Text())
+		if len(p) == 3 {
+			attrs[p[1]] = p[2]
+		}
+	}
+	return attrs, nil
+}
+
+func linuxRelease() (string, error) {
+	attrs, err := linuxReleaseAttributes()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s-%s", attrs["ID"], attrs["VERSION_ID"]), nil
+}
