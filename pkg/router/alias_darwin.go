965a0101e25169f701bfb69901bf0e63ba4f9ee6
David Dollar <david@dollar.io>
2018-08-22 18:52:19 -0400

    new router

--- pkg/router/alias_darwin.go
+++
@@ -1,9 +0,0 @@
-package router
-
-func createAlias(iface, ip string) error {
-	if err := execute("ifconfig", iface, "alias", ip, "netmask", "255.255.255.255"); err != nil {
-		return err
-	}
-
-	return nil
-}
