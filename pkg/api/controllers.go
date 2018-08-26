765a8b114992f17e8d21736fd259062e2c7e9ab5
David Dollar <david@dollar.io>
2018-08-26 10:11:44 -0400

    regenerate api

--- pkg/api/controllers.go
+++ pkg/api/controllers.go
@@ -866,29 +866,6 @@ func (s *Server) ProcessStop(c *stdapi.Context) error {
 	return c.RenderOK()
 }
 
-func (s *Server) ProcessWait(c *stdapi.Context) error {
-	if err := s.hook("ProcessWaitValidate", c); err != nil {
-		return err
-	}
-
-	app := c.Var("app")
-	pid := c.Var("pid")
-
-	v, err := s.provider(c).ProcessWait(app, pid)
-	if err != nil {
-		if ae, ok := s.provider(c).(ApiErrorer); ok {
-			return ae.ApiError(err)
-		}
-		return err
-	}
-
-	if vs, ok := interface{}(v).(Sortable); ok {
-		sort.Slice(v, vs.Less)
-	}
-
-	return c.RenderText(strconv.Itoa(v))
-}
-
 func (s *Server) Proxy(c *stdapi.Context) error {
 	if err := s.hook("ProxyValidate", c); err != nil {
 		return err
