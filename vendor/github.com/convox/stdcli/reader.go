cae55a09f695d1fa2135add847f745f904c64fae
David Dollar <david@dollar.io>
2018-08-29 06:48:03 -0600

    update stdcli

--- vendor/github.com/convox/stdcli/reader.go
+++ vendor/github.com/convox/stdcli/reader.go
@@ -1,23 +1,47 @@
 package stdcli
 
-import "os"
+import (
+	"io"
+	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
 
 var (
 	DefaultReader *Reader
 )
 
 type Reader struct {
-	*os.File
+	io.Reader
 }
 
 func init() {
-	DefaultReader = &Reader{File: os.Stdin}
-}
-
-func (r *Reader) Fd() uintptr {
-	return r.File.Fd()
+	DefaultReader = &Reader{os.Stdin}
 }
 
 func (r *Reader) IsTerminal() bool {
-	return IsTerminal(r.File)
+	if f, ok := r.Reader.(*os.File); ok {
+		return IsTerminal(f)
+	}
+	return false
+}
+
+func (r *Reader) TerminalRaw() func() bool {
+	var fd int
+	var state *terminal.State
+
+	if f, ok := r.Reader.(*os.File); ok {
+		fd = int(f.Fd())
+		if s, err := terminal.MakeRaw(fd); err == nil {
+			state = s
+		}
+	}
+
+	return func() bool {
+		if state != nil {
+			terminal.Restore(fd, state)
+			return true
+		}
+		return false
+	}
 }
