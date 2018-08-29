244b2605d9bbb2eb4cd243256f888ab4f478b60d
David Dollar <david@dollar.io>
2018-08-29 08:01:03 -0600

    tests for Apps and AppsCancel

--- cmd/convox/main.go
+++ cmd/convox/main.go
@@ -4,7 +4,6 @@ import (
 	"os"
 
 	"github.com/convox/rack/pkg/cli"
-	"github.com/convox/stdcli"
 )
 
 var (
@@ -13,17 +12,6 @@ var (
 
 func main() {
 	c := cli.New("convox", version)
-
-	c.Writer.Tags["app"] = stdcli.RenderColors(39)
-	c.Writer.Tags["build"] = stdcli.RenderColors(23)
-	c.Writer.Tags["rack"] = stdcli.RenderColors(26)
-	c.Writer.Tags["process"] = stdcli.RenderColors(27)
-	c.Writer.Tags["release"] = stdcli.RenderColors(24)
-	c.Writer.Tags["service"] = stdcli.RenderColors(25)
-
-	if dir := os.Getenv("CONVOX_CONFIG"); dir != "" {
-		c.Settings = dir
-	}
 
 	os.Exit(c.Execute(os.Args[1:]))
 }
