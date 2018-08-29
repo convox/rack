4a26416eb8d4e160bf233d1f46ced1e9a0d880ee
David Dollar <david@dollar.io>
2018-08-29 11:54:03 -0600

    test classic build

--- pkg/cli/builds_test.go
+++ pkg/cli/builds_test.go
@@ -78,3 +78,26 @@ func TestBuildError(t *testing.T) {
 		res.RequireStderr(t, []string{"ERROR: err1"})
 	})
 }
+
+func TestBuildClassic(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("SystemGet").Return(&fxSystemClassic, nil)
+		i.On("BuildCreateUpload", "app1", mock.Anything, structs.BuildCreateOptions{}).Return(&fxBuild, nil)
+		i.On("BuildLogs", "app1", "build1", structs.LogsOptions{}).Return(testLogs(fxLogs), nil)
+		i.On("BuildGet", "app1", "build1").Return(&fxBuildRunning, nil).Twice()
+		i.On("BuildGet", "app1", "build1").Return(&fxBuild, nil)
+
+		res, err := testExecute(e, "build ./testdata/httpd -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, res.Code)
+		require.Equal(t, "", res.Stderr)
+		res.RequireStdout(t, []string{
+			"Packaging source... OK",
+			"Starting build... OK",
+			"log1",
+			"log2",
+			"Build:   build1",
+			"Release: release1",
+		})
+	})
+}
