cb69bd1537913e890762ebcb40b24b5e5cd82d1a
David Dollar <david@dollar.io>
2018-08-30 10:23:14 -0600

    test for instances

--- pkg/api/instance_test.go
+++ pkg/api/instance_test.go
@@ -119,39 +119,3 @@ func TestInstanceTerminateError(t *testing.T) {
 		require.EqualError(t, err, "err1")
 	})
 }
-
-// func TestFilesDeleteError(t *testing.T) {
-//   testServer(t, func(c *stdsdk.Client, p *structs.MockProvider) {
-//     opts := stdsdk.RequestOptions{
-//       Query: stdsdk.Query{
-//         "files": "file1,file2",
-//       },
-//     }
-//     p.On("FilesDelete", "app1", "pid1", []string{"file1", "file2"}).Return(fmt.Errorf("err1"))
-//     err := c.Delete("/apps/app1/processes/pid1/files", opts, nil)
-//     require.EqualError(t, err, "err1")
-//   })
-// }
-
-// func TestFilesUpload(t *testing.T) {
-//   testServer(t, func(c *stdsdk.Client, p *structs.MockProvider) {
-//     opts := stdsdk.RequestOptions{
-//       Body: strings.NewReader("data"),
-//     }
-//     p.On("FilesUpload", "app1", "pid1", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
-//       data, err := ioutil.ReadAll(args.Get(2).(io.Reader))
-//       require.NoError(t, err)
-//       require.Equal(t, "data", string(data))
-//     })
-//     err := c.Post("/apps/app1/processes/pid1/files", opts, nil)
-//     require.NoError(t, err)
-//   })
-// }
-
-// func TestFilesUploadError(t *testing.T) {
-//   testServer(t, func(c *stdsdk.Client, p *structs.MockProvider) {
-//     p.On("FilesUpload", "app1", "pid1", mock.Anything).Return(fmt.Errorf("err1"))
-//     err := c.Post("/apps/app1/processes/pid1/files", stdsdk.RequestOptions{}, nil)
-//     require.EqualError(t, err, "err1")
-//   })
-// }
