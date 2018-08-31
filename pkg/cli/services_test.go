b05bb599b830f79b7d5b9161e653d86af7468065
David Dollar <david@dollar.io>
2018-08-31 15:55:37 -0400

    tests for services

--- pkg/cli/services_test.go
+++ pkg/cli/services_test.go
@@ -1,6 +1,14 @@
 package cli_test
 
-import "github.com/convox/rack/pkg/structs"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/convox/rack/pkg/cli"
+	mocksdk "github.com/convox/rack/pkg/mock/sdk"
+	"github.com/convox/rack/pkg/structs"
+	"github.com/stretchr/testify/require"
+)
 
 var fxService = structs.Service{
 	Name:   "service1",
@@ -12,4 +20,51 @@ var fxService = structs.Service{
 		{Balancer: 1, Certificate: "cert1", Container: 2},
 		{Balancer: 1, Certificate: "cert1", Container: 2},
 	},
+}
+
+func TestServices(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("SystemGet").Return(&fxSystem, nil)
+		i.On("ServiceList", "app1").Return(structs.Services{fxService, fxService}, nil)
+
+		res, err := testExecute(e, "services -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, res.Code)
+		res.RequireStderr(t, []string{""})
+		res.RequireStdout(t, []string{
+			"SERVICE   DOMAIN  PORTS  ",
+			"service1  domain  1:2 1:2",
+			"service1  domain  1:2 1:2",
+		})
+	})
+}
+
+func TestServicesError(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("SystemGet").Return(&fxSystem, nil)
+		i.On("ServiceList", "app1").Return(nil, fmt.Errorf("err1"))
+
+		res, err := testExecute(e, "services -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 1, res.Code)
+		res.RequireStderr(t, []string{"ERROR: err1"})
+		res.RequireStdout(t, []string{""})
+	})
+}
+
+func TestServicesClassic(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("SystemGet").Return(&fxSystemClassic, nil)
+		i.On("FormationGet", "app1").Return(structs.Services{fxService, fxService}, nil)
+
+		res, err := testExecute(e, "services -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, res.Code)
+		res.RequireStderr(t, []string{""})
+		res.RequireStdout(t, []string{
+			"SERVICE   DOMAIN  PORTS  ",
+			"service1  domain  1:2 1:2",
+			"service1  domain  1:2 1:2",
+		})
+	})
 }
