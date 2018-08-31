c404aee00a05d07857de04416a925f634d5ad79f
David Dollar <david@dollar.io>
2018-08-31 07:53:00 -0400

    tests for ps

---
+++ pkg/cli/ps_test.go
@@ -0,0 +1,113 @@
+package cli_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/convox/rack/pkg/cli"
+	mocksdk "github.com/convox/rack/pkg/mock/sdk"
+	"github.com/convox/rack/pkg/structs"
+	"github.com/stretchr/testify/require"
+)
+
+var fxProcess = structs.Process{
+	Id:       "pid1",
+	App:      "app1",
+	Command:  "command",
+	Cpu:      1.0,
+	Host:     "host",
+	Image:    "image",
+	Instance: "instance",
+	Memory:   2.0,
+	Name:     "name",
+	Ports:    []string{"1000", "2000"},
+	Release:  "release1",
+	Started:  time.Now().UTC().Add(-49 * time.Hour),
+	Status:   "status",
+}
+
+func TestPs(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("ProcessList", "app1", structs.ProcessListOptions{}).Return(structs.Processes{fxProcess, fxProcess}, nil)
+
+		res, err := testExecute(e, "ps -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, res.Code)
+		res.RequireStderr(t, []string{""})
+		res.RequireStdout(t, []string{
+			"ID    SERVICE  STATUS  RELEASE   STARTED     COMMAND",
+			"pid1  name     status  release1  2 days ago  command",
+			"pid1  name     status  release1  2 days ago  command",
+		})
+	})
+}
+
+func TestPsError(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("ProcessList", "app1", structs.ProcessListOptions{}).Return(nil, fmt.Errorf("err1"))
+
+		res, err := testExecute(e, "ps -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 1, res.Code)
+		res.RequireStderr(t, []string{"ERROR: err1"})
+		res.RequireStdout(t, []string{""})
+	})
+}
+
+func TestPsInfo(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("ProcessGet", "app1", "pid1").Return(&fxProcess, nil)
+
+		res, err := testExecute(e, "ps info pid1 -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, res.Code)
+		res.RequireStderr(t, []string{""})
+		res.RequireStdout(t, []string{
+			"Id        pid1",
+			"App       app1",
+			"Command   command",
+			"Instance  instance",
+			"Release   release1",
+			"Service   name",
+			"Started   2 days ago",
+			"Status    status",
+		})
+	})
+}
+
+func TestPsInfoError(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("ProcessGet", "app1", "pid1").Return(nil, fmt.Errorf("err1"))
+
+		res, err := testExecute(e, "ps info pid1 -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 1, res.Code)
+		res.RequireStderr(t, []string{"ERROR: err1"})
+		res.RequireStdout(t, []string{""})
+	})
+}
+
+func TestPsStop(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("ProcessStop", "app1", "pid1").Return(nil)
+
+		res, err := testExecute(e, "ps stop pid1 -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 0, res.Code)
+		res.RequireStderr(t, []string{""})
+		res.RequireStdout(t, []string{"Stopping pid1... OK"})
+	})
+}
+
+func TestPsStopError(t *testing.T) {
+	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
+		i.On("ProcessStop", "app1", "pid1").Return(fmt.Errorf("err1"))
+
+		res, err := testExecute(e, "ps stop pid1 -a app1", nil)
+		require.NoError(t, err)
+		require.Equal(t, 1, res.Code)
+		res.RequireStderr(t, []string{"ERROR: err1"})
+		res.RequireStdout(t, []string{"Stopping pid1... "})
+	})
+}
