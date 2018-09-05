c5d71b31f75a91740cecdcb2c55175b7a4383b2a
David Dollar <david@dollar.io>
2018-09-04 22:23:35 -0400

    move build into pkg

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"time"
 
-	"github.com/convox/rack/cmd/build/source"
-	"github.com/convox/rack/pkg/helpers"
-	"github.com/convox/rack/pkg/manifest"
-	"github.com/convox/rack/pkg/manifest1"
-	"github.com/convox/rack/pkg/options"
+	"github.com/convox/rack/pkg/build"
 	"github.com/convox/rack/pkg/structs"
 	"github.com/convox/rack/sdk"
 )
@@ -42,7 +30,15 @@ var (
 )
 
 func main() {
+	if err := execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func execute() error {
 	fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+
 	fs.StringVar(&flagApp, "app", "example", "app name")
 	fs.StringVar(&flagAuth, "auth", "", "docker auth data (json)")
 	fs.StringVar(&flagCache, "cache", "true", "use docker cache")
@@ -55,7 +51,7 @@ func main() {
 	fs.StringVar(&flagUrl, "url", "", "source url")
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
-		fail(err)
+		return err
 	}
 
 	if v := os.Getenv("BUILD_APP"); v != "" {
@@ -90,296 +86,26 @@ func main() {
 		flagUrl = v
 	}
 
-	if flagManifest == "" {
-		switch flagGeneration {
-		case "2":
-			flagManifest = "convox.yml"
-		default:
-			flagManifest = "docker-compose.yml"
-		}
+	opts := build.Options{
+		App:        flagApp,
+		Auth:       flagAuth,
+		Cache:      flagCache == "true",
+		Generation: flagGeneration,
+		Id:         flagID,
+		Manifest:   flagManifest,
+		Push:       flagPush,
+		Rack:       flagRack,
+		Source:     flagUrl,
 	}
 
-	var err error
-
-	rack, err = sdk.NewFromEnv()
-	if err != nil {
-		fail(err)
-	}
-
-	if err := execute(); err != nil {
-		fail(err)
-	}
-
-	if err := success(); err != nil {
-		fail(err)
-	}
-
-	rack.EventSend("build:create", structs.EventSendOptions{Data: map[string]string{"app": flagApp, "id": flagID, "release_id": currentBuild.Release}})
-
-	clean()
-
-	time.Sleep(1 * time.Second)
-}
-
-func execute() error {
-	b, err := rack.BuildGet(flagApp, flagID)
+	b, err := build.New(opts)
 	if err != nil {
 		return err
 	}
 
-	currentBuild = b
-
-	if err := login(); err != nil {
-		return err
-	}
-
-	dir, err := fetch()
-	if err != nil {
-		return err
-	}
-
-	defer os.RemoveAll(dir)
-
-	data, err := ioutil.ReadFile(filepath.Join(dir, flagManifest))
-	if err != nil {
-		return err
-	}
-
-	currentBuild.Manifest = string(data)
-
-	switch flagGeneration {
-	case "2":
-		if err := build2(dir); err != nil {
-			return err
-		}
-	default:
-		if err := build(dir); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func fetch() (string, error) {
-	var s source.Source
-
-	switch flagMethod {
-	case "git":
-		s = &source.SourceGit{flagUrl}
-	case "tgz":
-		s = &source.SourceTgz{flagUrl}
-	case "zip":
-		s = &source.SourceZip{flagUrl}
-	default:
-		return "", fmt.Errorf("unknown method: %s", flagMethod)
-	}
-
-	var buf bytes.Buffer
-
-	dir, err := s.Fetch(&buf)
-	logf(buf.String())
-	if err != nil {
-		return "", err
-	}
-
-	return dir, nil
-}
-
-func login() error {
-	var auth map[string]struct {
-		Username string
-		Password string
-	}
-
-	if err := json.Unmarshal([]byte(flagAuth), &auth); err != nil {
-		return err
-	}
-
-	for host, entry := range auth {
-		out, err := exec.Command("docker", "login", "-u", entry.Username, "-p", entry.Password, host).CombinedOutput()
-		log(fmt.Sprintf("Authenticating %s: %s", host, strings.TrimSpace(string(out))))
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func build(dir string) error {
-	dcy := filepath.Join(dir, flagManifest)
-
-	if _, err := os.Stat(dcy); os.IsNotExist(err) {
-		return fmt.Errorf("no such file: %s", flagManifest)
-	}
-
-	data, err := ioutil.ReadFile(dcy)
-	if err != nil {
-		return err
-	}
-
-	m, err := manifest1.Load(data)
-	if err != nil {
-		return err
-	}
-
-	errs := m.Validate()
-	if len(errs) > 0 {
-		return errs[0]
-	}
-
-	s := make(chan string)
-
-	go func() {
-		for l := range s {
-			log(l)
-		}
-	}()
-
-	defer close(s)
-
-	env, err := helpers.AppEnvironment(rack, flagApp)
-	if err != nil {
-		return err
-	}
-
-	err = m.Build(dir, flagApp, s, manifest1.BuildOptions{
-		Environment: env,
-		Cache:       flagCache == "true",
-		Verbose:     false,
-	})
-	if err != nil {
-		return err
-	}
-
-	if err := m.Push(flagPush, flagApp, flagID, s); err != nil {
+	if err := b.Execute(); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func build2(dir string) error {
-	config := filepath.Join(dir, flagManifest)
-
-	if _, err := os.Stat(config); os.IsNotExist(err) {
-		return fmt.Errorf("no such file: %s", flagManifest)
-	}
-
-	data, err := ioutil.ReadFile(config)
-	if err != nil {
-		return err
-	}
-
-	env, err := helpers.AppEnvironment(rack, flagApp)
-	if err != nil {
-		return err
-	}
-
-	m, err := manifest.Load(data, env)
-	if err != nil {
-		return err
-	}
-
-	r, w := io.Pipe()
-
-	go func() {
-		buf := make([]byte, 1024)
-
-		for {
-			n, err := r.Read(buf)
-			if err != nil {
-				if err != io.EOF {
-					log(fmt.Sprintf("ERROR: %s\n", err))
-				}
-				return
-			}
-			line := string(buf[0:n])
-			currentLogs += line
-			fmt.Print(line)
-		}
-	}()
-
-	prefix := fmt.Sprintf("%s/%s", flagRack, flagApp)
-
-	err = m.Build(prefix, flagID, manifest.BuildOptions{
-		Cache:  flagCache == "true",
-		Env:    env,
-		Push:   flagPush,
-		Root:   dir,
-		Stdout: w,
-		Stderr: w,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func success() error {
-	logs, err := rack.ObjectStore(flagApp, fmt.Sprintf("build/%s/logs", currentBuild.Id), bytes.NewReader([]byte(currentLogs)), structs.ObjectStoreOptions{})
-	if err != nil {
-		return err
-	}
-
-	opts := structs.BuildUpdateOptions{
-		Ended:    options.Time(time.Now()),
-		Logs:     options.String(logs.Url),
-		Manifest: options.String(currentBuild.Manifest),
-	}
-
-	if _, err := rack.BuildUpdate(flagApp, currentBuild.Id, opts); err != nil {
-		return err
-	}
-
-	r, err := rack.ReleaseCreate(flagApp, structs.ReleaseCreateOptions{Build: options.String(currentBuild.Id)})
-	if err != nil {
-		return err
-	}
-
-	opts = structs.BuildUpdateOptions{
-		Release: options.String(r.Id),
-		Status:  options.String("complete"),
-	}
-
-	if _, err := rack.BuildUpdate(flagApp, currentBuild.Id, opts); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func fail(err error) {
-	log(fmt.Sprintf("ERROR: %s", err))
-
-	rack.EventSend("build:create", structs.EventSendOptions{Data: map[string]string{"app": flagApp, "id": flagID, "release_id": currentBuild.Release}, Error: options.String(err.Error())})
-
-	logs, _ := rack.ObjectStore(flagApp, fmt.Sprintf("build/%s/logs", currentBuild.Id), bytes.NewReader([]byte(currentLogs)), structs.ObjectStoreOptions{})
-
-	now := time.Now()
-	status := "failed"
-
-	opts := structs.BuildUpdateOptions{
-		Ended:  options.Time(now),
-		Logs:   options.String(logs.Url),
-		Status: options.String(status),
-	}
-
-	if _, err := rack.BuildUpdate(flagApp, currentBuild.Id, opts); err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
-	}
-
-	os.Exit(1)
-}
-
-func log(line string) {
-	logf("%s\n", line)
-}
-
-func logf(f string, args ...interface{}) {
-	s := fmt.Sprintf(f, args...)
-	currentLogs += s
-	fmt.Print(s)
 }
---
+++ pkg/build/docker.go
@@ -0,0 +1,152 @@
+package build
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func (bb *Build) build(path, dockerfile string, tag string, env map[string]string) error {
+	if path == "" {
+		return fmt.Errorf("must have path to build")
+	}
+
+	df := filepath.Join(path, dockerfile)
+
+	args := []string{"build"}
+
+	if !bb.Cache {
+		args = append(args, "--no-cache")
+	}
+
+	args = append(args, "-t", tag)
+	args = append(args, "-f", df)
+
+	ba, err := bb.buildArgs(df, env)
+	if err != nil {
+		return err
+	}
+
+	args = append(args, ba...)
+
+	args = append(args, path)
+
+	if err := bb.Exec.Run(bb.writer, "docker", args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (bb *Build) buildArgs(dockerfile string, env map[string]string) ([]string, error) {
+	fd, err := os.Open(dockerfile)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	s := bufio.NewScanner(fd)
+
+	args := []string{}
+
+	for s.Scan() {
+		fields := strings.Fields(strings.TrimSpace(s.Text()))
+
+		if len(fields) < 2 {
+			continue
+		}
+
+		parts := strings.Split(fields[1], "=")
+
+		switch fields[0] {
+		case "FROM":
+			if bb.Development && strings.Contains(strings.ToLower(s.Text()), "as development") {
+				args = append(args, "--target", "development")
+			}
+		case "ARG":
+			k := strings.TrimSpace(parts[0])
+			if v, ok := env[k]; ok {
+				args = append(args, "--build-arg", fmt.Sprintf("%s=%s", k, v))
+			}
+		}
+	}
+
+	return args, nil
+}
+
+func (bb *Build) injectConvoxEnv(tag string) error {
+	var cmd []string
+	var entrypoint []string
+
+	data, err := bb.Exec.Execute("docker", "inspect", tag, "--format", "{{json .Config.Cmd}}")
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, &cmd); err != nil {
+		return err
+	}
+
+	data, err = bb.Exec.Execute("docker", "inspect", tag, "--format", "{{json .Config.Entrypoint}}")
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, &entrypoint); err != nil {
+		return err
+	}
+
+	epb, err := json.Marshal(append([]string{"/convox-env"}, entrypoint...))
+	if err != nil {
+		return err
+	}
+
+	epdfs := fmt.Sprintf("FROM %s\nCOPY ./convox-env /convox-env\nENTRYPOINT %s\n", tag, epb)
+
+	if cmd != nil {
+		cmdb, err := json.Marshal(cmd)
+		if err != nil {
+			return err
+		}
+
+		epdfs += fmt.Sprintf("CMD %s\n", cmdb)
+	}
+
+	tmp, err := ioutil.TempDir("", "")
+	if err != nil {
+		return err
+	}
+
+	if _, err := bb.Exec.Execute("cp", "/go/bin/convox-env", filepath.Join(tmp, "convox-env")); err != nil {
+		return err
+	}
+
+	epdf := filepath.Join(tmp, "Dockerfile")
+
+	if err := ioutil.WriteFile(epdf, []byte(epdfs), 0644); err != nil {
+		return err
+	}
+
+	data, err = bb.Exec.Execute("docker", "build", "-t", tag, tmp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (bb *Build) pull(tag string) error {
+	_, err := bb.Exec.Execute("docker", "pull", tag)
+	return err
+}
+
+func (bb *Build) push(tag string) error {
+	_, err := bb.Exec.Execute("docker", "push", tag)
+	return err
+}
+
+func (bb *Build) tag(from, to string) error {
+	return bb.Exec.Run(bb.writer, "docker", "tag", from, to)
+}
