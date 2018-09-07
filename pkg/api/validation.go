5eb13b878e42d3fbc23c116e4fa152b8f3cf702a
David Dollar <david@dollar.io>
2018-09-07 07:31:22 -0400

    add validation to release promote

--- pkg/api/validation.go
+++ pkg/api/validation.go
@@ -16,3 +16,16 @@ func (s *Server) AppCancelValidate(c *stdapi.Context) error {
 
 	return nil
 }
+
+func (s *Server) ReleasePromoteValidate(c *stdapi.Context) error {
+	a, err := s.Provider.AppGet(c.Var("app"))
+	if err != nil {
+		return err
+	}
+
+	if a.Status != "running" {
+		return stdapi.Errorf(403, "app is currently updating")
+	}
+
+	return nil
+}
