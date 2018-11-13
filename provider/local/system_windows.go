91359d4dcc6fa9a15d30475ace3bcb829a9eadf6
David Dollar <david@dollar.io>
2018-11-13 09:23:00 -0500

    add dns support for windows, check for powershell as admin

--- provider/local/system_windows.go
+++ provider/local/system_windows.go
@@ -3,18 +3,21 @@ package local
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func checkPermissions() error {
+	data, err := powershell(`([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")`)
+	if err != nil || strings.TrimSpace(string(data)) != "True" {
+		return fmt.Errorf("must be run in powershell running as administrator")
+	}
+
 	return nil
 }
 
 func dnsInstall(name string) error {
-	data, err := powershell(fmt.Sprintf(`Add-DnsClientNrptRule -Namespace ".%s" -NameServers "127.0.0.1"`, name))
-	fmt.Printf("string(data) = %+v\n", string(data))
-	fmt.Printf("err = %+v\n", err)
-	if err != nil {
-		return err
+	if _, err := powershell(fmt.Sprintf(`Add-DnsClientNrptRule -Namespace ".%s" -NameServers "127.0.0.1"`, name)); err != nil {
+		return fmt.Errorf("unable to install dns handlers")
 	}
 
 	return nil
@@ -29,7 +32,7 @@ func dnsUninstall(name string) error {
 }
 
 func powershell(command string) ([]byte, error) {
-	return exec.Command("powershell.exe", "-Sta", "-NonInteractive", "-ExecutionPolicy", "RemoteSigned", "-EncodedCommand", command).CombinedOutput()
+	return exec.Command("powershell.exe", "-Sta", "-NonInteractive", "-ExecutionPolicy", "RemoteSigned", command).CombinedOutput()
 }
 
 func removeOriginalRack(name string) error {
