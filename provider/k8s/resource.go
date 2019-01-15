fa9ff2ec028f42aa17e7e99aaa2a8fd0d32bee2f
David Dollar <david@dollar.io>
2019-01-15 11:41:55 -0500

    accomodate rebase

--- provider/k8s/resource.go
+++ provider/k8s/resource.go
@@ -6,34 +6,42 @@ import (
 	"github.com/convox/rack/pkg/structs"
 )
 
-func (p *Provider) ResourceCreate(kind string, opts structs.ResourceCreateOptions) (*structs.Resource, error) {
+func (p *Provider) ResourceGet(app, name string) (*structs.Resource, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceDelete(name string) error {
+func (p *Provider) ResourceList(app string) (structs.Resources, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+
+func (p *Provider) SystemResourceCreate(kind string, opts structs.ResourceCreateOptions) (*structs.Resource, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+
+func (p *Provider) SystemResourceDelete(name string) error {
 	return fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceGet(name string) (*structs.Resource, error) {
+func (p *Provider) SystemResourceGet(name string) (*structs.Resource, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceLink(name, app string) (*structs.Resource, error) {
+func (p *Provider) SystemResourceLink(name, app string) (*structs.Resource, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceList() (structs.Resources, error) {
+func (p *Provider) SystemResourceList() (structs.Resources, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceTypes() (structs.ResourceTypes, error) {
+func (p *Provider) SystemResourceTypes() (structs.ResourceTypes, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceUnlink(name, app string) (*structs.Resource, error) {
+func (p *Provider) SystemResourceUnlink(name, app string) (*structs.Resource, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (p *Provider) ResourceUpdate(name string, opts structs.ResourceUpdateOptions) (*structs.Resource, error) {
+func (p *Provider) SystemResourceUpdate(name string, opts structs.ResourceUpdateOptions) (*structs.Resource, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
