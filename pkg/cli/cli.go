22ccd54fac7b3cd99b80804399b9f81b8a30bb39
Radosław Lisowski <radoslaw.lisowski@schibsted.com>
2019-01-23 14:48:38 +0100

    moved out flag replace to be local for EnvSet command

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -23,7 +23,6 @@ var (
 	flagNoFollow = stdcli.BoolFlag("no-follow", "", "do not follow logs")
 	flagRack     = stdcli.StringFlag("rack", "r", "rack name")
 	flagWait     = stdcli.BoolFlag("wait", "w", "wait for completion")
-	flagReplace  = stdcli.BoolFlag("replace", "", "replace all environment variables with given ones")
 )
 
 func New(name, version string) *Engine {
