e63f07d906f9185119ce3527edfe5b71a2833bbd
David Dollar <david@dollar.io>
2019-03-06 18:15:25 -0500

    fix router tests

--- provider/k8s/k8s_test.go
+++ provider/k8s/k8s_test.go
@@ -4,16 +4,18 @@ import (
 	"testing"
 
 	"github.com/convox/rack/provider/k8s"
+	"github.com/stretchr/testify/require"
 	fakek8s "k8s.io/client-go/kubernetes/fake"
 )
 
 func testProvider(t *testing.T, fn func(*k8s.Provider, *fakek8s.Clientset)) {
 	c := fakek8s.NewSimpleClientset()
 
-	p := &k8s.Provider{
-		Cluster: c,
-		Rack:    "test",
-	}
+	p, err := k8s.FromEnv()
+	require.NoError(t, err)
+
+	p.Cluster = c
+	p.Rack = "test"
 
 	fn(p, c)
 }
