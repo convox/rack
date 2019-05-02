a56b8d1d2778fe071b15989197d3f64c215b43fa
David Dollar <david@dollar.io>
2019-05-02 15:09:18 -0400

    use circleci workflows

--- ci/install.sh
+++ ci/install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -ex -o pipefail
 
-export CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS:-/tmp/artifacts}
-export CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
-export $($(dirname $0)/region.sh ${CIRCLE_NODE_INDEX})
+source $(dirname $0)/env.sh
 
-args=""
-
-if [ "${RACK_PRIVATE}" == "true" ]; then
-  args="${args} Private=Yes"
-fi
-
-convox rack install aws --name convox-${CIRCLE_BUILD_NUM} --version $VERSION $args | tee $CIRCLE_ARTIFACTS/convox-installer.log
+convox rack install ${PROVIDER} --name ${RACK_NAME} --version ${VERSION} ${ARGS}
 convox instances
