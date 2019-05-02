db3a830b2d4a70dd985f2ca0c30634e90a8e7a45
David Dollar <david@dollar.io>
2019-05-02 12:29:19 -0400

    wip

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -6,6 +6,10 @@ source $(dirname $0)/env.sh
 # install utilities
 sudo apt-get update && sudo apt-get -y install awscli jq
 
+# install kubectl
+curl -Ls https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl -o /tmp/kubectl && \
+	sudo mv /tmp/kubectl /usr/bin/kubectl && sudo chmod +x /usr/bin/kubectl
+
 # download appropriate cli version
 curl -o ${GOPATH}/bin/convox https://convox.s3.amazonaws.com/release/${VERSION}/cli/linux/convox
 chmod +x ${GOPATH}/bin/convox
