85f121410849d2f668076decb09045a2b5c574ca
David Dollar <david@dollar.io>
2019-05-20 09:45:14 -0400

    prevent promotion of a release with an empty manifest

--- pkg/api/validation.go
+++ pkg/api/validation.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/convox/stdapi"
 )
@@ -36,7 +37,9 @@ func (s *Server) ProcessRunValidate(c *stdapi.Context) error {
 }
 
 func (s *Server) ReleasePromoteValidate(c *stdapi.Context) error {
-	a, err := s.Provider.AppGet(c.Var("app"))
+	app := c.Var("app")
+
+	a, err := s.Provider.AppGet(app)
 	if err != nil {
 		return err
 	}
@@ -45,18 +48,22 @@ func (s *Server) ReleasePromoteValidate(c *stdapi.Context) error {
 		return stdapi.Errorf(403, "app is currently updating")
 	}
 
+	r, err := s.Provider.ReleaseGet(app, c.Var("id"))
+	if err != nil {
+		return err
+	}
+
+	if strings.TrimSpace(r.Manifest) == "" {
+		return fmt.Errorf("can not promote a release with an empty manifest")
+	}
+
 	if a.Release != "" {
-		or, err := s.Provider.ReleaseGet(c.Var("app"), a.Release)
+		or, err := s.Provider.ReleaseGet(app, a.Release)
 		if err != nil {
 			return err
 		}
 
-		nr, err := s.Provider.ReleaseGet(c.Var("app"), c.Var("id"))
-		if err != nil {
-			return err
-		}
-
-		if nr.Created.Before(or.Created) {
+		if r.Created.Before(or.Created) {
 			return fmt.Errorf("can not promote an older release, try rollback")
 		}
 	}
