05434096991435b3ecd9cd89972c913f8f62f607
David Dollar <david@dollar.io>
2019-07-31 06:16:06 -0700

    install ca certificate into docker desktop vm

--- provider/local/helpers.go
+++ provider/local/helpers.go
@@ -1,7 +1,9 @@
 package local
 
 import (
+	"bytes"
 	"context"
+	"os/exec"
 	"time"
 )
 
@@ -23,4 +25,29 @@ func (p *Provider) watchForProcessCompletion(ctx context.Context, app, pid strin
 			}
 		}
 	}
+}
+
+func dockerDesktopCommand(command string) *exec.Cmd {
+	return exec.Command("docker", "run", "-i", "--privileged", "--rm", "--privileged", "--pid=host", "debian", "nsenter", "-t", "1", "-m", "-u", "-n", "-i", "sh", "-c", command)
+}
+
+func installDockerDesktopCertificate(data []byte) error {
+	cmd := dockerDesktopCommand("cat >> /run/config/etc/ssl/certs/ca-certificates.crt")
+	cmd.Stdin = bytes.NewReader(data)
+	cmd.Run()
+
+	dockerDesktopCommand("killall -HUP dockerd").Run()
+
+	exec.Command("killall", "Docker").Run()
+	exec.Command("open", "-a", "Docker", "--background").Run()
+
+	for {
+		if err := checkKubectl(); err == nil {
+			break
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
