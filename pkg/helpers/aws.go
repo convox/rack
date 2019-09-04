a887d3c4800f90c38dbec3cead5e4c6e825d6e3b
David Dollar <david@dollar.io>
2019-09-04 09:00:50 -0400

    handle the fact that CloudWatchLogs:FilterLogEvents always returns a NextToken now

--- pkg/helpers/aws.go
+++ pkg/helpers/aws.go
@@ -367,6 +367,13 @@ func CloudWatchLogsStream(ctx context.Context, cw cloudwatchlogsiface.CloudWatch
 		req.StartTime = aws.Int64(start)
 	}
 
+	follow := DefaultBool(opts.Follow, true)
+
+	if !follow {
+		end := time.Now().UTC().UnixNano() / int64(time.Millisecond)
+		req.EndTime = aws.Int64(end)
+	}
+
 	var seen = map[string]bool{}
 
 	sleep := time.Duration(100 * time.Millisecond)
@@ -423,16 +430,24 @@ func CloudWatchLogsStream(ctx context.Context, cw cloudwatchlogsiface.CloudWatch
 				return err
 			}
 
-			if res.NextToken != nil {
+			done := len(res.SearchedLogStreams) > 0
+
+			for _, s := range res.SearchedLogStreams {
+				if s.SearchedCompletely == nil || !*s.SearchedCompletely {
+					done = false
+				}
+			}
+
+			if !done && res.NextToken != nil {
 				req.NextToken = res.NextToken
 				continue
 			}
 
-			req.NextToken = nil
-
-			if !DefaultBool(opts.Follow, true) {
+			if !follow {
 				return nil
 			}
+
+			req.NextToken = nil
 
 			req.StartTime = aws.Int64(start)
 		}
