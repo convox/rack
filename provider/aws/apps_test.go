c44d97ad989a54e2ab04ade6bc0af00a89319f5d
Ed Fricker <edafricker@gmail.com>
2021-01-26 20:42:17 +0000

    Testing?

--- provider/aws/apps_test.go
+++ provider/aws/apps_test.go
@@ -307,7 +307,7 @@ var cycleLogFilterLogEventsLimit1 = awsutil.Cycle{
 			"filterPattern": "test",
 			"interleaved": true,
 			"logGroupName": "convox-httpd-LogGroup-L4V203L35WRM",
-			"startTime": 1546300800000
+			"startTime": 1546300800001
 		}`,
 	},
 	Response: awsutil.Response{
