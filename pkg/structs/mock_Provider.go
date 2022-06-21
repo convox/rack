81a9b8b737e80f78f7f0f60b3e81d000ed7cbc3c
Heron Rossi <rossiheron@gmail.com>
2022-06-21 18:58:35 -0300

    Add test interfaces impl

--- pkg/structs/mock_Provider.go
+++ pkg/structs/mock_Provider.go
@@ -1102,6 +1102,20 @@ func (_m *MockProvider) ServiceUpdate(app string, name string, opts ServiceUpdat
 	return r0
 }
 
+// Sync provides a mock function with given fields: _a0
+func (_m *MockProvider) Sync(_a0 string) error {
+	ret := _m.Called(_a0)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string) error); ok {
+		r0 = rf(_a0)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // SystemGet provides a mock function with given fields:
 func (_m *MockProvider) SystemGet() (*System, error) {
 	ret := _m.Called()
