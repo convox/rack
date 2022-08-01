18a19c80b392630ceb7b63a0a97369037c511609
Heron Rossi <rossiheron@gmail.com>
2022-08-01 15:19:35 -0300

    Revert "fix tests 2"
    This reverts commit f801595f727ef1425e3d382fab775878c15cf795.

--- pkg/cli/apps_test.go
+++ pkg/cli/apps_test.go
@@ -115,18 +115,15 @@ func TestAppsCancelWait(t *testing.T) {
 
 func TestAppsCancelError(t *testing.T) {
 	testClient(t, func(e *cli.Engine, i *mocksdk.Interface) {
-		fxapp := fxApp()
-		fxrelease := fxRelease()
-
 		i.On("AppCancel", "app1").Return(fmt.Errorf("err1"))
-		i.On("ReleaseList", fxapp.Name, structs.ReleaseListOptions{Limit: options.Int(1)}).Return(fxReleaseList(), nil)
-		i.On("ReleaseGet", "app1", fxrelease.Id).Return(fxRelease(), nil)
 
 		res, err := testExecute(e, "apps cancel app1", nil)
 		require.NoError(t, err)
 		require.Equal(t, 1, res.Code)
 		res.RequireStderr(t, []string{"ERROR: err1"})
-		res.RequireStdout(t, []string{"Cancelling deployment of app1..."})
+		res.RequireStdout(t, []string{
+			"Cancelling app1... ",
+		})
 	})
 }
 
