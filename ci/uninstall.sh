87eff868e685b139b1b6311023ef9e63e02deb9e
Taynan Souza <taynan.tws@gmail.com>
2022-08-24 16:19:37 -0300

    Add CI to test updating rack and app

--- ci/uninstall.sh
+++ ci/uninstall.sh
@@ -3,4 +3,9 @@ set -ex -o pipefail
 
 source $(dirname $0)/env.sh
 
+# cleanup
+if [ "${IS_UPDATE}" == "true" ]; then
+  convox apps delete ci2 --wait
+fi
+
 convox rack uninstall ${PROVIDER} ${RACK_NAME} --force
