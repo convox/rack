9fc57e0de241dc6a4f4cf89a43c93b46ec7dd748
Taynan Souza <taynan.tws@gmail.com>
2022-09-05 16:35:30 -0300

    Split update/downgrade tests and using type approval
    The test will only run when it's approved

--- ci/install.sh
+++ ci/install.sh
@@ -3,9 +3,12 @@ set -ex -o pipefail
 
 source $(dirname $0)/env.sh
 
-if [ "${IS_UPDATE}" == "false" ]; then
-  convox rack install ${PROVIDER} --name ${RACK_NAME} --version ${VERSION} ${ARGS}
-else
+if [ "${EXTEND}" == "update" ]; then
+  # install the latest release, it will be updated to the release candidate
   convox rack install ${PROVIDER} --name ${RACK_NAME} ${ARGS}
+else
+  # install the release candidate, if EXTEND is "downgrade" it will be downgraded to the latest release
+  convox rack install ${PROVIDER} --name ${RACK_NAME} --version ${VERSION} ${ARGS}
 fi
+
 convox instances
--- ci/update.sh
+++ ci/update.sh
@@ -3,6 +3,6 @@ set -ex -o pipefail
 
 source $(dirname $0)/env.sh
 
-if [ "${VERSION}" != "$(convox api get /system | jq -r '.version')" ]; then
+if [ "${EXTEND}" == "update" ] && [ "${VERSION}" != "$(convox api get /system | jq -r '.version')" ]; then
   convox rack update "${VERSION}" --wait
 fi
