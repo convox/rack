dc7755b2e76640f643be847dbfe1ecd8b2212dd4
Taynan Souza <taynan.tws@gmail.com>
2022-09-05 16:35:30 -0300

    Split update/downgrade tests and using type approval
    The test will only run when it's approved

--- ci/test.sh
+++ ci/test.sh
@@ -52,8 +52,10 @@ esac
 
 # app
 cd $root/examples/httpd
-if [ "${IS_UPDATE}" == "false" ]; then
-  convox apps create ci2 --wait
+
+if [ "${EXTEND}" != "false" ]; then
+  # if update or downgrade test, it will deploy the app again
+  convox deploy -a ci2 --wait
 fi
 
 convox apps | grep ci2
@@ -202,9 +204,7 @@ case $provider in
 esac
 
 # cleanup
-if [ "${IS_UPDATE}" == "false" ]; then
-  convox apps delete ci2 --wait
-fi
+convox apps delete ci2 --wait
 
 # cleanup (provider-specific)
 case $provider in
