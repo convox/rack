3eb68a68691e0b1518e576acbb9d0421239b949f
Taynan Souza <taynan.tws@gmail.com>
2022-09-05 16:35:30 -0300

    Split update/downgrade tests and using type approval
    The test will only run when it's approved

--- ci/uninstall.sh
+++ ci/uninstall.sh
@@ -3,9 +3,4 @@ set -ex -o pipefail
 
 source $(dirname $0)/env.sh
 
-# cleanup
-if [ "${IS_UPDATE}" == "true" ]; then
-  convox apps delete ci2 --wait
-fi
-
 convox rack uninstall ${PROVIDER} ${RACK_NAME} --force
