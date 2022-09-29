63a7734076786c0b6a52b56bf75783a190a9a5c8
yuriadams <yuriadams@gmail.com>
2022-09-29 14:30:26 -0300

    addressing some suggestion in the PR

--- ci/apps-create.sh
+++ ci/apps-create.sh
@@ -4,12 +4,12 @@ set -ex -o pipefail
 root="$(cd $(dirname ${0:-})/..; pwd)"
 
 # cli
-convox2 version
+convox version
 
 # app
 cd $root/examples/httpd
-convox2 apps create ci2 --wait
-convox2 apps | grep ci2
-convox2 apps info ci2 | grep running
-convox2 deploy -a ci2 --wait
-convox2 apps info ci2 | grep running
+convox apps create ci2 --wait
+convox apps | grep ci2
+convox apps info ci2 | grep running
+convox deploy -a ci2 --wait
+convox apps info ci2 | grep running
