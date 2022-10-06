069dcc3f503b7a2b00bcc5dc84da83c93f0919f2
Taynan Souza <taynan.tws@gmail.com>
2022-10-06 20:19:23 -0300

    Add some tests and fix redis/memcached resource

--- ci/test.sh
+++ ci/test.sh
@@ -116,26 +116,26 @@ convox deploy -a ci2 --wait
 
 ps=$(convox api get /apps/ci2/processes | jq -r '.[]|select(.status=="running" and .name == "web")|.id' | grep web | head -n 1)
 # postgres resource
-convox exec -a ci2 $ps -- env | grep "POSTGRESS_URL="
-convox exec -a ci2 $ps -- env | grep "POSTGRESS_USER="
-convox exec -a ci2 $ps -- env | grep "POSTGRESS_PASS="
-convox exec -a ci2 $ps -- env | grep "POSTGRESS_HOST="
-convox exec -a ci2 $ps -- env | grep "POSTGRESS_PORT="
-convox exec -a ci2 $ps -- env | grep "POSTGRESS_NAME="
+convox exec -a ci2 $ps -- env | grep "POSTGRES_URL"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_USER"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_PASS"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_HOST"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_PORT"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_NAME"
 # mysql resource
-convox exec -a ci2 $ps -- env | grep "MYSQL_URL="
-convox exec -a ci2 $ps -- env | grep "MYSQL_USER="
-convox exec -a ci2 $ps -- env | grep "MYSQL_PASS="
-convox exec -a ci2 $ps -- env | grep "MYSQL_HOST="
-convox exec -a ci2 $ps -- env | grep "MYSQL_PORT="
-convox exec -a ci2 $ps -- env | grep "MYSQL_NAME="
+convox exec -a ci2 $ps -- env | grep "MYSQL_URL"
+convox exec -a ci2 $ps -- env | grep "MYSQL_USER"
+convox exec -a ci2 $ps -- env | grep "MYSQL_PASS"
+convox exec -a ci2 $ps -- env | grep "MYSQL_HOST"
+convox exec -a ci2 $ps -- env | grep "MYSQL_PORT"
+convox exec -a ci2 $ps -- env | grep "MYSQL_NAME"
 # mariadb resource
-convox exec -a ci2 $ps -- env | grep "MARIADB_URL="
-convox exec -a ci2 $ps -- env | grep "MARIADB_USER="
-convox exec -a ci2 $ps -- env | grep "MARIADB_PASS="
-convox exec -a ci2 $ps -- env | grep "MARIADB_HOST="
-convox exec -a ci2 $ps -- env | grep "MARIADB_PORT="
-convox exec -a ci2 $ps -- env | grep "MARIADB_NAME="
+convox exec -a ci2 $ps -- env | grep "MARIADB_URL"
+convox exec -a ci2 $ps -- env | grep "MARIADB_USER"
+convox exec -a ci2 $ps -- env | grep "MARIADB_PASS"
+convox exec -a ci2 $ps -- env | grep "MARIADB_HOST"
+convox exec -a ci2 $ps -- env | grep "MARIADB_PORT"
+convox exec -a ci2 $ps -- env | grep "MARIADB_NAME"
 
 # registries
 convox registries
