a71af554f167199fedcf2940a376f7efcd0bd751
Taynan Souza <taynan.tws@gmail.com>
2022-10-11 15:37:01 -0300

    [#3594] Add additional env var for resources
    Split the `${RESOURCE_NAME}_URL` into :
    
    ```
    ${RESOURCE_NAME}_USER
    ${RESOURCE_NAME}_PASS
    ${RESOURCE_NAME}_HOST
    ${RESOURCE_NAME}_PORT
    ${RESOURCE_NAME}_NAME
    ```

--- ci/test.sh
+++ ci/test.sh
@@ -113,6 +113,28 @@ cat /tmp/file2 | grep foo
 convox ps stop $ps -a ci2
 convox ps -a ci2 | grep -v $ps
 convox deploy -a ci2 --wait
+ps=$(convox api get /apps/ci2/processes | jq -r '.[]|select(.status=="running")|.id' | head -n 1)
+# postgres resource
+convox exec -a ci2 $ps -- env | grep "POSTGRES_URL"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_USER"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_PASS"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_HOST"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_PORT"
+convox exec -a ci2 $ps -- env | grep "POSTGRES_NAME"
+# mysql resource
+convox exec -a ci2 $ps -- env | grep "MYSQL_URL"
+convox exec -a ci2 $ps -- env | grep "MYSQL_USER"
+convox exec -a ci2 $ps -- env | grep "MYSQL_PASS"
+convox exec -a ci2 $ps -- env | grep "MYSQL_HOST"
+convox exec -a ci2 $ps -- env | grep "MYSQL_PORT"
+convox exec -a ci2 $ps -- env | grep "MYSQL_NAME"
+# mariadb resource
+convox exec -a ci2 $ps -- env | grep "MARIADB_URL"
+convox exec -a ci2 $ps -- env | grep "MARIADB_USER"
+convox exec -a ci2 $ps -- env | grep "MARIADB_PASS"
+convox exec -a ci2 $ps -- env | grep "MARIADB_HOST"
+convox exec -a ci2 $ps -- env | grep "MARIADB_PORT"
+convox exec -a ci2 $ps -- env | grep "MARIADB_NAME"
 
 # test apps cancel
 echo "FOO=not-bar" | convox env set -a ci2
