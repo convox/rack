0657fbfff895402ee70a1df235c782b64d1cbcaf
nightfury1204 <knightnahid@gmail.com>
2023-04-20 18:11:45 +0100

    Fix ci

--- ci/update.sh
+++ ci/update.sh
@@ -4,5 +4,21 @@ set -ex -o pipefail
 source $(dirname $0)/env.sh
 
 if [ "${ACTION}" == "update" ]; then
-  convox rack update "${VERSION}" --wait
+  convox rack update "${VERSION}" --wait | tee update-log.txt
+
+  if grep -Fq "_FAILED" update-log.txt; then
+    exit 1;
+  else
+    echo ok;
+  fi
+
+  version=$(convox rack | grep Version | awk -F '  +' '{print $2}')
+  if [ "${version}" != "${VERSION}" ]; then
+    exit 1;
+  fi
+
+  status=$(convox rack | grep Status | awk -F '  +' '{print $2}')
+  if [ "${status}" != "running" ]; then
+    exit 1;
+  fi
 fi
