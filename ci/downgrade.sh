ae3fa9190c8e7e0eb1f2b88222612d539e31f4ef
nightfury1204 <knightnahid@gmail.com>
2023-04-20 18:11:45 +0100

    Fix ci

--- ci/downgrade.sh
+++ ci/downgrade.sh
@@ -4,5 +4,21 @@ set -ex -o pipefail
 source $(dirname $0)/env.sh
 
 if [ "${ACTION}" == "downgrade" ]; then
-  convox rack update "${LATEST}" --wait
+  convox rack update "${LATEST}" --wait | tee downgrade-log.txt
+
+  if grep -Fxq "_FAILED" downgrade-log.txt; then
+    exit 1;
+  else
+    echo ok;
+  fi
+
+  version=$(convox rack | grep Version | awk -F '  +' '{print $2}')
+  if [ "${version}" != "${LATEST}" ]; then
+    exit 1;
+  fi
+
+  status=$(convox rack | grep Status | awk -F '  +' '{print $2}')
+  if [ "${status}" != "running" ]; then
+    exit 1;
+  fi
 fi
