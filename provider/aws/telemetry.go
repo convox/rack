bbb1da03fe7d51dec2abc489640e12ff4e10bc7f
yuriadams <yuriadams@gmail.com>
2023-05-09 10:27:12 -0300

    [#3668] Store rack install & rack update telemetry data (V2)
    ### What is the feature/fix?
    ![Screenshot 2023-04-25 at 11 14 26](https://user-images.githubusercontent.com/759623/234304722-24209b72-d129-45bd-8d1e-0756b6554021.png)
    
    ### Add screenshot or video (optional)
    
    Doc: https://convox602-my.sharepoint.com/:w:/g/personal/yuri_adams_convox_com/ERKCC87MHOxLi6_3F8PqWqIBOWecg2nEg06tIbl9NA6ujQ?e=8bW5AX
    
    ### Does it has a breaking change?
    
    No

---
+++ provider/aws/telemetry.go
@@ -0,0 +1,159 @@
+package aws
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	skipParams = strings.Join([]string{
+		"DefaultAmi",
+		"DefaultAmiArm",
+		"Password",
+		"Telemetry",
+		"Version",
+	}, ",")
+
+	redactedParams = strings.Join([]string{
+		"Ami",
+		"ApiCount",
+		"ApiCpu",
+		"ApiMonitorMemory",
+		"ApiRouter",
+		"ApiWebMemory",
+		"Autoscale",
+		"AvailabilityZones",
+		"BuildCpu",
+		"BuildImage",
+		"BuildInstance",
+		"BuildInstancePolicy",
+		"BuildMemory",
+		"BuildVolumeSize",
+		"ClientId",
+		"Development",
+		"EcsPollInterval",
+		"EncryptEbs",
+		"Encryption",
+		"ExistingVpc",
+		"HighAvailability",
+		"HttpProxy",
+		"ImagePullBehavior",
+		"IMDSHttpTokens",
+		"Internal",
+		"InternalOnly",
+		"InternalSuffix",
+		"InstanceBootCommand",
+		"InstanceRunCommand",
+		"InstanceType",
+		"InstanceUpdateBatchSize",
+		"InstancePolicy",
+		"InstanceSecurityGroup",
+		"BuildInstanceSecurityGroup",
+		"InternetGateway",
+		"Key",
+		"LogBucket",
+		"LogDriver",
+		"LogRetention",
+		"Private",
+		"PrivateApi",
+		"PrivateApiSecurityGroup",
+		"PrivateBuild",
+		"RouterInternalSecurityGroup",
+		"RouterSecurityGroup",
+		"ScheduleRackScaleDown",
+		"ScheduleRackScaleUp",
+		"SpotInstanceBid",
+		"SslPolicy",
+		"Subnet0CIDR",
+		"Subnet1CIDR",
+		"Subnet2CIDR",
+		"SubnetPrivate0CIDR",
+		"SubnetPrivate1CIDR",
+		"SubnetPrivate2CIDR",
+		"SwapSize",
+		"SyslogDestination",
+		"SyslogFormat",
+		"VPCCIDR",
+		"Tenancy",
+		"WhiteList",
+	}, ",")
+)
+
+func (p *Provider) RackParamsToSync(release string, params map[string]string) map[string]string {
+	defaultParams, err := templateParams(release)
+	if err != nil {
+		return nil
+	}
+
+	toSync := make(map[string]string)
+	for k, v := range params {
+		if strings.Contains(skipParams, k) {
+			continue
+		}
+
+		if v2, ok := defaultParams[k]; ok && v == v2 {
+			continue
+		}
+
+		if strings.Contains(redactedParams, k) {
+			v = hashParamValue(v)
+		}
+
+		toSync[k] = v
+	}
+
+	return toSync
+}
+
+func templateParams(release string) (map[string]string, error) {
+	var template []byte
+
+	if release == "dev" {
+		data, err := os.ReadFile("provider/aws/formation/rack.json")
+		if err != nil {
+			return nil, err
+		}
+
+		template = data
+	} else {
+		res, err := http.Get(fmt.Sprintf("https://convox.s3.amazonaws.com/release/%s/rack.json", release))
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		template = data
+	}
+
+	var templateMap map[string]interface{}
+	if err := json.Unmarshal([]byte(template), &templateMap); err != nil {
+		return nil, err
+	}
+
+	params := map[string]string{}
+	for k, v := range templateMap["Parameters"].(map[string]interface{}) {
+		d, ok := v.(map[string]interface{})["Default"]
+		if ok {
+			params[k] = d.(string)
+		}
+	}
+
+	return params, nil
+}
+
+func hashParamValue(value string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(value))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
