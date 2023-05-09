9edfa42421202bd34a9eb0063983f3941b2eb944
yuriadams <yuriadams@gmail.com>
2023-05-09 11:19:55 -0300

    [#3670] E2e tests rack resources
    ### What is the feature/fix?
    
    Create a new set of CI Tests to test apps resources, apps resources and timer.
    
    The `example/httpd` will only include the minimum convox.yaml possible. Created a new `examples/full-convox-yaml` to include more parameters to be used on the new CI tests.
    
    It will increase the test timer from 02:30~03:00 to 01:45 ~02:00.
    
    ### Does it has a breaking change?
    
    No.
    
    ### How to use/test it?
    
    N/A
    
    ### Checklist
    - [N/A] New coverage tests
    - [ ] Unit tests passing
    - [ ] E2E tests passing
    - [ ] E2E downgrade/update test passing
    - [N/A] Documentation updated
    - [ ] No warnings or errors on Deepsource/Codecov

---
+++ ci/additonal-test/timers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex -o pipefail
+
+timerLog=$(convox logs -a ci2 --no-follow --since 1m | grep service/example)
+if ! [[ $timerLog == *"Hello Timer"* ]]; then
+  echo "failed"; exit 1;
+fi
