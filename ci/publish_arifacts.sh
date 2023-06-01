d3255a7208891b458a49901346d276b89d83fe44
MD NURE ALAM Nahid <mdnnahid@amazon.com>
2023-06-01 17:02:23 +0100

    redesign v2 release

---
+++ ci/publish_arifacts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "version:$VERSION"
+
+# check for version
+[ -z "$VERSION" ] && echo "VERSION is empty" && exit 1
+
+# link cli
+aws s3 cp s3://convox/release/${VERSION}/cli/linux/convox s3://convox/cli/linux/convox --acl public-read
+aws s3 cp s3://convox/release/${VERSION}/cli/linux/convox-arm64 s3://convox/cli/linux/convox-arm64 --acl public-read
+aws s3 cp s3://convox/release/${VERSION}/cli/darwin/convox s3://convox/cli/darwin/convox --acl public-read
+aws s3 cp s3://convox/release/${VERSION}/cli/darwin/convox-arm64 s3://convox/cli/darwin/convox-arm64 --acl public-read
+aws s3 cp s3://convox/release/${VERSION}/cli/windows/convox.exe s3://convox/cli/windows/convox.exe --acl public-read
