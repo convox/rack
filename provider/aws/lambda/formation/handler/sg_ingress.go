9f54e945bef4841f32afd487b7679867c4d4dbf8
nightfury1204 <knightnahid@gmail.com>
2023-06-05 21:55:00 +0100

    Reimplement whitelisting for rack api

---
+++ provider/aws/lambda/formation/handler/sg_ingress.go
@@ -0,0 +1,110 @@
+package handler
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+const CONVOX_MANAGED = "convox managed"
+
+func HandleSGIngress(req Request) (string, map[string]string, error) {
+	defer recoverFailure(req)
+
+	switch req.RequestType {
+	case "Create":
+		fmt.Println("CREATING SG ingresses")
+		fmt.Printf("req %+v\n", req)
+		return SGIngressApply(req)
+	case "Update":
+		fmt.Println("UPDATING SG ingresses")
+		fmt.Printf("req %+v\n", req)
+		return SGIngressApply(req)
+	case "Delete":
+		fmt.Println("no need to delete, since sg will be deleted")
+		fmt.Printf("req %+v\n", req)
+		return "", nil, nil
+	}
+
+	return "", nil, fmt.Errorf("unknown RequestType: %s", req.RequestType)
+}
+
+func SGIngressApply(req Request) (string, map[string]string, error) {
+	sgId := aws.String(req.ResourceProperties["SecurityGroupID"].(string))
+	newIpStrs := strings.Split(req.ResourceProperties["Ips"].(string), ",")
+
+	res, err := EC2(req).DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+		GroupIds: []*string{sgId},
+	})
+	if err != nil {
+		return "invalid", nil, err
+	}
+
+	var ipPermissions []*ec2.IpPermission
+	for _, group := range res.SecurityGroups {
+		ipPermissions = group.IpPermissions
+	}
+
+	var prevIps []*ec2.IpRange
+	for i := range ipPermissions {
+		if ipPermissions[i].IpProtocol != nil && *ipPermissions[i].IpProtocol == "tcp" &&
+			ipPermissions[i].FromPort != nil && *ipPermissions[i].FromPort == 443 &&
+			ipPermissions[i].ToPort != nil && *ipPermissions[i].ToPort == 443 {
+			for _, ipRange := range ipPermissions[i].IpRanges {
+				if ipRange.Description != nil && strings.Contains(*ipRange.Description, CONVOX_MANAGED) {
+					prevIps = append(prevIps, &ec2.IpRange{
+						CidrIp: ipRange.CidrIp,
+					})
+				}
+			}
+		}
+	}
+
+	if len(prevIps) > 0 {
+		_, err = EC2(req).RevokeSecurityGroupIngress(&ec2.RevokeSecurityGroupIngressInput{
+			GroupId: sgId,
+			IpPermissions: []*ec2.IpPermission{
+				{
+					FromPort:   aws.Int64(443),
+					ToPort:     aws.Int64(443),
+					IpProtocol: aws.String("tcp"),
+					IpRanges:   prevIps,
+				},
+			},
+		})
+		if err != nil {
+			return "invalid", nil, err
+		}
+	}
+
+	var newIps []*ec2.IpRange
+	for _, ipStr := range newIpStrs {
+		cidr := strings.TrimSpace(ipStr)
+		if cidr != "" {
+			newIps = append(newIps, &ec2.IpRange{
+				CidrIp:      aws.String(cidr),
+				Description: aws.String(CONVOX_MANAGED),
+			})
+		}
+	}
+
+	if len(newIps) > 0 {
+		_, err = EC2(req).AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
+			GroupId: sgId,
+			IpPermissions: []*ec2.IpPermission{
+				{
+					FromPort:   aws.Int64(443),
+					ToPort:     aws.Int64(443),
+					IpProtocol: aws.String("tcp"),
+					IpRanges:   newIps,
+				},
+			},
+		})
+		if err != nil {
+			return "invalid", nil, err
+		}
+	}
+	return fmt.Sprintf("ingress-%s", *sgId), nil, nil
+}
