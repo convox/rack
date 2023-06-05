fa72721c40a9df1c56ec9e1920f4f11414948f0e
nightfury1204 <knightnahid@gmail.com>
2023-06-05 15:53:44 +0100

    [#3670] E2e tests rack resources
    ### What is the feature/fix?
    
    Create a new set of CI Tests to test apps resources, apps resources and timer.
    
    The `example/httpd` will only include the minimum convox.yaml possible. Created a new `examples/full-convox-yaml` to include more parameters to be used on the new CI tests.
    
    It will increase the test timer from 02:30~03:00 to 01:45 ~02:00.
    
    ### Does it has a breaking change?
    
    No.
    
    ### How to use/test it?
    
    N/A
    
    ### Checklist
    - [N/A] New coverage tests
    - [ ] Unit tests passing
    - [ ] E2E tests passing
    - [ ] E2E downgrade/update test passing
    - [N/A] Documentation updated
    - [ ] No warnings or errors on Deepsource/Codecov

---
+++ ci/additonal-test/rack-resources.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -ex -o pipefail
+
+declare -a RESOURCES=("s3" "sns" "sqs" "mysql")
+
+# syslog resource
+convox rack resources create syslog Url=tcp://syslog.convox.com --name cilog --wait
+convox rack resources | grep cilog | grep syslog
+convox rack resources info cilog | grep -v Apps
+convox rack resources url cilog | grep tcp://syslog.convox.com
+convox rack resources link cilog -a ci2 --wait
+convox rack resources info cilog | grep Apps | grep ci2
+convox rack resources unlink cilog -a ci2 --wait
+convox rack resources info cilog | grep -v Apps
+convox rack resources info cilog | grep -v Apps
+convox rack resources update cilog Url=tcp://syslog2.convox.com --wait
+convox rack resources info cilog | grep syslog2.convox.com
+convox rack resources url cilog | grep tcp://syslog2.convox.com
+convox rack resources delete cilog --wait
+
+# postgres resource
+convox rack resources create postgres --name pgdb --wait
+convox rack resources | grep pgdb | grep postgres
+dburl=$(convox rack resources url pgdb)
+convox rack resources update pgdb BackupRetentionPeriod=2 --wait
+[ "$dburl" == "$(convox rack resources url pgdb)" ]
+convox rack resources delete pgdb --wait
+
+# create all 4 resources
+for i in "${RESOURCES[@]}"
+do
+  convox rack resources create $i
+done
+
+for i in "${RESOURCES[@]}"
+do
+  # Check for resource to be marked as running
+  j=0
+  while [ "$(convox rack resources | grep $i | grep running | wc -l)" != "1" ]
+  do
+    # Exit if it takes more than 15 minutes
+    # mysql can take up to 15 minutes to create
+    if [ $((j++)) -gt 75 ]; then
+      exit 1
+    fi
+    echo "Waiting for resource $i to be marked as running..."
+    sleep 10
+  done
+done
+
+# delete all 4 resources
+for i in "${RESOURCES[@]}"
+do
+  name=$(convox rack resources | grep $i | awk '{print $1}')
+  echo "deleting resource $name"
+  convox rack resources delete $name --wait
+done
