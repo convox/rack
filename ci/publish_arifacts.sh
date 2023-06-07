95370e9ff61d014f70d63ee9eb346d494cfc163f
MD NURE ALAM Nahid <mdnnahid@amazon.com>
2023-06-07 17:31:57 +0100

    Avoid copying tags

--- ci/publish_arifacts.sh
+++ ci/publish_arifacts.sh
@@ -7,8 +7,8 @@ echo "version:$VERSION"
 [ -z "$VERSION" ] && echo "VERSION is empty" && exit 1
 
 # link cli
-aws s3 cp s3://convox/release/${VERSION}/cli/linux/convox s3://convox/cli/linux/convox --acl public-read
-aws s3 cp s3://convox/release/${VERSION}/cli/linux/convox-arm64 s3://convox/cli/linux/convox-arm64 --acl public-read
-aws s3 cp s3://convox/release/${VERSION}/cli/darwin/convox s3://convox/cli/darwin/convox --acl public-read
-aws s3 cp s3://convox/release/${VERSION}/cli/darwin/convox-arm64 s3://convox/cli/darwin/convox-arm64 --acl public-read
-aws s3 cp s3://convox/release/${VERSION}/cli/windows/convox.exe s3://convox/cli/windows/convox.exe --acl public-read
+aws s3 cp s3://convox/release/${VERSION}/cli/linux/convox s3://convox/cli/linux/convox --acl public-read --copy-props none
+aws s3 cp s3://convox/release/${VERSION}/cli/linux/convox-arm64 s3://convox/cli/linux/convox-arm64 --acl public-read --copy-props none
+aws s3 cp s3://convox/release/${VERSION}/cli/darwin/convox s3://convox/cli/darwin/convox --acl public-read --copy-props none
+aws s3 cp s3://convox/release/${VERSION}/cli/darwin/convox-arm64 s3://convox/cli/darwin/convox-arm64 --acl public-read --copy-props none
+aws s3 cp s3://convox/release/${VERSION}/cli/windows/convox.exe s3://convox/cli/windows/convox.exe --acl public-read --copy-props none
